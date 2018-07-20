NebulousLabs/Sia

Add api client method for cancel contract, siatest, and moved LoadString method to encoding.go

node/api/client/renter.go
@@ -9,7 +9,17 @@ import (
 
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/node/api"
+	"github.com/NebulousLabs/Sia/types"
 )
+
+// RenterCancelContractPost uses the /renter/cancel/contract endpoint to cancel
+// a contract
+func (c *Client) RenterCancelContractPost(id types.FileContractID) error {
+	values := url.Values{}
+	values.Set("id", id.String())
+	err := c.post("/renter/cancel/contract", values.Encode(), nil)
+	return err
+}
 
 // RenterContractsGet requests the /renter/contracts resource and returns
 // Contracts and ActiveContracts

SiaFoundation/siad

node/api/client/renter.go
@@ -9,7 +9,17 @@ import (
 
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/node/api"
+	"gitlab.com/NebulousLabs/Sia/types"
 )
+
+// RenterCancelContractPost uses the /renter/cancel/contract endpoint to cancel
+// a contract
+func (c *Client) RenterCancelContractPost(id types.FileContractID) error {
+	values := url.Values{}
+	values.Set("id", id.String())
+	err := c.post("/renter/cancel/contract", values.Encode(), nil)
+	return err
+}
 
 // RenterContractsGet requests the /renter/contracts resource and returns
 // Contracts and ActiveContracts