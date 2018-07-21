25621e7b7c2bd60ae4b112c326d150e81ea1c7bc
Matthew Sevey <mjsevey@gmail.com>
2018-07-20 21:39:16 -0400

    Split TestRenter into two groups to speed up test package

--- siatest/renter/renter_test.go
+++ siatest/renter/renter_test.go
@@ -82,6 +82,53 @@ func TestRenter(t *testing.T) {
 		{"TestDownloadAfterRenew", testDownloadAfterRenew},
 		{"TestDownloadMultipleLargeSectors", testDownloadMultipleLargeSectors},
 		{"TestLocalRepair", testLocalRepair},
+		// {"TestRemoteRepair", testRemoteRepair},
+		// {"TestSingleFileGet", testSingleFileGet},
+		// {"TestStreamingCache", testStreamingCache},
+		// {"TestUploadDownload", testUploadDownload},
+	}
+	// Run subtests
+	for _, subtest := range subTests {
+		t.Run(subtest.name, func(t *testing.T) {
+			subtest.test(t, tg)
+		})
+	}
+}
+
+// TestRenterTwo executes a number of subtests using the same TestGroup to
+// save time on initialization
+func TestRenterTwo(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+	// t.SkipNow()
+
+	// Create a group for the subtests
+	groupParams := siatest.GroupParams{
+		Hosts:   5,
+		Renters: 1,
+		Miners:  1,
+	}
+	tg, err := siatest.NewGroupFromTemplate(renterTestDir(t.Name()), groupParams)
+	if err != nil {
+		t.Fatal("Failed to create group: ", err)
+	}
+	defer func() {
+		if err := tg.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// Specify subtests to run
+	subTests := []struct {
+		name string
+		test func(*testing.T, *siatest.TestGroup)
+	}{
+		// {"TestClearDownloadHistory", testClearDownloadHistory},
+		// {"TestDownloadAfterRenew", testDownloadAfterRenew},
+		// {"TestDownloadMultipleLargeSectors", testDownloadMultipleLargeSectors},
+		// {"TestLocalRepair", testLocalRepair},
 		{"TestRemoteRepair", testRemoteRepair},
 		{"TestSingleFileGet", testSingleFileGet},
 		{"TestStreamingCache", testStreamingCache},
