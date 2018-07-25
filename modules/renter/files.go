rashikkathuria/Sia

Add mode and size functions

modules/renter/files.go
@@ -1,7 +1,6 @@
 package renter
 
 import (
-	"errors"
 	"fmt"
 	"math"
 	"os"
@@ -13,6 +12,8 @@ import (
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/persist"
 	"github.com/NebulousLabs/Sia/types"
+
+	"github.com/NebulousLabs/errors"
 )
 
 var (
@@ -262,24 +263,11 @@ func (r *Renter) DeleteFile(nickname string) error {
 	delete(r.files, nickname)
 	delete(r.persist.Tracking, nickname)
 
-	err := persist.RemoveFile(filepath.Join(r.persistDir, f.name+ShareExtension))
-	if err != nil {
-		r.log.Println("WARN: couldn't remove file :", err)
-	}
-
 	r.saveSync()
 	r.mu.Unlock(lockID)
 
-	// delete the file's associated contract data.
-	f.mu.Lock()
-	defer f.mu.Unlock()
-
-	// mark the file as deleted
-	f.deleted = true
-
 	// TODO: delete the sectors of the file as well.
-
-	return nil
+	return errors.AddContext(f.Delete(), "failed to delete file")
 }
 
 // FileList returns all of the files that the renter has.

modules/renter/renter.go
@@ -30,6 +30,7 @@ import (
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/modules/renter/contractor"
 	"github.com/NebulousLabs/Sia/modules/renter/hostdb"
+	"github.com/NebulousLabs/Sia/modules/renter/siafile"
 	"github.com/NebulousLabs/Sia/persist"
 	siasync "github.com/NebulousLabs/Sia/sync"
 	"github.com/NebulousLabs/Sia/types"
@@ -172,9 +173,7 @@ type trackedFile struct {
 type Renter struct {
 	// File management.
 	//
-	// tracking contains a list of files that the user intends to maintain. By
-	// default, files loaded through sharing are not maintained by the user.
-	files map[string]*file
+	files map[string]*siafile.SiaFile
 
 	// Download management. The heap has a separate mutex because it is always
 	// accessed in isolation.

modules/renter/siafile/metadata.go
@@ -1,0 +1,39 @@
+package siafile
+
+import (
+	"os"
+
+	"github.com/NebulousLabs/Sia/types"
+)
+
+// Delete removes the file from disk and marks it as deleted. Once the file is
+// deleted, certain methods should return an error.
+func (sf *SiaFile) Delete() error {
+	panic("not implemented yet")
+}
+
+// Deleted indicates if this file has been deleted by the user.
+func (sf *SiaFile) Deleted() bool {
+	panic("not implemented yet")
+}
+
+// HostPublicKeys returns all the public keys of hosts the file has ever been
+// uploaded to. That means some of those hosts might no longer be in use.
+func (sf *SiaFile) HostPublicKeys() []types.SiaPublicKey {
+	panic("not implemented yet")
+}
+
+// Mode returns the FileMode of the SiaFile.
+func (sf *SiaFile) Mode() os.FileMode {
+	panic("not implemented yet")
+}
+
+// Name returns the file's name.
+func (sf *SiaFile) Name() string {
+	panic("not implemented yet")
+}
+
+// Size returns the file's size.
+func (sf *SiaFile) Size() uint64 {
+	panic("not implemented yet")
+}

modules/renter/siafile/siafile.go
@@ -1,13 +1,14 @@
 package siafile
 
 import (
-	"crypto"
 	"os"
 	"sync"
 	"time"
 
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
+
+	"github.com/NebulousLabs/Sia/crypto"
 )
 
 type (
@@ -32,17 +33,17 @@ type (
 
 		// utility fields. These are not persisted.
 		erasureCode modules.ErasureCoder
-		f           *os.File
+		filePath    string
 		mu          sync.Mutex
 		uid         string
 	}
 
 	// Metadata is the metadata of a SiaFile and is JSON encoded.
 	Metadata struct {
-		version   [16]byte          // version of the sia file format used
-		fileSize  int64             // total size of the file re
-		mode      os.FileMode       // unix filemode of the sia file - uint32
-		masterKey crypto.TwofishKey // masterkey used to encrypt pieces
+		version        [16]byte          // version of the sia file format used
+		staticFileSize int64             // total size of the file re
+		mode           os.FileMode       // unix filemode of the sia file - uint32
+		masterKey      crypto.TwofishKey // masterkey used to encrypt pieces
 
 		// following timestamps will be persisted using int64 unix timestamps
 		modTime    time.Time // time of last content modification

SiaFoundation/siad

modules/renter/files.go
@@ -1,7 +1,6 @@
 package renter
 
 import (
-	"errors"
 	"fmt"
 	"math"
 	"os"
@@ -13,6 +12,8 @@ import (
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/persist"
 	"gitlab.com/NebulousLabs/Sia/types"
+
+	"gitlab.com/NebulousLabs/errors"
 )
 
 var (
@@ -262,24 +263,11 @@ func (r *Renter) DeleteFile(nickname string) error {
 	delete(r.files, nickname)
 	delete(r.persist.Tracking, nickname)
 
-	err := persist.RemoveFile(filepath.Join(r.persistDir, f.name+ShareExtension))
-	if err != nil {
-		r.log.Println("WARN: couldn't remove file :", err)
-	}
-
 	r.saveSync()
 	r.mu.Unlock(lockID)
 
-	// delete the file's associated contract data.
-	f.mu.Lock()
-	defer f.mu.Unlock()
-
-	// mark the file as deleted
-	f.deleted = true
-
 	// TODO: delete the sectors of the file as well.
-
-	return nil
+	return errors.AddContext(f.Delete(), "failed to delete file")
 }
 
 // FileList returns all of the files that the renter has.

modules/renter/renter.go
@@ -30,6 +30,7 @@ import (
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/modules/renter/contractor"
 	"gitlab.com/NebulousLabs/Sia/modules/renter/hostdb"
+	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
 	"gitlab.com/NebulousLabs/Sia/persist"
 	siasync "gitlab.com/NebulousLabs/Sia/sync"
 	"gitlab.com/NebulousLabs/Sia/types"
@@ -175,9 +176,7 @@ type trackedFile struct {
 type Renter struct {
 	// File management.
 	//
-	// tracking contains a list of files that the user intends to maintain. By
-	// default, files loaded through sharing are not maintained by the user.
-	files map[string]*file
+	files map[string]*siafile.SiaFile
 
 	// Download management. The heap has a separate mutex because it is always
 	// accessed in isolation.

modules/renter/siafile/metadata.go
@@ -1,0 +1,39 @@
+package siafile
+
+import (
+	"os"
+
+	"gitlab.com/NebulousLabs/Sia/types"
+)
+
+// Delete removes the file from disk and marks it as deleted. Once the file is
+// deleted, certain methods should return an error.
+func (sf *SiaFile) Delete() error {
+	panic("not implemented yet")
+}
+
+// Deleted indicates if this file has been deleted by the user.
+func (sf *SiaFile) Deleted() bool {
+	panic("not implemented yet")
+}
+
+// HostPublicKeys returns all the public keys of hosts the file has ever been
+// uploaded to. That means some of those hosts might no longer be in use.
+func (sf *SiaFile) HostPublicKeys() []types.SiaPublicKey {
+	panic("not implemented yet")
+}
+
+// Mode returns the FileMode of the SiaFile.
+func (sf *SiaFile) Mode() os.FileMode {
+	panic("not implemented yet")
+}
+
+// Name returns the file's name.
+func (sf *SiaFile) Name() string {
+	panic("not implemented yet")
+}
+
+// Size returns the file's size.
+func (sf *SiaFile) Size() uint64 {
+	panic("not implemented yet")
+}

modules/renter/siafile/siafile.go
@@ -1,13 +1,14 @@
 package siafile
 
 import (
-	"crypto"
 	"os"
 	"sync"
 	"time"
 
-	"github.com/NebulousLabs/Sia/modules"
-	"github.com/NebulousLabs/Sia/types"
+	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/types"
+
+	"gitlab.com/NebulousLabs/Sia/crypto"
 )
 
 type (
@@ -32,17 +33,17 @@ type (
 
 		// utility fields. These are not persisted.
 		erasureCode modules.ErasureCoder
-		f           *os.File
+		filePath    string
 		mu          sync.Mutex
 		uid         string
 	}
 
 	// Metadata is the metadata of a SiaFile and is JSON encoded.
 	Metadata struct {
-		version   [16]byte          // version of the sia file format used
-		fileSize  int64             // total size of the file re
-		mode      os.FileMode       // unix filemode of the sia file - uint32
-		masterKey crypto.TwofishKey // masterkey used to encrypt pieces
+		version        [16]byte          // version of the sia file format used
+		staticFileSize int64             // total size of the file re
+		mode           os.FileMode       // unix filemode of the sia file - uint32
+		masterKey      crypto.TwofishKey // masterkey used to encrypt pieces
 
 		// following timestamps will be persisted using int64 unix timestamps
 		modTime    time.Time // time of last content modification