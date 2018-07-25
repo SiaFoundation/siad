jkawamoto/Sia

move erasure code to chunks, add localPath and move available out of metadata.go

modules/renter/siafile/siafile.go
@@ -2,11 +2,12 @@ package siafile
 
 import (
 	"bytes"
-	"encoding/base32"
 	"encoding/binary"
 	"fmt"
+	"os"
 	"sync"
 
+	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
 	"github.com/NebulousLabs/fastrand"
@@ -35,10 +36,9 @@ type (
 		chunks []Chunk
 
 		// utility fields. These are not persisted.
-		deleted     bool
-		erasureCode modules.ErasureCoder
-		mu          sync.RWMutex
-		uid         string
+		deleted bool
+		mu      sync.RWMutex
+		uid     string
 	}
 
 	// Chunk represents a single chunk of a file on disk
@@ -56,6 +56,7 @@ type (
 		//
 		erasureCodeType   [4]byte
 		erasureCodeParams [8]byte
+		erasureCode       modules.ErasureCoder
 
 		// extensionInfo is some reserved space for each chunk that allows us
 		// to indicate if a chunk is special.
@@ -74,30 +75,33 @@ type (
 )
 
 // New create a new SiaFile.
-func New(siaPath string, erasureCode modules.ErasureCoder, pieceSize, fileSize uint64) *SiaFile {
+// TODO needs changes once we move persistence over.
+func New(siaPath string, erasureCode []modules.ErasureCoder, pieceSize, fileSize uint64, fileMode os.FileMode, source string) *SiaFile {
 	file := &SiaFile{
 		metadata: Metadata{
 			fileSize:  int64(fileSize),
+			localPath: source,
 			masterKey: crypto.GenerateTwofishKey(),
+			mode:      fileMode,
 			pieceSize: pieceSize,
 			siaPath:   siaPath,
 		},
-		erasureCode: erasureCode,
-		uid:         base32.StdEncoding.EncodeToString(fastrand.Bytes(20))[:20],
+		uid: string(fastrand.Bytes(20)),
 	}
-	chunks := make([]Chunk, file.NumChunks())
-	for i := range chunks {
-		chunks[i].erasureCodeType = [4]byte{0, 0, 0, 1}
-		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[0:4], uint32(erasureCode.MinPieces()))
-		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[4:8], uint32(erasureCode.NumPieces()-erasureCode.MinPieces()))
-		chunks[i].pieces = make([][]Piece, erasureCode.NumPieces())
+	file.chunks = make([]Chunk, len(erasureCode))
+	for i := range file.chunks {
+		file.chunks[i].erasureCode = erasureCode[i]
+		file.chunks[i].erasureCodeType = [4]byte{0, 0, 0, 1}
+		binary.LittleEndian.PutUint32(file.chunks[i].erasureCodeParams[0:4], uint32(erasureCode[i].MinPieces()))
+		binary.LittleEndian.PutUint32(file.chunks[i].erasureCodeParams[4:8], uint32(erasureCode[i].NumPieces()-erasureCode[i].MinPieces()))
+		file.chunks[i].pieces = make([][]Piece, erasureCode[i].NumPieces())
 	}
-	file.chunks = chunks
 	return file
 }
 
 // AddPiece adds an uploaded piece to the file. It also updates the host table
 // if the public key of the host is not aleady known.
+// TODO needs changes once we move persistence over.
 func (sf *SiaFile) AddPiece(pk types.SiaPublicKey, chunkIndex, pieceIndex uint64, merkleRoot crypto.Hash) error {
 	sf.mu.Lock()
 	defer sf.mu.Unlock()
@@ -131,11 +135,50 @@ func (sf *SiaFile) AddPiece(pk types.SiaPublicKey, chunkIndex, pieceIndex uint64
 	return nil
 }
 
-// ErasureCode returns the erasure coder used by the file.
-func (sf *SiaFile) ErasureCode() modules.ErasureCoder {
+// Available indicates whether the file is ready to be downloaded.
+func (sf *SiaFile) Available(offline map[string]bool) bool {
 	sf.mu.RLock()
 	defer sf.mu.RUnlock()
-	return sf.erasureCode
+	// We need to find at least erasureCode.MinPieces different pieces for each
+	// chunk for the file to be available.
+	for chunkIndex, chunk := range sf.chunks {
+		piecesForChunk := 0
+		for _, pieceSet := range chunk.pieces {
+			for _, piece := range pieceSet {
+				if !offline[string(piece.HostPubKey.Key)] {
+					piecesForChunk++
+					break // break out since we only count unique pieces
+				}
+			}
+			if piecesForChunk >= sf.chunks[chunkIndex].erasureCode.MinPieces() {
+				break // we already have enough pieces for this chunk.
+			}
+		}
+		if piecesForChunk < sf.chunks[chunkIndex].erasureCode.MinPieces() {
+			return false // this chunk isn't available.
+		}
+	}
+	return true
+}
+
+// ChunkIndexByOffset will return the chunkIndex that contains the provided
+// offset of a file and also the relative offset within the chunk. If the
+// offset is out of bounds, chunkIndex will be equal to NumChunk().
+func (sf *SiaFile) ChunkIndexByOffset(offset uint64) (chunkIndex uint64, off uint64) {
+	for chunkIndex := uint64(0); chunkIndex < uint64(len(sf.chunks)); chunkIndex++ {
+		if sf.chunkSize(chunkIndex) > offset {
+			return chunkIndex, offset
+		}
+		offset -= sf.chunkSize(chunkIndex)
+	}
+	return
+}
+
+// ErasureCode returns the erasure coder used by the file.
+func (sf *SiaFile) ErasureCode(chunkIndex uint64) modules.ErasureCoder {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.chunks[chunkIndex].erasureCode
 }
 
 // NumChunks returns the number of chunks the file consists of. This will
@@ -144,16 +187,7 @@ func (sf *SiaFile) ErasureCode() modules.ErasureCoder {
 func (sf *SiaFile) NumChunks() uint64 {
 	sf.mu.RLock()
 	defer sf.mu.RUnlock()
-	// empty files still need at least one chunk
-	if sf.metadata.fileSize == 0 {
-		return 1
-	}
-	n := uint64(sf.metadata.fileSize) / sf.chunkSize()
-	// last chunk will be padded, unless chunkSize divides file evenly.
-	if uint64(sf.metadata.fileSize)%sf.chunkSize() != 0 {
-		n++
-	}
-	return n
+	return uint64(len(sf.chunks))
 }
 
 // Pieces returns all the pieces for a chunk in a slice of slices that contains
@@ -171,6 +205,78 @@ func (sf *SiaFile) Pieces(chunkIndex uint64) ([][]Piece, error) {
 		copy(pieces[pieceIndex], sf.chunks[chunkIndex].pieces[pieceIndex])
 	}
 	return pieces, nil
+}
+
+// Redundancy returns the redundancy of the least redundant chunk. A file
+// becomes available when this redundancy is >= 1. Assumes that every piece is
+// unique within a file contract. -1 is returned if the file has size 0. It
+// takes one argument, a map of offline contracts for this file.
+func (sf *SiaFile) Redundancy(offlineMap map[string]bool, goodForRenewMap map[string]bool) float64 {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	if sf.metadata.fileSize == 0 {
+		return -1
+	}
+
+	minPiecesRenew := ^uint64(0)
+	minPiecesNoRenew := ^uint64(0)
+	for _, chunk := range sf.chunks {
+		// Loop over chunks and remember how many unique pieces of the chunk
+		// were goodForRenew and how many were not.
+		numPiecesRenew := uint64(0)
+		numPiecesNoRenew := uint64(0)
+		for _, pieceSet := range chunk.pieces {
+			// Remember if we encountered a goodForRenew piece or a
+			// !goodForRenew piece that was at least online.
+			foundGoodForRenew := false
+			foundOnline := false
+			for _, piece := range pieceSet {
+				offline, exists1 := offlineMap[string(piece.HostPubKey.Key)]
+				goodForRenew, exists2 := goodForRenewMap[string(piece.HostPubKey.Key)]
+				if exists1 != exists2 {
+					build.Critical("contract can't be in one map but not in the other")
+				}
+				if !exists1 || offline {
+					continue
+				}
+				// If we found a goodForRenew piece we can stop.
+				if goodForRenew {
+					foundGoodForRenew = true
+					break
+				}
+				// Otherwise we continue since there might be other hosts with
+				// the same piece that are goodForRenew. We still remember that
+				// we found an online piece though.
+				foundOnline = true
+			}
+			if foundGoodForRenew {
+				numPiecesRenew++
+				numPiecesNoRenew++
+			} else if foundOnline {
+				numPiecesNoRenew++
+			}
+		}
+		// Remember the smallest number of goodForRenew pieces encountered.
+		if numPiecesRenew < minPiecesRenew {
+			minPiecesRenew = numPiecesRenew
+		}
+		// Remember the smallest number of !goodForRenew pieces encountered.
+		if numPiecesNoRenew < minPiecesNoRenew {
+			minPiecesNoRenew = numPiecesNoRenew
+		}
+	}
+
+	// If the redundancy is smaller than 1x we return the redundancy that
+	// includes contracts that are not good for renewal. The reason for this is
+	// a better user experience. If the renter operates correctly, redundancy
+	// should never go above numPieces / minPieces and redundancyNoRenew should
+	// never go below 1.
+	redundancy := float64(minPiecesRenew) / float64(sf.chunks[0].erasureCode.MinPieces())          // TODO this shouldn't be chunks[0]
+	redundancyNoRenew := float64(minPiecesNoRenew) / float64(sf.chunks[0].erasureCode.MinPieces()) //TODO this shouldn't be chunks[0]
+	if redundancy < 1 {
+		return redundancyNoRenew
+	}
+	return redundancy
 }
 
 // UID returns a unique identifier for this file.

SiaFoundation/siad

modules/renter/siafile/siafile.go
@@ -2,11 +2,12 @@ package siafile
 
 import (
 	"bytes"
-	"encoding/base32"
 	"encoding/binary"
 	"fmt"
+	"os"
 	"sync"
 
+	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/types"
 	"gitlab.com/NebulousLabs/fastrand"
@@ -35,10 +36,9 @@ type (
 		chunks []Chunk
 
 		// utility fields. These are not persisted.
-		deleted     bool
-		erasureCode modules.ErasureCoder
-		mu          sync.RWMutex
-		uid         string
+		deleted bool
+		mu      sync.RWMutex
+		uid     string
 	}
 
 	// Chunk represents a single chunk of a file on disk
@@ -56,6 +56,7 @@ type (
 		//
 		erasureCodeType   [4]byte
 		erasureCodeParams [8]byte
+		erasureCode       modules.ErasureCoder
 
 		// extensionInfo is some reserved space for each chunk that allows us
 		// to indicate if a chunk is special.
@@ -74,30 +75,33 @@ type (
 )
 
 // New create a new SiaFile.
-func New(siaPath string, erasureCode modules.ErasureCoder, pieceSize, fileSize uint64) *SiaFile {
+// TODO needs changes once we move persistence over.
+func New(siaPath string, erasureCode []modules.ErasureCoder, pieceSize, fileSize uint64, fileMode os.FileMode, source string) *SiaFile {
 	file := &SiaFile{
 		metadata: Metadata{
 			fileSize:  int64(fileSize),
+			localPath: source,
 			masterKey: crypto.GenerateTwofishKey(),
+			mode:      fileMode,
 			pieceSize: pieceSize,
 			siaPath:   siaPath,
 		},
-		erasureCode: erasureCode,
-		uid:         base32.StdEncoding.EncodeToString(fastrand.Bytes(20))[:20],
+		uid: string(fastrand.Bytes(20)),
 	}
-	chunks := make([]Chunk, file.NumChunks())
-	for i := range chunks {
-		chunks[i].erasureCodeType = [4]byte{0, 0, 0, 1}
-		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[0:4], uint32(erasureCode.MinPieces()))
-		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[4:8], uint32(erasureCode.NumPieces()-erasureCode.MinPieces()))
-		chunks[i].pieces = make([][]Piece, erasureCode.NumPieces())
+	file.chunks = make([]Chunk, len(erasureCode))
+	for i := range file.chunks {
+		file.chunks[i].erasureCode = erasureCode[i]
+		file.chunks[i].erasureCodeType = [4]byte{0, 0, 0, 1}
+		binary.LittleEndian.PutUint32(file.chunks[i].erasureCodeParams[0:4], uint32(erasureCode[i].MinPieces()))
+		binary.LittleEndian.PutUint32(file.chunks[i].erasureCodeParams[4:8], uint32(erasureCode[i].NumPieces()-erasureCode[i].MinPieces()))
+		file.chunks[i].pieces = make([][]Piece, erasureCode[i].NumPieces())
 	}
-	file.chunks = chunks
 	return file
 }
 
 // AddPiece adds an uploaded piece to the file. It also updates the host table
 // if the public key of the host is not aleady known.
+// TODO needs changes once we move persistence over.
 func (sf *SiaFile) AddPiece(pk types.SiaPublicKey, chunkIndex, pieceIndex uint64, merkleRoot crypto.Hash) error {
 	sf.mu.Lock()
 	defer sf.mu.Unlock()
@@ -131,11 +135,50 @@ func (sf *SiaFile) AddPiece(pk types.SiaPublicKey, chunkIndex, pieceIndex uint64
 	return nil
 }
 
-// ErasureCode returns the erasure coder used by the file.
-func (sf *SiaFile) ErasureCode() modules.ErasureCoder {
+// Available indicates whether the file is ready to be downloaded.
+func (sf *SiaFile) Available(offline map[string]bool) bool {
 	sf.mu.RLock()
 	defer sf.mu.RUnlock()
-	return sf.erasureCode
+	// We need to find at least erasureCode.MinPieces different pieces for each
+	// chunk for the file to be available.
+	for chunkIndex, chunk := range sf.chunks {
+		piecesForChunk := 0
+		for _, pieceSet := range chunk.pieces {
+			for _, piece := range pieceSet {
+				if !offline[string(piece.HostPubKey.Key)] {
+					piecesForChunk++
+					break // break out since we only count unique pieces
+				}
+			}
+			if piecesForChunk >= sf.chunks[chunkIndex].erasureCode.MinPieces() {
+				break // we already have enough pieces for this chunk.
+			}
+		}
+		if piecesForChunk < sf.chunks[chunkIndex].erasureCode.MinPieces() {
+			return false // this chunk isn't available.
+		}
+	}
+	return true
+}
+
+// ChunkIndexByOffset will return the chunkIndex that contains the provided
+// offset of a file and also the relative offset within the chunk. If the
+// offset is out of bounds, chunkIndex will be equal to NumChunk().
+func (sf *SiaFile) ChunkIndexByOffset(offset uint64) (chunkIndex uint64, off uint64) {
+	for chunkIndex := uint64(0); chunkIndex < uint64(len(sf.chunks)); chunkIndex++ {
+		if sf.chunkSize(chunkIndex) > offset {
+			return chunkIndex, offset
+		}
+		offset -= sf.chunkSize(chunkIndex)
+	}
+	return
+}
+
+// ErasureCode returns the erasure coder used by the file.
+func (sf *SiaFile) ErasureCode(chunkIndex uint64) modules.ErasureCoder {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.chunks[chunkIndex].erasureCode
 }
 
 // NumChunks returns the number of chunks the file consists of. This will
@@ -144,16 +187,7 @@ func (sf *SiaFile) ErasureCode() modules.ErasureCoder {
 func (sf *SiaFile) NumChunks() uint64 {
 	sf.mu.RLock()
 	defer sf.mu.RUnlock()
-	// empty files still need at least one chunk
-	if sf.metadata.fileSize == 0 {
-		return 1
-	}
-	n := uint64(sf.metadata.fileSize) / sf.chunkSize()
-	// last chunk will be padded, unless chunkSize divides file evenly.
-	if uint64(sf.metadata.fileSize)%sf.chunkSize() != 0 {
-		n++
-	}
-	return n
+	return uint64(len(sf.chunks))
 }
 
 // Pieces returns all the pieces for a chunk in a slice of slices that contains
@@ -171,6 +205,85 @@ func (sf *SiaFile) Pieces(chunkIndex uint64) ([][]Piece, error) {
 		copy(pieces[pieceIndex], sf.chunks[chunkIndex].pieces[pieceIndex])
 	}
 	return pieces, nil
+}
+
+// Redundancy returns the redundancy of the least redundant chunk. A file
+// becomes available when this redundancy is >= 1. Assumes that every piece is
+// unique within a file contract. -1 is returned if the file has size 0. It
+// takes one argument, a map of offline contracts for this file.
+func (sf *SiaFile) Redundancy(offlineMap map[string]bool, goodForRenewMap map[string]bool) float64 {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	if sf.metadata.fileSize == 0 {
+		// TODO change this once tiny files are supported.
+		if len(sf.chunks) != 1 {
+			// should never happen
+			return -1
+		} else {
+			ec := sf.chunks[0].erasureCode
+			return float64(ec.NumPieces()) / float64(ec.MinPieces())
+		}
+	}
+
+	minPiecesRenew := ^uint64(0)
+	minPiecesNoRenew := ^uint64(0)
+	for _, chunk := range sf.chunks {
+		// Loop over chunks and remember how many unique pieces of the chunk
+		// were goodForRenew and how many were not.
+		numPiecesRenew := uint64(0)
+		numPiecesNoRenew := uint64(0)
+		for _, pieceSet := range chunk.pieces {
+			// Remember if we encountered a goodForRenew piece or a
+			// !goodForRenew piece that was at least online.
+			foundGoodForRenew := false
+			foundOnline := false
+			for _, piece := range pieceSet {
+				offline, exists1 := offlineMap[string(piece.HostPubKey.Key)]
+				goodForRenew, exists2 := goodForRenewMap[string(piece.HostPubKey.Key)]
+				if exists1 != exists2 {
+					build.Critical("contract can't be in one map but not in the other")
+				}
+				if !exists1 || offline {
+					continue
+				}
+				// If we found a goodForRenew piece we can stop.
+				if goodForRenew {
+					foundGoodForRenew = true
+					break
+				}
+				// Otherwise we continue since there might be other hosts with
+				// the same piece that are goodForRenew. We still remember that
+				// we found an online piece though.
+				foundOnline = true
+			}
+			if foundGoodForRenew {
+				numPiecesRenew++
+				numPiecesNoRenew++
+			} else if foundOnline {
+				numPiecesNoRenew++
+			}
+		}
+		// Remember the smallest number of goodForRenew pieces encountered.
+		if numPiecesRenew < minPiecesRenew {
+			minPiecesRenew = numPiecesRenew
+		}
+		// Remember the smallest number of !goodForRenew pieces encountered.
+		if numPiecesNoRenew < minPiecesNoRenew {
+			minPiecesNoRenew = numPiecesNoRenew
+		}
+	}
+
+	// If the redundancy is smaller than 1x we return the redundancy that
+	// includes contracts that are not good for renewal. The reason for this is
+	// a better user experience. If the renter operates correctly, redundancy
+	// should never go above numPieces / minPieces and redundancyNoRenew should
+	// never go below 1.
+	redundancy := float64(minPiecesRenew) / float64(sf.chunks[0].erasureCode.MinPieces())          // TODO this shouldn't be chunks[0]
+	redundancyNoRenew := float64(minPiecesNoRenew) / float64(sf.chunks[0].erasureCode.MinPieces()) //TODO this shouldn't be chunks[0]
+	if redundancy < 1 {
+		return redundancyNoRenew
+	}
+	return redundancy
 }
 
 // UID returns a unique identifier for this file.