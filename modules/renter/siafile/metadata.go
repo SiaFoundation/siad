skunkjunk5/Sia

Implement all methods except fileToSiaFile and siaFileToFile

modules/renter/siafile/metadata.go
@@ -1,10 +1,13 @@
 package siafile
 
 import (
+	"math"
 	"os"
 	"time"
 
+	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/crypto"
+	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
 )
 
@@ -52,7 +55,28 @@ type (
 
 // Available indicates whether the file is ready to be downloaded.
 func (sf *SiaFile) Available(offline map[string]bool) bool {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	// We need to find at least erasureCode.MinPieces different pieces for each
+	// chunk for the file to be available.
+	for _, chunk := range sf.chunks {
+		piecesForChunk := 0
+		for _, pieceSet := range chunk.pieces {
+			for _, piece := range pieceSet {
+				if !offline[string(piece.HostPubKey.Key)] {
+					piecesForChunk++
+					break // break out since we only count unique pieces
+				}
+			}
+			if piecesForChunk >= sf.erasureCode.MinPieces() {
+				break // we already have enough pieces for this chunk.
+			}
+		}
+		if piecesForChunk < sf.erasureCode.MinPieces() {
+			return false // this chunk isn't available.
+		}
+	}
+	return true
 }
 
 // ChunkSize returns the size of a single chunk of the file.
@@ -64,25 +88,50 @@ func (sf *SiaFile) ChunkSize() uint64 {
 
 // Delete removes the file from disk and marks it as deleted. Once the file is
 // deleted, certain methods should return an error.
+// TODO: This will actually delete the file from disk once we change the
+// persistence structure to use the new file format.
 func (sf *SiaFile) Delete() error {
-	panic("not implemented yet")
+	sf.mu.Lock()
+	defer sf.mu.Unlock()
+	sf.deleted = true
+	return nil
 }
 
 // Deleted indicates if this file has been deleted by the user.
 func (sf *SiaFile) Deleted() bool {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.deleted
 }
 
 // Expiration returns the lowest height at which any of the file's contracts
 // will expire.
-func (sf *SiaFile) Expiration() types.BlockHeight {
-	panic("not implemented yet")
+func (sf *SiaFile) Expiration(contracts map[string]modules.RenterContract) types.BlockHeight {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	if len(sf.pubKeyTable) == 0 {
+		return 0
+	}
+
+	lowest := ^types.BlockHeight(0)
+	for _, pk := range sf.pubKeyTable {
+		contract, exists := contracts[string(pk.Key)]
+		if !exists {
+			continue
+		}
+		if contract.EndHeight < lowest {
+			lowest = contract.EndHeight
+		}
+	}
+	return lowest
 }
 
 // HostPublicKeys returns all the public keys of hosts the file has ever been
 // uploaded to. That means some of those hosts might no longer be in use.
 func (sf *SiaFile) HostPublicKeys() []types.SiaPublicKey {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.pubKeyTable
 }
 
 // MasterKey returns the masterkey used to encrypt the file.
@@ -94,7 +143,9 @@ func (sf *SiaFile) MasterKey() crypto.TwofishKey {
 
 // Mode returns the FileMode of the SiaFile.
 func (sf *SiaFile) Mode() os.FileMode {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.metadata.mode
 }
 
 // PieceSize returns the size of a single piece of the file.
@@ -109,12 +160,81 @@ func (sf *SiaFile) PieceSize() uint64 {
 // unique within a file contract. -1 is returned if the file has size 0. It
 // takes one argument, a map of offline contracts for this file.
 func (sf *SiaFile) Redundancy(offlineMap map[string]bool, goodForRenewMap map[string]bool) float64 {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	sf.mu.RUnlock()
+	if sf.metadata.fileSize == 0 {
+		return -1
+	}
+
+	minPiecesRenew := ^uint64(0)
+	minPiecesNoRenew := ^uint64(0)
+	for _, chunk := range sf.chunks {
+		// Loop over chunks and remember how many unique pieces of the chunk
+		// were goodForRenew and how many were not.
+		numPiecesRenew := uint64(0)
+		numPiecesNoRenew := uint64(0)
+		for _, pieceSet := range chunk.pieces {
+			// Remember if we encountered a goodForRenew piece or a
+			// !goodForRenew piece that was at least online.
+			foundGoodForRenew := false
+			foundOnline := false
+			for _, piece := range pieceSet {
+				offline, exists1 := offlineMap[string(piece.HostPubKey.Key)]
+				goodForRenew, exists2 := goodForRenewMap[string(piece.HostPubKey.Key)]
+				if exists1 != exists2 {
+					build.Critical("contract can't be in one map but not in the other")
+				}
+				if !exists1 || offline {
+					continue
+				}
+				// If we found a goodForRenew piece we can stop.
+				if goodForRenew {
+					foundGoodForRenew = true
+					break
+				}
+				// Otherwise we continue since there might be other hosts with
+				// the same piece that are goodForRenew. We still remember that
+				// we found an online piece though.
+				foundOnline = true
+			}
+			if foundGoodForRenew {
+				numPiecesRenew++
+				numPiecesNoRenew++
+			} else if foundOnline {
+				numPiecesNoRenew++
+			}
+		}
+		// Remember the smallest number of goodForRenew pieces encountered.
+		if numPiecesRenew < minPiecesRenew {
+			minPiecesRenew = numPiecesRenew
+		}
+		// Remember the smallest number of !goodForRenew pieces encountered.
+		if numPiecesNoRenew < minPiecesNoRenew {
+			minPiecesNoRenew = numPiecesNoRenew
+		}
+	}
+
+	// If the redundancy is smaller than 1x we return the redundancy that
+	// includes contracts that are not good for renewal. The reason for this is
+	// a better user experience. If the renter operates correctly, redundancy
+	// should never go above numPieces / minPieces and redundancyNoRenew should
+	// never go below 1.
+	redundancy := float64(minPiecesRenew) / float64(sf.erasureCode.MinPieces())
+	redundancyNoRenew := float64(minPiecesNoRenew) / float64(sf.erasureCode.MinPieces())
+	if redundancy < 1 {
+		return redundancyNoRenew
+	}
+	return redundancy
 }
 
 // Rename changes the name of the file to a new one.
-func (sf *SiaFile) Rename(newName string) string {
-	panic("not implemented yet")
+// TODO: This will actually rename the file on disk once we persist the new
+// file format.
+func (sf *SiaFile) Rename(newName string) error {
+	sf.mu.Lock()
+	defer sf.mu.Unlock()
+	sf.metadata.siaPath = newName
+	return nil
 }
 
 // SetMode sets the filemode of the sia file.
@@ -126,26 +246,43 @@ func (sf *SiaFile) SetMode(mode os.FileMode) {
 
 // SiaPath returns the file's sia path.
 func (sf *SiaFile) SiaPath() string {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.metadata.siaPath
 }
 
 // Size returns the file's size.
 func (sf *SiaFile) Size() uint64 {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return uint64(sf.metadata.fileSize)
 }
 
 // UploadedBytes indicates how many bytes of the file have been uploaded via
 // current file contracts. Note that this includes padding and redundancy, so
 // uploadedBytes can return a value much larger than the file's original filesize.
 func (sf *SiaFile) UploadedBytes() uint64 {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	var uploaded uint64
+	for _, chunk := range sf.chunks {
+		for _, pieceSet := range chunk.pieces {
+			// Note: we need to multiply by SectorSize here instead of
+			// f.pieceSize because the actual bytes uploaded include overhead
+			// from TwoFish encryption
+			uploaded += uint64(len(pieceSet)) * modules.SectorSize
+		}
+	}
+	return uploaded
 }
 
 // UploadProgress indicates what percentage of the file (plus redundancy) has
 // been uploaded. Note that a file may be Available long before UploadProgress
 // reaches 100%, and UploadProgress may report a value greater than 100%.
 func (sf *SiaFile) UploadProgress() float64 {
-	panic("not implemented yet")
+	uploaded := sf.UploadedBytes()
+	desired := modules.SectorSize * uint64(sf.ErasureCode().NumPieces()) * sf.NumChunks()
+	return math.Min(100*(float64(uploaded)/float64(desired)), 100)
 }
 
 // ChunkSize returns the size of a single chunk of the file.

modules/renter/upload.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/modules"
+	"github.com/NebulousLabs/Sia/modules/renter/siafile"
 )
 
 var (
@@ -81,7 +82,7 @@ func (r *Renter) Upload(up modules.FileUploadParams) error {
 	}
 
 	// Create file object.
-	f := fileToSiaFile(newFile(up.SiaPath, up.ErasureCode, pieceSize, uint64(fileInfo.Size())))
+	f := siafile.New(up.SiaPath, up.ErasureCode, pieceSize, uint64(fileInfo.Size()))
 	f.SetMode(fileInfo.Mode())
 
 	// Add file to renter.

SiaFoundation/siad

modules/renter/siafile/metadata.go
@@ -1,10 +1,13 @@
 package siafile
 
 import (
+	"math"
 	"os"
 	"time"
 
+	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/crypto"
+	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/types"
 )
 
@@ -52,7 +55,28 @@ type (
 
 // Available indicates whether the file is ready to be downloaded.
 func (sf *SiaFile) Available(offline map[string]bool) bool {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	// We need to find at least erasureCode.MinPieces different pieces for each
+	// chunk for the file to be available.
+	for _, chunk := range sf.chunks {
+		piecesForChunk := 0
+		for _, pieceSet := range chunk.pieces {
+			for _, piece := range pieceSet {
+				if !offline[string(piece.HostPubKey.Key)] {
+					piecesForChunk++
+					break // break out since we only count unique pieces
+				}
+			}
+			if piecesForChunk >= sf.erasureCode.MinPieces() {
+				break // we already have enough pieces for this chunk.
+			}
+		}
+		if piecesForChunk < sf.erasureCode.MinPieces() {
+			return false // this chunk isn't available.
+		}
+	}
+	return true
 }
 
 // ChunkSize returns the size of a single chunk of the file.
@@ -64,25 +88,50 @@ func (sf *SiaFile) ChunkSize() uint64 {
 
 // Delete removes the file from disk and marks it as deleted. Once the file is
 // deleted, certain methods should return an error.
+// TODO: This will actually delete the file from disk once we change the
+// persistence structure to use the new file format.
 func (sf *SiaFile) Delete() error {
-	panic("not implemented yet")
+	sf.mu.Lock()
+	defer sf.mu.Unlock()
+	sf.deleted = true
+	return nil
 }
 
 // Deleted indicates if this file has been deleted by the user.
 func (sf *SiaFile) Deleted() bool {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.deleted
 }
 
 // Expiration returns the lowest height at which any of the file's contracts
 // will expire.
-func (sf *SiaFile) Expiration() types.BlockHeight {
-	panic("not implemented yet")
+func (sf *SiaFile) Expiration(contracts map[string]modules.RenterContract) types.BlockHeight {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	if len(sf.pubKeyTable) == 0 {
+		return 0
+	}
+
+	lowest := ^types.BlockHeight(0)
+	for _, pk := range sf.pubKeyTable {
+		contract, exists := contracts[string(pk.Key)]
+		if !exists {
+			continue
+		}
+		if contract.EndHeight < lowest {
+			lowest = contract.EndHeight
+		}
+	}
+	return lowest
 }
 
 // HostPublicKeys returns all the public keys of hosts the file has ever been
 // uploaded to. That means some of those hosts might no longer be in use.
 func (sf *SiaFile) HostPublicKeys() []types.SiaPublicKey {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.pubKeyTable
 }
 
 // MasterKey returns the masterkey used to encrypt the file.
@@ -94,7 +143,9 @@ func (sf *SiaFile) MasterKey() crypto.TwofishKey {
 
 // Mode returns the FileMode of the SiaFile.
 func (sf *SiaFile) Mode() os.FileMode {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.metadata.mode
 }
 
 // PieceSize returns the size of a single piece of the file.
@@ -109,12 +160,81 @@ func (sf *SiaFile) PieceSize() uint64 {
 // unique within a file contract. -1 is returned if the file has size 0. It
 // takes one argument, a map of offline contracts for this file.
 func (sf *SiaFile) Redundancy(offlineMap map[string]bool, goodForRenewMap map[string]bool) float64 {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	sf.mu.RUnlock()
+	if sf.metadata.fileSize == 0 {
+		return -1
+	}
+
+	minPiecesRenew := ^uint64(0)
+	minPiecesNoRenew := ^uint64(0)
+	for _, chunk := range sf.chunks {
+		// Loop over chunks and remember how many unique pieces of the chunk
+		// were goodForRenew and how many were not.
+		numPiecesRenew := uint64(0)
+		numPiecesNoRenew := uint64(0)
+		for _, pieceSet := range chunk.pieces {
+			// Remember if we encountered a goodForRenew piece or a
+			// !goodForRenew piece that was at least online.
+			foundGoodForRenew := false
+			foundOnline := false
+			for _, piece := range pieceSet {
+				offline, exists1 := offlineMap[string(piece.HostPubKey.Key)]
+				goodForRenew, exists2 := goodForRenewMap[string(piece.HostPubKey.Key)]
+				if exists1 != exists2 {
+					build.Critical("contract can't be in one map but not in the other")
+				}
+				if !exists1 || offline {
+					continue
+				}
+				// If we found a goodForRenew piece we can stop.
+				if goodForRenew {
+					foundGoodForRenew = true
+					break
+				}
+				// Otherwise we continue since there might be other hosts with
+				// the same piece that are goodForRenew. We still remember that
+				// we found an online piece though.
+				foundOnline = true
+			}
+			if foundGoodForRenew {
+				numPiecesRenew++
+				numPiecesNoRenew++
+			} else if foundOnline {
+				numPiecesNoRenew++
+			}
+		}
+		// Remember the smallest number of goodForRenew pieces encountered.
+		if numPiecesRenew < minPiecesRenew {
+			minPiecesRenew = numPiecesRenew
+		}
+		// Remember the smallest number of !goodForRenew pieces encountered.
+		if numPiecesNoRenew < minPiecesNoRenew {
+			minPiecesNoRenew = numPiecesNoRenew
+		}
+	}
+
+	// If the redundancy is smaller than 1x we return the redundancy that
+	// includes contracts that are not good for renewal. The reason for this is
+	// a better user experience. If the renter operates correctly, redundancy
+	// should never go above numPieces / minPieces and redundancyNoRenew should
+	// never go below 1.
+	redundancy := float64(minPiecesRenew) / float64(sf.erasureCode.MinPieces())
+	redundancyNoRenew := float64(minPiecesNoRenew) / float64(sf.erasureCode.MinPieces())
+	if redundancy < 1 {
+		return redundancyNoRenew
+	}
+	return redundancy
 }
 
 // Rename changes the name of the file to a new one.
-func (sf *SiaFile) Rename(newName string) string {
-	panic("not implemented yet")
+// TODO: This will actually rename the file on disk once we persist the new
+// file format.
+func (sf *SiaFile) Rename(newName string) error {
+	sf.mu.Lock()
+	defer sf.mu.Unlock()
+	sf.metadata.siaPath = newName
+	return nil
 }
 
 // SetMode sets the filemode of the sia file.
@@ -126,26 +246,43 @@ func (sf *SiaFile) SetMode(mode os.FileMode) {
 
 // SiaPath returns the file's sia path.
 func (sf *SiaFile) SiaPath() string {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return sf.metadata.siaPath
 }
 
 // Size returns the file's size.
 func (sf *SiaFile) Size() uint64 {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return uint64(sf.metadata.fileSize)
 }
 
 // UploadedBytes indicates how many bytes of the file have been uploaded via
 // current file contracts. Note that this includes padding and redundancy, so
 // uploadedBytes can return a value much larger than the file's original filesize.
 func (sf *SiaFile) UploadedBytes() uint64 {
-	panic("not implemented yet")
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	var uploaded uint64
+	for _, chunk := range sf.chunks {
+		for _, pieceSet := range chunk.pieces {
+			// Note: we need to multiply by SectorSize here instead of
+			// f.pieceSize because the actual bytes uploaded include overhead
+			// from TwoFish encryption
+			uploaded += uint64(len(pieceSet)) * modules.SectorSize
+		}
+	}
+	return uploaded
 }
 
 // UploadProgress indicates what percentage of the file (plus redundancy) has
 // been uploaded. Note that a file may be Available long before UploadProgress
 // reaches 100%, and UploadProgress may report a value greater than 100%.
 func (sf *SiaFile) UploadProgress() float64 {
-	panic("not implemented yet")
+	uploaded := sf.UploadedBytes()
+	desired := modules.SectorSize * uint64(sf.ErasureCode().NumPieces()) * sf.NumChunks()
+	return math.Min(100*(float64(uploaded)/float64(desired)), 100)
 }
 
 // ChunkSize returns the size of a single chunk of the file.

modules/renter/upload.go
@@ -20,6 +20,7 @@ import (
 
 	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
 )
 
 var (
@@ -81,7 +82,7 @@ func (r *Renter) Upload(up modules.FileUploadParams) error {
 	}
 
 	// Create file object.
-	f := fileToSiaFile(newFile(up.SiaPath, up.ErasureCode, pieceSize, uint64(fileInfo.Size())))
+	f := siafile.New(up.SiaPath, up.ErasureCode, pieceSize, uint64(fileInfo.Size()))
 	f.SetMode(fileInfo.Mode())
 
 	// Add file to renter.