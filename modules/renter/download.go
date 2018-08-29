rashikkathuria/Sia

Replace all the occurences of the legacy file with the new file by creating unimplemented methods

modules/renter/download.go
@@ -132,6 +132,7 @@ import (
 	"time"
 
 	"github.com/NebulousLabs/Sia/modules"
+	"github.com/NebulousLabs/Sia/modules/renter/siafile"
 	"github.com/NebulousLabs/Sia/persist"
 
 	"github.com/NebulousLabs/errors"
@@ -177,7 +178,7 @@ type (
 		destination       downloadDestination // The place to write the downloaded data.
 		destinationType   string              // "file", "buffer", "http stream", etc.
 		destinationString string              // The string to report to the user for the destination.
-		file              *file               // The file to download.
+		file              *siafile.SiaFile    // The file to download.
 
 		latencyTarget time.Duration // Workers above this latency will be automatically put on standby initially.
 		length        uint64        // Length of download. Cannot be 0.
@@ -351,7 +352,7 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 	if params.offset < 0 {
 		return nil, errors.New("download offset cannot be a negative number")
 	}
-	if params.offset+params.length > params.file.size {
+	if params.offset+params.length > params.file.Size() {
 		return nil, errors.New("download is requesting data past the boundary of the file")
 	}
 
@@ -368,7 +369,7 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 		staticLength:          params.length,
 		staticOffset:          params.offset,
 		staticOverdrive:       params.overdrive,
-		staticSiaPath:         params.file.name,
+		staticSiaPath:         params.file.SiaPath(),
 		staticPriority:        params.priority,
 
 		log:           r.log,
@@ -376,34 +377,26 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 	}
 
 	// Determine which chunks to download.
-	minChunk := params.offset / params.file.staticChunkSize()
-	maxChunk := (params.offset + params.length - 1) / params.file.staticChunkSize()
+	minChunk := params.offset / params.file.ChunkSize()
+	maxChunk := (params.offset + params.length - 1) / params.file.ChunkSize()
 
 	// For each chunk, assemble a mapping from the contract id to the index of
 	// the piece within the chunk that the contract is responsible for.
 	chunkMaps := make([]map[string]downloadPieceInfo, maxChunk-minChunk+1)
 	for i := range chunkMaps {
 		chunkMaps[i] = make(map[string]downloadPieceInfo)
-	}
-	params.file.mu.Lock()
-	for id, contract := range params.file.contracts {
-		resolvedKey := r.hostContractor.ResolveIDToPubKey(id)
-		for _, piece := range contract.Pieces {
-			if piece.Chunk >= minChunk && piece.Chunk <= maxChunk {
-				// Sanity check - the same worker should not have two pieces for
-				// the same chunk.
-				_, exists := chunkMaps[piece.Chunk-minChunk][string(resolvedKey.Key)]
-				if exists {
-					r.log.Println("ERROR: Worker has multiple pieces uploaded for the same chunk.")
-				}
-				chunkMaps[piece.Chunk-minChunk][string(resolvedKey.Key)] = downloadPieceInfo{
-					index: piece.Piece,
-					root:  piece.MerkleRoot,
-				}
+		for j := uint64(0); j < uint64(params.file.NumPieces()); j++ {
+			piece, err := params.file.Piece(uint64(i), j)
+			if err != nil {
+				return nil, err
+			}
+			chunkMaps[i][string(piece.HostPubKey.Key)] = downloadPieceInfo{
+				index: j,
+				root:  piece.MerkleRoot,
 			}
 		}
+
 	}
-	params.file.mu.Unlock()
 
 	// Queue the downloads for each chunk.
 	writeOffset := int64(0) // where to write a chunk within the download destination.
@@ -411,14 +404,14 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 	for i := minChunk; i <= maxChunk; i++ {
 		udc := &unfinishedDownloadChunk{
 			destination: params.destination,
-			erasureCode: params.file.erasureCode,
-			masterKey:   params.file.masterKey,
+			erasureCode: params.file.ErasureCode(),
+			masterKey:   params.file.MasterKey(),
 
 			staticChunkIndex: i,
 			staticCacheID:    fmt.Sprintf("%v:%v", d.staticSiaPath, i),
 			staticChunkMap:   chunkMaps[i-minChunk],
-			staticChunkSize:  params.file.staticChunkSize(),
-			staticPieceSize:  params.file.pieceSize,
+			staticChunkSize:  params.file.ChunkSize(),
+			staticPieceSize:  params.file.PieceSize(),
 
 			// TODO: 25ms is just a guess for a good default. Really, we want to
 			// set the latency target such that slower workers will pick up the
@@ -433,8 +426,8 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 			staticNeedsMemory:   params.needsMemory,
 			staticPriority:      params.priority,
 
-			physicalChunkData: make([][]byte, params.file.erasureCode.NumPieces()),
-			pieceUsage:        make([]bool, params.file.erasureCode.NumPieces()),
+			physicalChunkData: make([][]byte, params.file.ErasureCode().NumPieces()),
+			pieceUsage:        make([]bool, params.file.ErasureCode().NumPieces()),
 
 			download:          d,
 			staticStreamCache: r.staticStreamCache,
@@ -443,16 +436,16 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 		// Set the fetchOffset - the offset within the chunk that we start
 		// downloading from.
 		if i == minChunk {
-			udc.staticFetchOffset = params.offset % params.file.staticChunkSize()
+			udc.staticFetchOffset = params.offset % params.file.ChunkSize()
 		} else {
 			udc.staticFetchOffset = 0
 		}
 		// Set the fetchLength - the number of bytes to fetch within the chunk
 		// that we start downloading from.
-		if i == maxChunk && (params.length+params.offset)%params.file.staticChunkSize() != 0 {
-			udc.staticFetchLength = ((params.length + params.offset) % params.file.staticChunkSize()) - udc.staticFetchOffset
+		if i == maxChunk && (params.length+params.offset)%params.file.ChunkSize() != 0 {
+			udc.staticFetchLength = ((params.length + params.offset) % params.file.ChunkSize()) - udc.staticFetchOffset
 		} else {
-			udc.staticFetchLength = params.file.staticChunkSize() - udc.staticFetchOffset
+			udc.staticFetchLength = params.file.ChunkSize() - udc.staticFetchOffset
 		}
 		// Set the writeOffset within the destination for where the data should
 		// be written.

modules/renter/downloadstreamer.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"time"
 
+	"github.com/NebulousLabs/Sia/modules/renter/siafile"
 	"github.com/NebulousLabs/errors"
 )
 
@@ -14,7 +15,7 @@ type (
 	// streamer is a io.ReadSeeker that can be used to stream downloads from
 	// the sia network.
 	streamer struct {
-		file   *file
+		file   *siafile.SiaFile
 		offset int64
 		r      *Renter
 	}
@@ -46,7 +47,7 @@ func (r *Renter) Streamer(siaPath string) (string, io.ReadSeeker, error) {
 		file: file,
 		r:    r,
 	}
-	return file.Name(), s, nil
+	return file.SiaPath(), s, nil
 }
 
 // Read implements the standard Read interface. It will download the requested
@@ -55,9 +56,7 @@ func (r *Renter) Streamer(siaPath string) (string, io.ReadSeeker, error) {
 // only request a single chunk at once.
 func (s *streamer) Read(p []byte) (n int, err error) {
 	// Get the file's size
-	s.file.mu.RLock()
-	fileSize := int64(s.file.size)
-	s.file.mu.RUnlock()
+	fileSize := int64(s.file.Size())
 
 	// Make sure we haven't reached the EOF yet.
 	if s.offset >= fileSize {
@@ -65,7 +64,7 @@ func (s *streamer) Read(p []byte) (n int, err error) {
 	}
 
 	// Calculate how much we can download. We never download more than a single chunk.
-	chunkSize := s.file.staticChunkSize()
+	chunkSize := s.file.ChunkSize()
 	remainingData := uint64(fileSize - s.offset)
 	requestedData := uint64(len(p))
 	remainingChunk := chunkSize - uint64(s.offset)%chunkSize
@@ -127,9 +126,7 @@ func (s *streamer) Seek(offset int64, whence int) (int64, error) {
 	case io.SeekCurrent:
 		newOffset = s.offset
 	case io.SeekEnd:
-		s.file.mu.RLock()
-		newOffset = int64(s.file.size)
-		s.file.mu.RUnlock()
+		newOffset = int64(s.file.Size())
 	}
 	newOffset += offset
 

modules/renter/siafile/siafile.go
@@ -1,10 +1,12 @@
 package siafile
 
 import (
+	"encoding/base32"
 	"sync"
 
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
+	"github.com/NebulousLabs/fastrand"
 
 	"github.com/NebulousLabs/Sia/crypto"
 )
@@ -31,7 +33,7 @@ type (
 
 		// utility fields. These are not persisted.
 		erasureCode modules.ErasureCoder
-		mu          sync.Mutex
+		mu          sync.RWMutex
 		uid         string
 	}
 
@@ -63,8 +65,69 @@ type (
 
 	// Piece represents a single piece of a chunk on disk
 	Piece struct {
-		keyNonce  [4]byte     // nonce used for encrypting the piece
-		pubKeyOff uint16      // offset in the pubKeyTable
-		root      crypto.Hash // merkle root of the piece
+		KeyNonce   [4]byte            // nonce used for encrypting the piece
+		HostPubKey types.SiaPublicKey // public key of the host
+		MerkleRoot crypto.Hash        // merkle root of the piece
 	}
 )
+
+// New create a new SiaFile.
+func New(siaPath string, erasureCode modules.ErasureCoder, masterKey crypto.TwofishKey) *SiaFile {
+	file := &SiaFile{
+		metadata: Metadata{
+			masterKey: masterKey,
+			pieceSize: modules.SectorSize - crypto.TwofishOverhead,
+			siaPath:   siaPath,
+		},
+		erasureCode: erasureCode,
+		uid:         base32.StdEncoding.EncodeToString(fastrand.Bytes(20))[:20],
+	}
+	return file
+}
+
+// AddPiece adds an uploaded piece to the file. It also updates the host table
+// if the public key of the host is not aleady known.
+func (sf *SiaFile) AddPiece(pk types.SiaPublicKey, chunkIndex, pieceIndex uint64, merkleRoot crypto.Hash) error {
+	panic("Not implemented yet")
+}
+
+// ErasureCode returns the erasure coder used by the file.
+func (sf *SiaFile) ErasureCode() modules.ErasureCoder {
+	sf.mu.RLock()
+	sf.mu.RUnlock()
+	return sf.erasureCode
+}
+
+// NumChunks returns the number of chunks the file consists of.
+func (sf *SiaFile) NumChunks() uint64 {
+	// empty files still need at least one chunk
+	if sf.metadata.fileSize == 0 {
+		return 1
+	}
+	n := uint64(sf.metadata.fileSize) / sf.chunkSize()
+	// last chunk will be padded, unless chunkSize divides file evenly.
+	if uint64(sf.metadata.fileSize)%sf.chunkSize() != 0 {
+		n++
+	}
+	return n
+}
+
+// NumPieces returns the number of pieces each chunk in the file consists of.
+func (sf *SiaFile) NumPieces() uint64 {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return uint64(sf.erasureCode.NumPieces())
+}
+
+// Piece returns the piece the index pieceIndex from within the chunk at the
+// index chunkIndex.
+func (sf *SiaFile) Piece(chunkIndex, pieceIndex uint64) (Piece, error) {
+	// TODO should return a deep copy to make sure that the caller can't modify
+	// the chunks without holding a lock.
+	panic("Not implemented yet")
+}
+
+// UID returns a unique identifier for this file.
+func (sf *SiaFile) UID() string {
+	panic("Not implemented yet")
+}

modules/renter/uploadchunk.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 
 	"github.com/NebulousLabs/Sia/crypto"
+	"github.com/NebulousLabs/Sia/modules/renter/siafile"
 
 	"github.com/NebulousLabs/errors"
 )
@@ -23,7 +24,7 @@ type unfinishedUploadChunk struct {
 	// is known not to exist locally.
 	id         uploadChunkID
 	localPath  string
-	renterFile *file
+	renterFile *siafile.SiaFile
 
 	// Information about the chunk, namely where it exists within the file.
 	//
@@ -129,8 +130,8 @@ func (r *Renter) managedDownloadLogicalChunkData(chunk *unfinishedUploadChunk) e
 	// TODO: There is a disparity in the way that the upload and download code
 	// handle the last chunk, which may not be full sized.
 	downloadLength := chunk.length
-	if chunk.index == chunk.renterFile.numChunks()-1 && chunk.renterFile.size%chunk.length != 0 {
-		downloadLength = chunk.renterFile.size % chunk.length
+	if chunk.index == chunk.renterFile.NumChunks()-1 && chunk.renterFile.Size()%chunk.length != 0 {
+		downloadLength = chunk.renterFile.Size() % chunk.length
 	}
 
 	// Create the download.
@@ -176,7 +177,7 @@ func (r *Renter) managedDownloadLogicalChunkData(chunk *unfinishedUploadChunk) e
 func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 	// Calculate the amount of memory needed for erasure coding. This will need
 	// to be released if there's an error before erasure coding is complete.
-	erasureCodingMemory := chunk.renterFile.pieceSize * uint64(chunk.renterFile.erasureCode.MinPieces())
+	erasureCodingMemory := chunk.renterFile.PieceSize() * uint64(chunk.renterFile.ErasureCode().MinPieces())
 
 	// Calculate the amount of memory to release due to already completed
 	// pieces. This memory gets released during encryption, but needs to be
@@ -184,7 +185,7 @@ func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 	var pieceCompletedMemory uint64
 	for i := 0; i < len(chunk.pieceUsage); i++ {
 		if chunk.pieceUsage[i] {
-			pieceCompletedMemory += chunk.renterFile.pieceSize + crypto.TwofishOverhead
+			pieceCompletedMemory += chunk.renterFile.PieceSize() + crypto.TwofishOverhead
 		}
 	}
 
@@ -221,7 +222,7 @@ func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 	// fact to reduce the total memory required to create the physical data.
 	// That will also change the amount of memory we need to allocate, and the
 	// number of times we need to return memory.
-	chunk.physicalChunkData, err = chunk.renterFile.erasureCode.EncodeShards(chunk.logicalChunkData)
+	chunk.physicalChunkData, err = chunk.renterFile.ErasureCode().EncodeShards(chunk.logicalChunkData)
 	chunk.logicalChunkData = nil
 	r.memoryManager.Return(erasureCodingMemory)
 	chunk.memoryReleased += erasureCodingMemory
@@ -251,7 +252,7 @@ func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 			chunk.physicalChunkData[i] = nil
 		} else {
 			// Encrypt the piece.
-			key := deriveKey(chunk.renterFile.masterKey, chunk.index, uint64(i))
+			key := deriveKey(chunk.renterFile.MasterKey(), chunk.index, uint64(i))
 			chunk.physicalChunkData[i] = key.EncryptBytes(chunk.physicalChunkData[i])
 		}
 	}
@@ -320,6 +321,7 @@ func (r *Renter) managedFetchLogicalChunkData(chunk *unfinishedUploadChunk) erro
 // cleanup required. This can include returning rememory and releasing the chunk
 // from the map of active chunks in the chunk heap.
 func (r *Renter) managedCleanUpUploadChunk(uc *unfinishedUploadChunk) {
+	pieceSize := uc.renterFile.PieceSize()
 	uc.mu.Lock()
 	piecesAvailable := 0
 	var memoryReleased uint64
@@ -336,7 +338,7 @@ func (r *Renter) managedCleanUpUploadChunk(uc *unfinishedUploadChunk) {
 		// will prefer releasing later pieces, which improves computational
 		// complexity for erasure coding.
 		if piecesAvailable >= uc.workersRemaining {
-			memoryReleased += uc.renterFile.pieceSize + crypto.TwofishOverhead
+			memoryReleased += pieceSize + crypto.TwofishOverhead
 			uc.physicalChunkData[i] = nil
 			// Mark this piece as taken so that we don't double release memory.
 			uc.pieceUsage[i] = true

SiaFoundation/siad

modules/renter/download.go
@@ -132,6 +132,7 @@ import (
 	"time"
 
 	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
 	"gitlab.com/NebulousLabs/Sia/persist"
 	"gitlab.com/NebulousLabs/Sia/types"
 
@@ -178,7 +179,7 @@ type (
 		destination       downloadDestination // The place to write the downloaded data.
 		destinationType   string              // "file", "buffer", "http stream", etc.
 		destinationString string              // The string to report to the user for the destination.
-		file              *file               // The file to download.
+		file              *siafile.SiaFile    // The file to download.
 
 		latencyTarget time.Duration // Workers above this latency will be automatically put on standby initially.
 		length        uint64        // Length of download. Cannot be 0.
@@ -355,7 +356,7 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 	if params.offset < 0 {
 		return nil, errors.New("download offset cannot be a negative number")
 	}
-	if params.offset+params.length > params.file.size {
+	if params.offset+params.length > params.file.Size() {
 		return nil, errors.New("download is requesting data past the boundary of the file")
 	}
 
@@ -372,7 +373,7 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 		staticLength:          params.length,
 		staticOffset:          params.offset,
 		staticOverdrive:       params.overdrive,
-		staticSiaPath:         params.file.name,
+		staticSiaPath:         params.file.SiaPath(),
 		staticPriority:        params.priority,
 
 		log:           r.log,
@@ -380,10 +381,10 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 	}
 
 	// Determine which chunks to download.
-	minChunk := params.offset / params.file.staticChunkSize()
-	maxChunk := (params.offset + params.length - 1) / params.file.staticChunkSize()
+	minChunk := params.offset / params.file.ChunkSize()
+	maxChunk := (params.offset + params.length - 1) / params.file.ChunkSize()
 	// Protect maxChunk underflow on tiny files
-	if params.file.size < 4096 {
+	if params.file.Size() < 4096 {
 		maxChunk = 0
 	}
 
@@ -392,26 +393,18 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 	chunkMaps := make([]map[string]downloadPieceInfo, maxChunk-minChunk+1)
 	for i := range chunkMaps {
 		chunkMaps[i] = make(map[string]downloadPieceInfo)
-	}
-	params.file.mu.Lock()
-	for id, contract := range params.file.contracts {
-		resolvedKey := r.hostContractor.ResolveIDToPubKey(id)
-		for _, piece := range contract.Pieces {
-			if piece.Chunk >= minChunk && piece.Chunk <= maxChunk {
-				// Sanity check - the same worker should not have two pieces for
-				// the same chunk.
-				_, exists := chunkMaps[piece.Chunk-minChunk][string(resolvedKey.Key)]
-				if exists {
-					r.log.Println("ERROR: Worker has multiple pieces uploaded for the same chunk.")
-				}
-				chunkMaps[piece.Chunk-minChunk][string(resolvedKey.Key)] = downloadPieceInfo{
-					index: piece.Piece,
-					root:  piece.MerkleRoot,
-				}
+		for j := uint64(0); j < uint64(params.file.NumPieces()); j++ {
+			piece, err := params.file.Piece(uint64(i), j)
+			if err != nil {
+				return nil, err
+			}
+			chunkMaps[i][string(piece.HostPubKey.Key)] = downloadPieceInfo{
+				index: j,
+				root:  piece.MerkleRoot,
 			}
 		}
+
 	}
-	params.file.mu.Unlock()
 
 	// Queue the downloads for each chunk.
 	writeOffset := int64(0) // where to write a chunk within the download destination.
@@ -419,14 +412,14 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 	for i := minChunk; i <= maxChunk; i++ {
 		udc := &unfinishedDownloadChunk{
 			destination: params.destination,
-			erasureCode: params.file.erasureCode,
-			masterKey:   params.file.masterKey,
+			erasureCode: params.file.ErasureCode(),
+			masterKey:   params.file.MasterKey(),
 
 			staticChunkIndex: i,
 			staticCacheID:    fmt.Sprintf("%v:%v", d.staticSiaPath, i),
 			staticChunkMap:   chunkMaps[i-minChunk],
-			staticChunkSize:  params.file.staticChunkSize(),
-			staticPieceSize:  params.file.pieceSize,
+			staticChunkSize:  params.file.ChunkSize(),
+			staticPieceSize:  params.file.PieceSize(),
 
 			// TODO: 25ms is just a guess for a good default. Really, we want to
 			// set the latency target such that slower workers will pick up the
@@ -441,8 +434,8 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 			staticNeedsMemory:   params.needsMemory,
 			staticPriority:      params.priority,
 
-			physicalChunkData: make([][]byte, params.file.erasureCode.NumPieces()),
-			pieceUsage:        make([]bool, params.file.erasureCode.NumPieces()),
+			physicalChunkData: make([][]byte, params.file.ErasureCode().NumPieces()),
+			pieceUsage:        make([]bool, params.file.ErasureCode().NumPieces()),
 
 			download:          d,
 			staticStreamCache: r.staticStreamCache,
@@ -451,16 +444,16 @@ func (r *Renter) managedNewDownload(params downloadParams) (*download, error) {
 		// Set the fetchOffset - the offset within the chunk that we start
 		// downloading from.
 		if i == minChunk {
-			udc.staticFetchOffset = params.offset % params.file.staticChunkSize()
+			udc.staticFetchOffset = params.offset % params.file.ChunkSize()
 		} else {
 			udc.staticFetchOffset = 0
 		}
 		// Set the fetchLength - the number of bytes to fetch within the chunk
 		// that we start downloading from.
-		if i == maxChunk && (params.length+params.offset)%params.file.staticChunkSize() != 0 {
-			udc.staticFetchLength = ((params.length + params.offset) % params.file.staticChunkSize()) - udc.staticFetchOffset
+		if i == maxChunk && (params.length+params.offset)%params.file.ChunkSize() != 0 {
+			udc.staticFetchLength = ((params.length + params.offset) % params.file.ChunkSize()) - udc.staticFetchOffset
 		} else {
-			udc.staticFetchLength = params.file.staticChunkSize() - udc.staticFetchOffset
+			udc.staticFetchLength = params.file.ChunkSize() - udc.staticFetchOffset
 		}
 		// Set the writeOffset within the destination for where the data should
 		// be written.

modules/renter/downloadstreamer.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"time"
 
+	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
 	"gitlab.com/NebulousLabs/errors"
 )
 
@@ -14,7 +15,7 @@ type (
 	// streamer is a io.ReadSeeker that can be used to stream downloads from
 	// the sia network.
 	streamer struct {
-		file   *file
+		file   *siafile.SiaFile
 		offset int64
 		r      *Renter
 	}
@@ -46,7 +47,7 @@ func (r *Renter) Streamer(siaPath string) (string, io.ReadSeeker, error) {
 		file: file,
 		r:    r,
 	}
-	return file.Name(), s, nil
+	return file.SiaPath(), s, nil
 }
 
 // Read implements the standard Read interface. It will download the requested
@@ -55,9 +56,7 @@ func (r *Renter) Streamer(siaPath string) (string, io.ReadSeeker, error) {
 // only request a single chunk at once.
 func (s *streamer) Read(p []byte) (n int, err error) {
 	// Get the file's size
-	s.file.mu.RLock()
-	fileSize := int64(s.file.size)
-	s.file.mu.RUnlock()
+	fileSize := int64(s.file.Size())
 
 	// Make sure we haven't reached the EOF yet.
 	if s.offset >= fileSize {
@@ -65,7 +64,7 @@ func (s *streamer) Read(p []byte) (n int, err error) {
 	}
 
 	// Calculate how much we can download. We never download more than a single chunk.
-	chunkSize := s.file.staticChunkSize()
+	chunkSize := s.file.ChunkSize()
 	remainingData := uint64(fileSize - s.offset)
 	requestedData := uint64(len(p))
 	remainingChunk := chunkSize - uint64(s.offset)%chunkSize
@@ -127,9 +126,7 @@ func (s *streamer) Seek(offset int64, whence int) (int64, error) {
 	case io.SeekCurrent:
 		newOffset = s.offset
 	case io.SeekEnd:
-		s.file.mu.RLock()
-		newOffset = int64(s.file.size)
-		s.file.mu.RUnlock()
+		newOffset = int64(s.file.Size())
 	}
 	newOffset += offset
 

modules/renter/siafile/siafile.go
@@ -1,10 +1,12 @@
 package siafile
 
 import (
+	"encoding/base32"
 	"sync"
 
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/types"
+	"gitlab.com/NebulousLabs/fastrand"
 
 	"gitlab.com/NebulousLabs/Sia/crypto"
 )
@@ -31,7 +33,7 @@ type (
 
 		// utility fields. These are not persisted.
 		erasureCode modules.ErasureCoder
-		mu          sync.Mutex
+		mu          sync.RWMutex
 		uid         string
 	}
 
@@ -63,8 +65,69 @@ type (
 
 	// Piece represents a single piece of a chunk on disk
 	Piece struct {
-		keyNonce  [4]byte     // nonce used for encrypting the piece
-		pubKeyOff uint16      // offset in the pubKeyTable
-		root      crypto.Hash // merkle root of the piece
+		KeyNonce   [4]byte            // nonce used for encrypting the piece
+		HostPubKey types.SiaPublicKey // public key of the host
+		MerkleRoot crypto.Hash        // merkle root of the piece
 	}
 )
+
+// New create a new SiaFile.
+func New(siaPath string, erasureCode modules.ErasureCoder, masterKey crypto.TwofishKey) *SiaFile {
+	file := &SiaFile{
+		metadata: Metadata{
+			masterKey: masterKey,
+			pieceSize: modules.SectorSize - crypto.TwofishOverhead,
+			siaPath:   siaPath,
+		},
+		erasureCode: erasureCode,
+		uid:         base32.StdEncoding.EncodeToString(fastrand.Bytes(20))[:20],
+	}
+	return file
+}
+
+// AddPiece adds an uploaded piece to the file. It also updates the host table
+// if the public key of the host is not aleady known.
+func (sf *SiaFile) AddPiece(pk types.SiaPublicKey, chunkIndex, pieceIndex uint64, merkleRoot crypto.Hash) error {
+	panic("Not implemented yet")
+}
+
+// ErasureCode returns the erasure coder used by the file.
+func (sf *SiaFile) ErasureCode() modules.ErasureCoder {
+	sf.mu.RLock()
+	sf.mu.RUnlock()
+	return sf.erasureCode
+}
+
+// NumChunks returns the number of chunks the file consists of.
+func (sf *SiaFile) NumChunks() uint64 {
+	// empty files still need at least one chunk
+	if sf.metadata.fileSize == 0 {
+		return 1
+	}
+	n := uint64(sf.metadata.fileSize) / sf.chunkSize()
+	// last chunk will be padded, unless chunkSize divides file evenly.
+	if uint64(sf.metadata.fileSize)%sf.chunkSize() != 0 {
+		n++
+	}
+	return n
+}
+
+// NumPieces returns the number of pieces each chunk in the file consists of.
+func (sf *SiaFile) NumPieces() uint64 {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	return uint64(sf.erasureCode.NumPieces())
+}
+
+// Piece returns the piece the index pieceIndex from within the chunk at the
+// index chunkIndex.
+func (sf *SiaFile) Piece(chunkIndex, pieceIndex uint64) (Piece, error) {
+	// TODO should return a deep copy to make sure that the caller can't modify
+	// the chunks without holding a lock.
+	panic("Not implemented yet")
+}
+
+// UID returns a unique identifier for this file.
+func (sf *SiaFile) UID() string {
+	panic("Not implemented yet")
+}

modules/renter/uploadchunk.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 
 	"gitlab.com/NebulousLabs/Sia/crypto"
+	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
 
 	"gitlab.com/NebulousLabs/errors"
 )
@@ -23,7 +24,7 @@ type unfinishedUploadChunk struct {
 	// is known not to exist locally.
 	id         uploadChunkID
 	localPath  string
-	renterFile *file
+	renterFile *siafile.SiaFile
 
 	// Information about the chunk, namely where it exists within the file.
 	//
@@ -129,8 +130,8 @@ func (r *Renter) managedDownloadLogicalChunkData(chunk *unfinishedUploadChunk) e
 	// TODO: There is a disparity in the way that the upload and download code
 	// handle the last chunk, which may not be full sized.
 	downloadLength := chunk.length
-	if chunk.index == chunk.renterFile.numChunks()-1 && chunk.renterFile.size%chunk.length != 0 {
-		downloadLength = chunk.renterFile.size % chunk.length
+	if chunk.index == chunk.renterFile.NumChunks()-1 && chunk.renterFile.Size()%chunk.length != 0 {
+		downloadLength = chunk.renterFile.Size() % chunk.length
 	}
 
 	// Create the download.
@@ -176,7 +177,7 @@ func (r *Renter) managedDownloadLogicalChunkData(chunk *unfinishedUploadChunk) e
 func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 	// Calculate the amount of memory needed for erasure coding. This will need
 	// to be released if there's an error before erasure coding is complete.
-	erasureCodingMemory := chunk.renterFile.pieceSize * uint64(chunk.renterFile.erasureCode.MinPieces())
+	erasureCodingMemory := chunk.renterFile.PieceSize() * uint64(chunk.renterFile.ErasureCode().MinPieces())
 
 	// Calculate the amount of memory to release due to already completed
 	// pieces. This memory gets released during encryption, but needs to be
@@ -184,7 +185,7 @@ func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 	var pieceCompletedMemory uint64
 	for i := 0; i < len(chunk.pieceUsage); i++ {
 		if chunk.pieceUsage[i] {
-			pieceCompletedMemory += chunk.renterFile.pieceSize + crypto.TwofishOverhead
+			pieceCompletedMemory += chunk.renterFile.PieceSize() + crypto.TwofishOverhead
 		}
 	}
 
@@ -221,7 +222,7 @@ func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 	// fact to reduce the total memory required to create the physical data.
 	// That will also change the amount of memory we need to allocate, and the
 	// number of times we need to return memory.
-	chunk.physicalChunkData, err = chunk.renterFile.erasureCode.EncodeShards(chunk.logicalChunkData)
+	chunk.physicalChunkData, err = chunk.renterFile.ErasureCode().EncodeShards(chunk.logicalChunkData)
 	chunk.logicalChunkData = nil
 	r.memoryManager.Return(erasureCodingMemory)
 	chunk.memoryReleased += erasureCodingMemory
@@ -251,7 +252,7 @@ func (r *Renter) managedFetchAndRepairChunk(chunk *unfinishedUploadChunk) {
 			chunk.physicalChunkData[i] = nil
 		} else {
 			// Encrypt the piece.
-			key := deriveKey(chunk.renterFile.masterKey, chunk.index, uint64(i))
+			key := deriveKey(chunk.renterFile.MasterKey(), chunk.index, uint64(i))
 			chunk.physicalChunkData[i] = key.EncryptBytes(chunk.physicalChunkData[i])
 		}
 	}
@@ -320,6 +321,7 @@ func (r *Renter) managedFetchLogicalChunkData(chunk *unfinishedUploadChunk) erro
 // cleanup required. This can include returning rememory and releasing the chunk
 // from the map of active chunks in the chunk heap.
 func (r *Renter) managedCleanUpUploadChunk(uc *unfinishedUploadChunk) {
+	pieceSize := uc.renterFile.PieceSize()
 	uc.mu.Lock()
 	piecesAvailable := 0
 	var memoryReleased uint64
@@ -336,7 +338,7 @@ func (r *Renter) managedCleanUpUploadChunk(uc *unfinishedUploadChunk) {
 		// will prefer releasing later pieces, which improves computational
 		// complexity for erasure coding.
 		if piecesAvailable >= uc.workersRemaining {
-			memoryReleased += uc.renterFile.pieceSize + crypto.TwofishOverhead
+			memoryReleased += pieceSize + crypto.TwofishOverhead
 			uc.physicalChunkData[i] = nil
 			// Mark this piece as taken so that we don't double release memory.
 			uc.pieceUsage[i] = true