skunkjunk5/Sia

Implement conversion methods

modules/renter/files.go
@@ -5,9 +5,11 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/crypto"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/modules/renter/siafile"
+	"github.com/NebulousLabs/Sia/persist"
 	"github.com/NebulousLabs/Sia/types"
 
 	"github.com/NebulousLabs/errors"
@@ -83,11 +85,20 @@ func (r *Renter) DeleteFile(nickname string) error {
 	delete(r.files, nickname)
 	delete(r.persist.Tracking, nickname)
 
+	err := persist.RemoveFile(filepath.Join(r.persistDir, f.SiaPath()+ShareExtension))
+	if err != nil {
+		r.log.Println("WARN: couldn't remove file :", err)
+	}
+
 	r.saveSync()
 	r.mu.Unlock(lockID)
 
+	// mark the file as deleted
+	f.Delete()
+
 	// TODO: delete the sectors of the file as well.
-	return errors.AddContext(f.Delete(), "failed to delete file")
+
+	return nil
 }
 
 // FileList returns all of the files that the renter has.
@@ -250,12 +261,97 @@ func (r *Renter) RenameFile(currentName, newName string) error {
 
 // fileToSiaFile converts a legacy file to a SiaFile. Fields that can't be
 // populated using the legacy file remain blank.
-func fileToSiaFile(f *file) *siafile.SiaFile {
-	panic("not implemented yet")
+func (r *Renter) fileToSiaFile(f *file) *siafile.SiaFile {
+	fileData := siafile.FileData{
+		Name:        f.name,
+		FileSize:    f.size,
+		MasterKey:   f.masterKey,
+		ErasureCode: f.erasureCode,
+		PieceSize:   f.pieceSize,
+		Mode:        os.FileMode(f.mode),
+		Deleted:     f.deleted,
+		UID:         f.staticUID,
+	}
+	chunks := make([]siafile.FileChunk, f.numChunks())
+	for i := 0; i < len(chunks); i++ {
+		chunks[i].Pieces = make([][]siafile.Piece, f.erasureCode.NumPieces())
+	}
+	for _, contract := range f.contracts {
+		pk := r.hostContractor.ResolveIDToPubKey(contract.ID)
+		for _, piece := range contract.Pieces {
+			chunks[piece.Chunk].Pieces[piece.Piece] = append(chunks[piece.Chunk].Pieces[piece.Piece], siafile.Piece{
+				HostPubKey: pk,
+				MerkleRoot: piece.MerkleRoot,
+			})
+		}
+	}
+	fileData.Chunks = chunks
+	return siafile.NewFromFileData(fileData)
 }
 
 // siaFileToFile converts a SiaFile to a legacy file. Fields that don't exist
 // in the legacy file will get lost and therefore not persisted.
-func siaFileToFile(sf *siafile.SiaFile) *file {
-	panic("not implemented yet")
+func (r *Renter) siaFileToFile(sf *siafile.SiaFile) *file {
+	fileData := sf.ExportFileData()
+	f := &file{
+		contracts:   make(map[types.FileContractID]fileContract),
+		name:        fileData.Name,
+		size:        fileData.FileSize,
+		masterKey:   fileData.MasterKey,
+		erasureCode: fileData.ErasureCode,
+		pieceSize:   fileData.PieceSize,
+		mode:        uint32(fileData.Mode),
+		deleted:     fileData.Deleted,
+		staticUID:   fileData.UID,
+	}
+	for chunkIndex, chunk := range fileData.Chunks {
+		for pieceIndex, pieceSet := range chunk.Pieces {
+			for _, piece := range pieceSet {
+				c, ok := r.hostContractor.ContractByPublicKey(piece.HostPubKey)
+				if !ok {
+					build.Critical("missing contract when converting SiaFile to file")
+					continue
+				}
+				h, ok := r.hostDB.Host(piece.HostPubKey)
+				if !ok {
+					build.Critical("missing host when converting SiaFile to file")
+					continue
+				}
+				if _, exists := f.contracts[c.ID]; !exists {
+					f.contracts[c.ID] = fileContract{
+						ID:          c.ID,
+						IP:          h.NetAddress,
+						WindowStart: c.EndHeight,
+					}
+				}
+				fc := f.contracts[c.ID]
+				fc.Pieces = append(fc.Pieces, pieceData{
+					Chunk:      uint64(chunkIndex),
+					Piece:      uint64(pieceIndex),
+					MerkleRoot: piece.MerkleRoot,
+				})
+				f.contracts[c.ID] = fc
+			}
+		}
+	}
+	return f
+}
+
+// numChunks returns the number of chunks that f was split into.
+func (f *file) numChunks() uint64 {
+	// empty files still need at least one chunk
+	if f.size == 0 {
+		return 1
+	}
+	n := f.size / f.staticChunkSize()
+	// last chunk will be padded, unless chunkSize divides file evenly.
+	if f.size%f.staticChunkSize() != 0 {
+		n++
+	}
+	return n
+}
+
+// staticChunkSize returns the size of one chunk.
+func (f *file) staticChunkSize() uint64 {
+	return f.pieceSize * uint64(f.erasureCode.MinPieces())
 }

modules/renter/persist.go
@@ -10,10 +10,12 @@ import (
 	"path/filepath"
 	"strconv"
 
+	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/encoding"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/modules/renter/siafile"
 	"github.com/NebulousLabs/Sia/persist"
+	"github.com/NebulousLabs/Sia/types"
 )
 
 const (
@@ -57,6 +59,121 @@ type (
 	}
 )
 
+// MarshalSia implements the encoding.SiaMarshaller interface, writing the
+// file data to w.
+func (f *file) MarshalSia(w io.Writer) error {
+	enc := encoding.NewEncoder(w)
+
+	// encode easy fields
+	err := enc.EncodeAll(
+		f.name,
+		f.size,
+		f.masterKey,
+		f.pieceSize,
+		f.mode,
+	)
+	if err != nil {
+		return err
+	}
+	// COMPATv0.4.3 - encode the bytesUploaded and chunksUploaded fields
+	// TODO: the resulting .sia file may confuse old clients.
+	err = enc.EncodeAll(f.pieceSize*f.numChunks()*uint64(f.erasureCode.NumPieces()), f.numChunks())
+	if err != nil {
+		return err
+	}
+
+	// encode erasureCode
+	switch code := f.erasureCode.(type) {
+	case *rsCode:
+		err = enc.EncodeAll(
+			"Reed-Solomon",
+			uint64(code.dataPieces),
+			uint64(code.numPieces-code.dataPieces),
+		)
+		if err != nil {
+			return err
+		}
+	default:
+		if build.DEBUG {
+			panic("unknown erasure code")
+		}
+		return errors.New("unknown erasure code")
+	}
+	// encode contracts
+	if err := enc.Encode(uint64(len(f.contracts))); err != nil {
+		return err
+	}
+	for _, c := range f.contracts {
+		if err := enc.Encode(c); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// UnmarshalSia implements the encoding.SiaUnmarshaller interface,
+// reconstructing a file from the encoded bytes read from r.
+func (f *file) UnmarshalSia(r io.Reader) error {
+	dec := encoding.NewDecoder(r)
+
+	// COMPATv0.4.3 - decode bytesUploaded and chunksUploaded into dummy vars.
+	var bytesUploaded, chunksUploaded uint64
+
+	// Decode easy fields.
+	err := dec.DecodeAll(
+		&f.name,
+		&f.size,
+		&f.masterKey,
+		&f.pieceSize,
+		&f.mode,
+		&bytesUploaded,
+		&chunksUploaded,
+	)
+	if err != nil {
+		return err
+	}
+	f.staticUID = persist.RandomSuffix()
+
+	// Decode erasure coder.
+	var codeType string
+	if err := dec.Decode(&codeType); err != nil {
+		return err
+	}
+	switch codeType {
+	case "Reed-Solomon":
+		var nData, nParity uint64
+		err = dec.DecodeAll(
+			&nData,
+			&nParity,
+		)
+		if err != nil {
+			return err
+		}
+		rsc, err := NewRSCode(int(nData), int(nParity))
+		if err != nil {
+			return err
+		}
+		f.erasureCode = rsc
+	default:
+		return errors.New("unrecognized erasure code type: " + codeType)
+	}
+
+	// Decode contracts.
+	var nContracts uint64
+	if err := dec.Decode(&nContracts); err != nil {
+		return err
+	}
+	f.contracts = make(map[types.FileContractID]fileContract)
+	var contract fileContract
+	for i := uint64(0); i < nContracts; i++ {
+		if err := dec.Decode(&contract); err != nil {
+			return err
+		}
+		f.contracts[contract.ID] = contract
+	}
+	return nil
+}
+
 // saveFile saves a file to the renter directory.
 func (r *Renter) saveFile(f *siafile.SiaFile) error {
 	if f.Deleted() { // TODO: violation of locking convention
@@ -77,7 +194,7 @@ func (r *Renter) saveFile(f *siafile.SiaFile) error {
 	defer handle.Close()
 
 	// Write file data.
-	err = shareFiles([]*siafile.SiaFile{f}, handle)
+	err = r.shareFiles([]*siafile.SiaFile{f}, handle)
 	if err != nil {
 		return err
 	}
@@ -162,11 +279,11 @@ func (r *Renter) loadSettings() error {
 
 // shareFiles writes the specified files to w. First a header is written,
 // followed by the gzipped concatenation of each file.
-func shareFiles(siaFiles []*siafile.SiaFile, w io.Writer) error {
+func (r *Renter) shareFiles(siaFiles []*siafile.SiaFile, w io.Writer) error {
 	// Convert files to old type.
 	files := make([]*file, 0, len(siaFiles))
 	for _, sf := range siaFiles {
-		files = append(files, siaFileToFile(sf))
+		files = append(files, r.siaFileToFile(sf))
 	}
 	// Write header.
 	err := encoding.NewEncoder(w).EncodeAll(
@@ -219,7 +336,7 @@ func (r *Renter) ShareFiles(nicknames []string, shareDest string) error {
 		files[i] = f
 	}
 
-	err = shareFiles(files, handle)
+	err = r.shareFiles(files, handle)
 	if err != nil {
 		os.Remove(shareDest)
 		return err
@@ -244,7 +361,7 @@ func (r *Renter) ShareFilesASCII(nicknames []string) (string, error) {
 	}
 
 	buf := new(bytes.Buffer)
-	err := shareFiles(files, base64.NewEncoder(base64.URLEncoding, buf))
+	err := r.shareFiles(files, base64.NewEncoder(base64.URLEncoding, buf))
 	if err != nil {
 		return "", err
 	}
@@ -304,12 +421,12 @@ func (r *Renter) loadSharedFiles(reader io.Reader) ([]string, error) {
 	// Add files to renter.
 	names := make([]string, numFiles)
 	for i, f := range files {
-		r.files[f.name] = fileToSiaFile(f)
+		r.files[f.name] = r.fileToSiaFile(f)
 		names[i] = f.name
 	}
 	// Save the files.
 	for _, f := range files {
-		r.saveFile(fileToSiaFile(f))
+		r.saveFile(r.fileToSiaFile(f))
 	}
 
 	return names, nil

modules/renter/siafile/compat.go
@@ -1,0 +1,102 @@
+package siafile
+
+import (
+	"encoding/binary"
+	"os"
+
+	"github.com/NebulousLabs/Sia/crypto"
+	"github.com/NebulousLabs/Sia/modules"
+)
+
+type (
+	// FileData is a helper struct that contains all the relevant information
+	// of a file. It simplifies passing the necessary data between modules and
+	// keeps the interface clean.
+	FileData struct {
+		Name        string
+		FileSize    uint64
+		MasterKey   crypto.TwofishKey
+		ErasureCode modules.ErasureCoder
+		PieceSize   uint64
+		Mode        os.FileMode
+		Deleted     bool
+		UID         string
+		Chunks      []FileChunk
+	}
+	// FileChunk is a helper struct that contains data about a chunk.
+	FileChunk struct {
+		Pieces [][]Piece
+	}
+)
+
+// NewFromFileData creates a new SiaFile from a FileData object that was
+// previously created from a legacy file.
+func NewFromFileData(fd FileData) *SiaFile {
+	file := &SiaFile{
+		metadata: Metadata{
+			fileSize:  int64(fd.FileSize),
+			masterKey: fd.MasterKey,
+			mode:      fd.Mode,
+			pieceSize: fd.PieceSize,
+			siaPath:   fd.Name,
+		},
+		deleted:     fd.Deleted,
+		erasureCode: fd.ErasureCode,
+		uid:         fd.UID,
+	}
+	chunks := make([]Chunk, file.NumChunks())
+	for i := range chunks {
+		chunks[i].erasureCodeType = [4]byte{0, 0, 0, 1}
+		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[0:4], uint32(file.erasureCode.MinPieces()))
+		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[4:8], uint32(file.erasureCode.NumPieces()-file.erasureCode.MinPieces()))
+		chunks[i].pieces = make([][]Piece, file.erasureCode.NumPieces())
+	}
+	file.chunks = chunks
+
+	// Populate the pubKeyTable of the file and add the pieces.
+	pubKeyMap := make(map[string]int)
+	for chunkIndex, chunk := range fd.Chunks {
+		for pieceIndex, pieceSet := range chunk.Pieces {
+			for _, piece := range pieceSet {
+				// Check if we already added that public key.
+				if _, exists := pubKeyMap[string(piece.HostPubKey.Key)]; !exists {
+					pubKeyMap[string(piece.HostPubKey.Key)] = len(file.pubKeyTable)
+					file.pubKeyTable = append(file.pubKeyTable, piece.HostPubKey)
+				}
+				// Add the piece to the SiaFile.
+				file.chunks[chunkIndex].pieces[pieceIndex] = append(file.chunks[chunkIndex].pieces[pieceIndex], Piece{
+					HostPubKey: piece.HostPubKey,
+					MerkleRoot: piece.MerkleRoot,
+				})
+			}
+		}
+	}
+	return file
+}
+
+// ExportFileData creates a FileData object from a SiaFile that can be used to
+// convert the file into a legacy file.
+func (sf *SiaFile) ExportFileData() FileData {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	fd := FileData{
+		Name:        sf.metadata.siaPath,
+		FileSize:    uint64(sf.metadata.fileSize),
+		MasterKey:   sf.metadata.masterKey,
+		ErasureCode: sf.erasureCode,
+		PieceSize:   sf.metadata.pieceSize,
+		Mode:        sf.metadata.mode,
+		Deleted:     sf.deleted,
+		UID:         sf.uid,
+	}
+	// Return a deep-copy to avoid race conditions.
+	fd.Chunks = make([]FileChunk, len(sf.chunks))
+	for chunkIndex := range fd.Chunks {
+		fd.Chunks[chunkIndex].Pieces = make([][]Piece, len(sf.chunks[chunkIndex].pieces))
+		for pieceIndex := range fd.Chunks[chunkIndex].Pieces {
+			fd.Chunks[chunkIndex].Pieces[pieceIndex] = make([]Piece, len(sf.chunks[chunkIndex].pieces[pieceIndex]))
+			copy(fd.Chunks[chunkIndex].Pieces[pieceIndex], sf.chunks[chunkIndex].pieces[pieceIndex])
+		}
+	}
+	return fd
+}

SiaFoundation/siad

modules/renter/files.go
@@ -5,9 +5,11 @@ import (
 	"path/filepath"
 	"sync"
 
+	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
+	"gitlab.com/NebulousLabs/Sia/persist"
 	"gitlab.com/NebulousLabs/Sia/types"
 
 	"gitlab.com/NebulousLabs/errors"
@@ -83,11 +85,20 @@ func (r *Renter) DeleteFile(nickname string) error {
 	delete(r.files, nickname)
 	delete(r.persist.Tracking, nickname)
 
+	err := persist.RemoveFile(filepath.Join(r.persistDir, f.SiaPath()+ShareExtension))
+	if err != nil {
+		r.log.Println("WARN: couldn't remove file :", err)
+	}
+
 	r.saveSync()
 	r.mu.Unlock(lockID)
 
+	// mark the file as deleted
+	f.Delete()
+
 	// TODO: delete the sectors of the file as well.
-	return errors.AddContext(f.Delete(), "failed to delete file")
+
+	return nil
 }
 
 // FileList returns all of the files that the renter has.
@@ -249,12 +260,97 @@ func (r *Renter) RenameFile(currentName, newName string) error {
 
 // fileToSiaFile converts a legacy file to a SiaFile. Fields that can't be
 // populated using the legacy file remain blank.
-func fileToSiaFile(f *file) *siafile.SiaFile {
-	panic("not implemented yet")
+func (r *Renter) fileToSiaFile(f *file) *siafile.SiaFile {
+	fileData := siafile.FileData{
+		Name:        f.name,
+		FileSize:    f.size,
+		MasterKey:   f.masterKey,
+		ErasureCode: f.erasureCode,
+		PieceSize:   f.pieceSize,
+		Mode:        os.FileMode(f.mode),
+		Deleted:     f.deleted,
+		UID:         f.staticUID,
+	}
+	chunks := make([]siafile.FileChunk, f.numChunks())
+	for i := 0; i < len(chunks); i++ {
+		chunks[i].Pieces = make([][]siafile.Piece, f.erasureCode.NumPieces())
+	}
+	for _, contract := range f.contracts {
+		pk := r.hostContractor.ResolveIDToPubKey(contract.ID)
+		for _, piece := range contract.Pieces {
+			chunks[piece.Chunk].Pieces[piece.Piece] = append(chunks[piece.Chunk].Pieces[piece.Piece], siafile.Piece{
+				HostPubKey: pk,
+				MerkleRoot: piece.MerkleRoot,
+			})
+		}
+	}
+	fileData.Chunks = chunks
+	return siafile.NewFromFileData(fileData)
 }
 
 // siaFileToFile converts a SiaFile to a legacy file. Fields that don't exist
 // in the legacy file will get lost and therefore not persisted.
-func siaFileToFile(sf *siafile.SiaFile) *file {
-	panic("not implemented yet")
+func (r *Renter) siaFileToFile(sf *siafile.SiaFile) *file {
+	fileData := sf.ExportFileData()
+	f := &file{
+		contracts:   make(map[types.FileContractID]fileContract),
+		name:        fileData.Name,
+		size:        fileData.FileSize,
+		masterKey:   fileData.MasterKey,
+		erasureCode: fileData.ErasureCode,
+		pieceSize:   fileData.PieceSize,
+		mode:        uint32(fileData.Mode),
+		deleted:     fileData.Deleted,
+		staticUID:   fileData.UID,
+	}
+	for chunkIndex, chunk := range fileData.Chunks {
+		for pieceIndex, pieceSet := range chunk.Pieces {
+			for _, piece := range pieceSet {
+				c, ok := r.hostContractor.ContractByPublicKey(piece.HostPubKey)
+				if !ok {
+					build.Critical("missing contract when converting SiaFile to file")
+					continue
+				}
+				h, ok := r.hostDB.Host(piece.HostPubKey)
+				if !ok {
+					build.Critical("missing host when converting SiaFile to file")
+					continue
+				}
+				if _, exists := f.contracts[c.ID]; !exists {
+					f.contracts[c.ID] = fileContract{
+						ID:          c.ID,
+						IP:          h.NetAddress,
+						WindowStart: c.EndHeight,
+					}
+				}
+				fc := f.contracts[c.ID]
+				fc.Pieces = append(fc.Pieces, pieceData{
+					Chunk:      uint64(chunkIndex),
+					Piece:      uint64(pieceIndex),
+					MerkleRoot: piece.MerkleRoot,
+				})
+				f.contracts[c.ID] = fc
+			}
+		}
+	}
+	return f
+}
+
+// numChunks returns the number of chunks that f was split into.
+func (f *file) numChunks() uint64 {
+	// empty files still need at least one chunk
+	if f.size == 0 {
+		return 1
+	}
+	n := f.size / f.staticChunkSize()
+	// last chunk will be padded, unless chunkSize divides file evenly.
+	if f.size%f.staticChunkSize() != 0 {
+		n++
+	}
+	return n
+}
+
+// staticChunkSize returns the size of one chunk.
+func (f *file) staticChunkSize() uint64 {
+	return f.pieceSize * uint64(f.erasureCode.MinPieces())
 }

modules/renter/persist.go
@@ -10,10 +10,12 @@ import (
 	"path/filepath"
 	"strconv"
 
+	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/encoding"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
 	"gitlab.com/NebulousLabs/Sia/persist"
+	"gitlab.com/NebulousLabs/Sia/types"
 )
 
 const (
@@ -57,6 +59,121 @@ type (
 	}
 )
 
+// MarshalSia implements the encoding.SiaMarshaller interface, writing the
+// file data to w.
+func (f *file) MarshalSia(w io.Writer) error {
+	enc := encoding.NewEncoder(w)
+
+	// encode easy fields
+	err := enc.EncodeAll(
+		f.name,
+		f.size,
+		f.masterKey,
+		f.pieceSize,
+		f.mode,
+	)
+	if err != nil {
+		return err
+	}
+	// COMPATv0.4.3 - encode the bytesUploaded and chunksUploaded fields
+	// TODO: the resulting .sia file may confuse old clients.
+	err = enc.EncodeAll(f.pieceSize*f.numChunks()*uint64(f.erasureCode.NumPieces()), f.numChunks())
+	if err != nil {
+		return err
+	}
+
+	// encode erasureCode
+	switch code := f.erasureCode.(type) {
+	case *rsCode:
+		err = enc.EncodeAll(
+			"Reed-Solomon",
+			uint64(code.dataPieces),
+			uint64(code.numPieces-code.dataPieces),
+		)
+		if err != nil {
+			return err
+		}
+	default:
+		if build.DEBUG {
+			panic("unknown erasure code")
+		}
+		return errors.New("unknown erasure code")
+	}
+	// encode contracts
+	if err := enc.Encode(uint64(len(f.contracts))); err != nil {
+		return err
+	}
+	for _, c := range f.contracts {
+		if err := enc.Encode(c); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// UnmarshalSia implements the encoding.SiaUnmarshaller interface,
+// reconstructing a file from the encoded bytes read from r.
+func (f *file) UnmarshalSia(r io.Reader) error {
+	dec := encoding.NewDecoder(r)
+
+	// COMPATv0.4.3 - decode bytesUploaded and chunksUploaded into dummy vars.
+	var bytesUploaded, chunksUploaded uint64
+
+	// Decode easy fields.
+	err := dec.DecodeAll(
+		&f.name,
+		&f.size,
+		&f.masterKey,
+		&f.pieceSize,
+		&f.mode,
+		&bytesUploaded,
+		&chunksUploaded,
+	)
+	if err != nil {
+		return err
+	}
+	f.staticUID = persist.RandomSuffix()
+
+	// Decode erasure coder.
+	var codeType string
+	if err := dec.Decode(&codeType); err != nil {
+		return err
+	}
+	switch codeType {
+	case "Reed-Solomon":
+		var nData, nParity uint64
+		err = dec.DecodeAll(
+			&nData,
+			&nParity,
+		)
+		if err != nil {
+			return err
+		}
+		rsc, err := NewRSCode(int(nData), int(nParity))
+		if err != nil {
+			return err
+		}
+		f.erasureCode = rsc
+	default:
+		return errors.New("unrecognized erasure code type: " + codeType)
+	}
+
+	// Decode contracts.
+	var nContracts uint64
+	if err := dec.Decode(&nContracts); err != nil {
+		return err
+	}
+	f.contracts = make(map[types.FileContractID]fileContract)
+	var contract fileContract
+	for i := uint64(0); i < nContracts; i++ {
+		if err := dec.Decode(&contract); err != nil {
+			return err
+		}
+		f.contracts[contract.ID] = contract
+	}
+	return nil
+}
+
 // saveFile saves a file to the renter directory.
 func (r *Renter) saveFile(f *siafile.SiaFile) error {
 	if f.Deleted() { // TODO: violation of locking convention
@@ -77,7 +194,7 @@ func (r *Renter) saveFile(f *siafile.SiaFile) error {
 	defer handle.Close()
 
 	// Write file data.
-	err = shareFiles([]*siafile.SiaFile{f}, handle)
+	err = r.shareFiles([]*siafile.SiaFile{f}, handle)
 	if err != nil {
 		return err
 	}
@@ -162,11 +279,11 @@ func (r *Renter) loadSettings() error {
 
 // shareFiles writes the specified files to w. First a header is written,
 // followed by the gzipped concatenation of each file.
-func shareFiles(siaFiles []*siafile.SiaFile, w io.Writer) error {
+func (r *Renter) shareFiles(siaFiles []*siafile.SiaFile, w io.Writer) error {
 	// Convert files to old type.
 	files := make([]*file, 0, len(siaFiles))
 	for _, sf := range siaFiles {
-		files = append(files, siaFileToFile(sf))
+		files = append(files, r.siaFileToFile(sf))
 	}
 	// Write header.
 	err := encoding.NewEncoder(w).EncodeAll(
@@ -219,7 +336,7 @@ func (r *Renter) ShareFiles(nicknames []string, shareDest string) error {
 		files[i] = f
 	}
 
-	err = shareFiles(files, handle)
+	err = r.shareFiles(files, handle)
 	if err != nil {
 		os.Remove(shareDest)
 		return err
@@ -244,7 +361,7 @@ func (r *Renter) ShareFilesASCII(nicknames []string) (string, error) {
 	}
 
 	buf := new(bytes.Buffer)
-	err := shareFiles(files, base64.NewEncoder(base64.URLEncoding, buf))
+	err := r.shareFiles(files, base64.NewEncoder(base64.URLEncoding, buf))
 	if err != nil {
 		return "", err
 	}
@@ -304,12 +421,12 @@ func (r *Renter) loadSharedFiles(reader io.Reader) ([]string, error) {
 	// Add files to renter.
 	names := make([]string, numFiles)
 	for i, f := range files {
-		r.files[f.name] = fileToSiaFile(f)
+		r.files[f.name] = r.fileToSiaFile(f)
 		names[i] = f.name
 	}
 	// Save the files.
 	for _, f := range files {
-		r.saveFile(fileToSiaFile(f))
+		r.saveFile(r.fileToSiaFile(f))
 	}
 
 	return names, nil

modules/renter/siafile/compat.go
@@ -1,0 +1,102 @@
+package siafile
+
+import (
+	"encoding/binary"
+	"os"
+
+	"gitlab.com/NebulousLabs/Sia/crypto"
+	"gitlab.com/NebulousLabs/Sia/modules"
+)
+
+type (
+	// FileData is a helper struct that contains all the relevant information
+	// of a file. It simplifies passing the necessary data between modules and
+	// keeps the interface clean.
+	FileData struct {
+		Name        string
+		FileSize    uint64
+		MasterKey   crypto.TwofishKey
+		ErasureCode modules.ErasureCoder
+		PieceSize   uint64
+		Mode        os.FileMode
+		Deleted     bool
+		UID         string
+		Chunks      []FileChunk
+	}
+	// FileChunk is a helper struct that contains data about a chunk.
+	FileChunk struct {
+		Pieces [][]Piece
+	}
+)
+
+// NewFromFileData creates a new SiaFile from a FileData object that was
+// previously created from a legacy file.
+func NewFromFileData(fd FileData) *SiaFile {
+	file := &SiaFile{
+		metadata: Metadata{
+			fileSize:  int64(fd.FileSize),
+			masterKey: fd.MasterKey,
+			mode:      fd.Mode,
+			pieceSize: fd.PieceSize,
+			siaPath:   fd.Name,
+		},
+		deleted:     fd.Deleted,
+		erasureCode: fd.ErasureCode,
+		uid:         fd.UID,
+	}
+	chunks := make([]Chunk, file.NumChunks())
+	for i := range chunks {
+		chunks[i].erasureCodeType = [4]byte{0, 0, 0, 1}
+		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[0:4], uint32(file.erasureCode.MinPieces()))
+		binary.LittleEndian.PutUint32(chunks[i].erasureCodeParams[4:8], uint32(file.erasureCode.NumPieces()-file.erasureCode.MinPieces()))
+		chunks[i].pieces = make([][]Piece, file.erasureCode.NumPieces())
+	}
+	file.chunks = chunks
+
+	// Populate the pubKeyTable of the file and add the pieces.
+	pubKeyMap := make(map[string]int)
+	for chunkIndex, chunk := range fd.Chunks {
+		for pieceIndex, pieceSet := range chunk.Pieces {
+			for _, piece := range pieceSet {
+				// Check if we already added that public key.
+				if _, exists := pubKeyMap[string(piece.HostPubKey.Key)]; !exists {
+					pubKeyMap[string(piece.HostPubKey.Key)] = len(file.pubKeyTable)
+					file.pubKeyTable = append(file.pubKeyTable, piece.HostPubKey)
+				}
+				// Add the piece to the SiaFile.
+				file.chunks[chunkIndex].pieces[pieceIndex] = append(file.chunks[chunkIndex].pieces[pieceIndex], Piece{
+					HostPubKey: piece.HostPubKey,
+					MerkleRoot: piece.MerkleRoot,
+				})
+			}
+		}
+	}
+	return file
+}
+
+// ExportFileData creates a FileData object from a SiaFile that can be used to
+// convert the file into a legacy file.
+func (sf *SiaFile) ExportFileData() FileData {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	fd := FileData{
+		Name:        sf.metadata.siaPath,
+		FileSize:    uint64(sf.metadata.fileSize),
+		MasterKey:   sf.metadata.masterKey,
+		ErasureCode: sf.erasureCode,
+		PieceSize:   sf.metadata.pieceSize,
+		Mode:        sf.metadata.mode,
+		Deleted:     sf.deleted,
+		UID:         sf.uid,
+	}
+	// Return a deep-copy to avoid race conditions.
+	fd.Chunks = make([]FileChunk, len(sf.chunks))
+	for chunkIndex := range fd.Chunks {
+		fd.Chunks[chunkIndex].Pieces = make([][]Piece, len(sf.chunks[chunkIndex].pieces))
+		for pieceIndex := range fd.Chunks[chunkIndex].Pieces {
+			fd.Chunks[chunkIndex].Pieces[pieceIndex] = make([]Piece, len(sf.chunks[chunkIndex].pieces[pieceIndex]))
+			copy(fd.Chunks[chunkIndex].Pieces[pieceIndex], sf.chunks[chunkIndex].pieces[pieceIndex])
+		}
+	}
+	return fd
+}