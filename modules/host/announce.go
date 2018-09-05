faa9e865bb23265fd71e1b36bd5c67908cc14ef8
Chris <chris@sia.tech>
2018-09-05 12:41:06 -0400

    fix comparison

--- modules/host/announce.go
+++ modules/host/announce.go
@@ -1,8 +1,8 @@
 package host
 
 import (
-	"bytes"
 	"fmt"
+	"net"
 
 	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/modules"
@@ -18,6 +18,18 @@ var (
 	// public address for itself to use in the announcement.
 	errUnknownAddress = errors.New("host cannot announce, does not seem to have a valid address")
 )
+
+// differentTypeIPs is a helper that returns true if two IPs are of a different
+// type.
+func differentTypeIPs(ip1, ip2 net.IP) bool {
+	if ip1.To4() != nil && ip2.To4() == nil {
+		return true
+	}
+	if ip2.To4() != nil && ip1.To4() == nil {
+		return true
+	}
+	return false
+}
 
 // staticVerifyAnnouncementAddress checks that the address is sane and not local.
 func (h *Host) staticVerifyAnnouncementAddress(addr modules.NetAddress) error {
@@ -37,8 +49,8 @@ func (h *Host) staticVerifyAnnouncementAddress(addr modules.NetAddress) error {
 	if len(ips) < 1 {
 		return fmt.Errorf("host %s doesn't resolve to any IP addresses", addr.Host())
 	}
-	// During testing we allow loopback IPs.
-	isLoopback := ips[0].IsLoopback() && ips[1].IsLoopback() && !bytes.Equal(ips[0], ips[1])
+	// During testing we allow loopback IPs if they are of different types.
+	isLoopback := ips[0].IsLoopback() && ips[1].IsLoopback() && differentTypeIPs(ips[0], ips[1])
 	if len(ips) == 2 && len(ips[0]) == len(ips[1]) && !isLoopback {
 		return fmt.Errorf("host %s resolves to 2 IPs of the same type", addr.Host())
 	}
