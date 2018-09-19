366a313fd27ff5c04917c324b4a35df85eb019ef
Chris <chris@sia.tech>
2018-09-19 11:03:13 -0400

    create testfiles and destinations with spaces in siatest package

--- siatest/localfile.go
+++ siatest/localfile.go
@@ -25,7 +25,7 @@ type (
 // NewFile creates and returns a new LocalFile. It will write size random bytes
 // to the file and give the file a random name.
 func (tn *TestNode) NewFile(size int) (*LocalFile, error) {
-	fileName := fmt.Sprintf("%dbytes-%s", size, hex.EncodeToString(fastrand.Bytes(4)))
+	fileName := fmt.Sprintf("%dbytes %s", size, hex.EncodeToString(fastrand.Bytes(4)))
 	path := filepath.Join(tn.filesDir(), fileName)
 	bytes := fastrand.Bytes(size)
 	err := ioutil.WriteFile(path, bytes, 0600)
--- siatest/renter.go
+++ siatest/renter.go
@@ -23,7 +23,7 @@ func (tn *TestNode) DownloadToDisk(rf *RemoteFile, async bool) (*LocalFile, erro
 		return nil, errors.AddContext(err, "failed to retrieve FileInfo")
 	}
 	// Create a random destination for the download
-	fileName := fmt.Sprintf("%dbytes-%s", fi.Filesize, hex.EncodeToString(fastrand.Bytes(4)))
+	fileName := fmt.Sprintf("%dbytes %s", fi.Filesize, hex.EncodeToString(fastrand.Bytes(4)))
 	dest := filepath.Join(tn.downloadsDir(), fileName)
 	if err := tn.RenterDownloadGet(rf.siaPath, dest, 0, fi.Filesize, async); err != nil {
 		return nil, errors.AddContext(err, "failed to download file")
