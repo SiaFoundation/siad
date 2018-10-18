b80bf7ffa152bbc77a56739b3f718bcf1c65255c
Chris <chris@sia.tech>
2018-10-18 15:37:12 -0400

    Fix failing loadSafeContract for interrupted writes

--- modules/renter/proto/contract_test.go
+++ modules/renter/proto/contract_test.go
@@ -80,8 +80,10 @@ func TestContractUncommittedTxn(t *testing.T) {
 		t.Fatal("Merkle roots should match initial Merkle roots")
 	}
 
-	// close and reopen the contract set
-	cs.Close()
+	// close and reopen the contract set.
+	if err := cs.Close(); err != nil {
+		t.Fatal(err)
+	}
 	cs, err = NewContractSet(dir, modules.ProdDependencies)
 	if err != nil {
 		t.Fatal(err)
@@ -123,4 +125,108 @@ func TestContractUncommittedTxn(t *testing.T) {
 	} else if !reflect.DeepEqual(merkleRoots, revisedRoots) {
 		t.Fatal("Merkle roots should match revised Merkle roots")
 	}
+}
+
+// TestContractIncompleteWrite tests that if the merkle root section has the wrong
+// length due to an incomplete write, it is truncated and the wal transactions
+// are applied.
+func TestContractIncompleteWrite(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	// create contract set with one contract
+	dir := build.TempDir(filepath.Join("proto", t.Name()))
+	cs, err := NewContractSet(dir, modules.ProdDependencies)
+	if err != nil {
+		t.Fatal(err)
+	}
+	initialHeader := contractHeader{
+		Transaction: types.Transaction{
+			FileContractRevisions: []types.FileContractRevision{{
+				NewRevisionNumber:    1,
+				NewValidProofOutputs: []types.SiacoinOutput{{}, {}},
+				UnlockConditions: types.UnlockConditions{
+					PublicKeys: []types.SiaPublicKey{{}, {}},
+				},
+			}},
+		},
+	}
+	initialRoots := []crypto.Hash{{1}}
+	c, err := cs.managedInsertContract(initialHeader, initialRoots)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// apply an update to the contract, but don't commit it
+	sc := cs.mustAcquire(t, c.ID)
+	revisedHeader := contractHeader{
+		Transaction: types.Transaction{
+			FileContractRevisions: []types.FileContractRevision{{
+				NewRevisionNumber:    2,
+				NewValidProofOutputs: []types.SiacoinOutput{{}, {}},
+				UnlockConditions: types.UnlockConditions{
+					PublicKeys: []types.SiaPublicKey{{}, {}},
+				},
+			}},
+		},
+		StorageSpending: types.NewCurrency64(7),
+		UploadSpending:  types.NewCurrency64(17),
+	}
+	revisedRoots := []crypto.Hash{{1}, {2}}
+	fcr := revisedHeader.Transaction.FileContractRevisions[0]
+	newRoot := revisedRoots[1]
+	storageCost := revisedHeader.StorageSpending.Sub(initialHeader.StorageSpending)
+	bandwidthCost := revisedHeader.UploadSpending.Sub(initialHeader.UploadSpending)
+	_, err = sc.recordUploadIntent(fcr, newRoot, storageCost, bandwidthCost)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// the state of the contract should match the initial state
+	// NOTE: can't use reflect.DeepEqual for the header because it contains
+	// types.Currency fields
+	merkleRoots, err := sc.merkleRoots.merkleRoots()
+	if err != nil {
+		t.Fatal("failed to get merkle roots", err)
+	}
+	if !bytes.Equal(encoding.Marshal(sc.header), encoding.Marshal(initialHeader)) {
+		t.Fatal("contractHeader should match initial contractHeader")
+	} else if !reflect.DeepEqual(merkleRoots, initialRoots) {
+		t.Fatal("Merkle roots should match initial Merkle roots")
+	}
+
+	// get the size of the merkle roots file.
+	size, err := sc.merkleRoots.rootsFile.Size()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// the size should be crypto.HashSize since we have exactly one root.
+	if size != crypto.HashSize {
+		t.Fatal("unexpected merkle root file size", size)
+	}
+	// truncate the rootsFile to simulate a corruption while writing the second
+	// root.
+	err = sc.merkleRoots.rootsFile.Truncate(size + crypto.HashSize/2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// close and reopen the contract set.
+	if err := cs.Close(); err != nil {
+		t.Fatal(err)
+	}
+	cs, err = NewContractSet(dir, modules.ProdDependencies)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// the uncommitted txn should be gone.
+	sc = cs.mustAcquire(t, c.ID)
+	if len(sc.unappliedTxns) != 0 {
+		t.Fatal("expected 0 unappliedTxn, got", len(sc.unappliedTxns))
+	}
+	if sc.merkleRoots.len() != 2 {
+		t.Fatal("expected 2 roots, got", sc.merkleRoots.len())
+	}
+	cs.Return(sc)
+	cs.Close()
 }
