6279e776a23962c64b1fd626df8b570f3efea9b1
Chris <chris@sia.tech>
2018-12-06 07:30:39 -0500

    move comment out of imports

--- modules/gateway/gateway.go
+++ modules/gateway/gateway.go
@@ -5,6 +5,92 @@
 // mined or about transactions that you have created.
 package gateway
 
+// For the user to be securely connected to the network, the user must be
+// connected to at least one node which will send them all of the blocks. An
+// attacker can trick the user into thinking that a different blockchain is the
+// full blockchain if the user is not connected to any nodes who are seeing +
+// broadcasting the real chain (and instead is connected only to attacker nodes
+// or to nodes that are not broadcasting). This situation is called an eclipse
+// attack.
+//
+// Connecting to a large number of nodes increases the resiliancy of the
+// network, but also puts a networking burden on the nodes and can slow down
+// block propagation or increase orphan rates. The gateway's job is to keep the
+// network efficient while also protecting the user against attacks.
+//
+// The gateway keeps a list of nodes that it knows about. It uses this list to
+// form connections with other nodes, and then uses those connections to
+// participate in the flood network. The primary vector for an attacker to
+// achieve an eclipse attack is node list domination. If a gateway's nodelist
+// is heavily dominated by attacking nodes, then when the gateway chooses to
+// make random connections the gateway is at risk of selecting only attacker
+// nodes.
+//
+// The gateway defends itself from these attacks by minimizing the amount of
+// control that an attacker has over the node list and peer list. The first
+// major defense is that the gateway maintains 8 'outbound' relationships,
+// which means that the gateway created those relationships instead of an
+// attacker. If a node forms a connection to you, that node is called
+// 'inbound', and because it may be an attacker node, it is not trusted.
+// Outbound nodes can also be attacker nodes, but they are less likely to be
+// attacker nodes because you chose them, instead of them choosing you.
+//
+// If the gateway forms too many connections, the gateway will allow incoming
+// connections by kicking an existing peer. But, to limit the amount of control
+// that an attacker may have, only inbound peers are selected to be kicked.
+// Furthermore, to increase the difficulty of attack, if a new inbound
+// connection shares the same IP address as an existing connection, the shared
+// connection is the connection that gets dropped (unless that connection is a
+// local or outbound connection).
+//
+// Nodes are added to a peerlist in two methods. The first method is that a
+// gateway will ask its outbound peers for a list of nodes. If the node list is
+// below a certain size (see consts.go), the gateway will repeatedly ask
+// outbound peers to expand the list. Nodes are also added to the nodelist
+// after they successfully form a connection with the gateway. To limit the
+// attacker's ability to add nodes to the nodelist, connections are
+// ratelimited. An attacker with lots of IP addresses still has the ability to
+// fill up the nodelist, however getting 90% dominance of the nodelist requires
+// forming thousands of connections, which will take hours or days. By that
+// time, the attacked node should already have its set of outbound peers,
+// limiting the amount of damage that the attacker can do.
+//
+// To limit DNS-based tomfoolry, nodes are only added to the nodelist if their
+// connection information takes the form of an IP address.
+//
+// Some research has been done on Bitcoin's flood networks. The more relevant
+// research has been listed below. The papers listed first are more relevant.
+//     Eclipse Attacks on Bitcoin's Peer-to-Peer Network (Heilman, Kendler, Zohar, Goldberg)
+//     Stubborn Mining: Generalizing Selfish Mining and Combining with an Eclipse Attack (Nayak, Kumar, Miller, Shi)
+//     An Overview of BGP Hijacking (https://www.bishopfox.com/blog/2015/08/an-overview-of-bgp-hijacking/)
+
+// TODO: Currently the gateway does not do much in terms of bucketing. The
+// gateway should make sure that it has outbound peers from a wide range of IP
+// addresses, and when kicking inbound peers it shouldn't just favor kicking
+// peers of the same IP address, it should favor kicking peers of the same ip
+// address range.
+//
+// TODO: There is no public key exchange, so communications cannot be
+// effectively encrypted or authenticated.
+//
+// TODO: Gateway hostname discovery currently has significant centralization,
+// namely the fallback is a single third-party website that can easily form any
+// response it wants. Instead, multiple TLS-protected third party websites
+// should be used, and the plurality answer should be accepted as the true
+// hostname.
+//
+// TODO: The gateway currently does hostname discovery in a non-blocking way,
+// which means that the first few peers that it connects to may not get the
+// correct hostname. This means that you may give the remote peer the wrong
+// hostname, which means they will not be able to dial you back, which means
+// they will not add you to their node list.
+//
+// TODO: The gateway should encrypt and authenticate all communications. Though
+// the gateway participates in a flood network, practical attacks have been
+// demonstrated which have been able to confuse nodes by manipulating messages
+// from their peers. Encryption + authentication would have made the attack
+// more difficult.
+
 import (
 	"fmt"
 	"net"
@@ -16,92 +102,6 @@ import (
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/persist"
 	"gitlab.com/NebulousLabs/ratelimit"
-
-	// For the user to be securely connected to the network, the user must be
-	// connected to at least one node which will send them all of the blocks. An
-	// attacker can trick the user into thinking that a different blockchain is the
-	// full blockchain if the user is not connected to any nodes who are seeing +
-	// broadcasting the real chain (and instead is connected only to attacker nodes
-	// or to nodes that are not broadcasting). This situation is called an eclipse
-	// attack.
-	//
-	// Connecting to a large number of nodes increases the resiliancy of the
-	// network, but also puts a networking burden on the nodes and can slow down
-	// block propagation or increase orphan rates. The gateway's job is to keep the
-	// network efficient while also protecting the user against attacks.
-	//
-	// The gateway keeps a list of nodes that it knows about. It uses this list to
-	// form connections with other nodes, and then uses those connections to
-	// participate in the flood network. The primary vector for an attacker to
-	// achieve an eclipse attack is node list domination. If a gateway's nodelist
-	// is heavily dominated by attacking nodes, then when the gateway chooses to
-	// make random connections the gateway is at risk of selecting only attacker
-	// nodes.
-	//
-	// The gateway defends itself from these attacks by minimizing the amount of
-	// control that an attacker has over the node list and peer list. The first
-	// major defense is that the gateway maintains 8 'outbound' relationships,
-	// which means that the gateway created those relationships instead of an
-	// attacker. If a node forms a connection to you, that node is called
-	// 'inbound', and because it may be an attacker node, it is not trusted.
-	// Outbound nodes can also be attacker nodes, but they are less likely to be
-	// attacker nodes because you chose them, instead of them choosing you.
-	//
-	// If the gateway forms too many connections, the gateway will allow incoming
-	// connections by kicking an existing peer. But, to limit the amount of control
-	// that an attacker may have, only inbound peers are selected to be kicked.
-	// Furthermore, to increase the difficulty of attack, if a new inbound
-	// connection shares the same IP address as an existing connection, the shared
-	// connection is the connection that gets dropped (unless that connection is a
-	// local or outbound connection).
-	//
-	// Nodes are added to a peerlist in two methods. The first method is that a
-	// gateway will ask its outbound peers for a list of nodes. If the node list is
-	// below a certain size (see consts.go), the gateway will repeatedly ask
-	// outbound peers to expand the list. Nodes are also added to the nodelist
-	// after they successfully form a connection with the gateway. To limit the
-	// attacker's ability to add nodes to the nodelist, connections are
-	// ratelimited. An attacker with lots of IP addresses still has the ability to
-	// fill up the nodelist, however getting 90% dominance of the nodelist requires
-	// forming thousands of connections, which will take hours or days. By that
-	// time, the attacked node should already have its set of outbound peers,
-	// limiting the amount of damage that the attacker can do.
-	//
-	// To limit DNS-based tomfoolry, nodes are only added to the nodelist if their
-	// connection information takes the form of an IP address.
-	//
-	// Some research has been done on Bitcoin's flood networks. The more relevant
-	// research has been listed below. The papers listed first are more relevant.
-	//     Eclipse Attacks on Bitcoin's Peer-to-Peer Network (Heilman, Kendler, Zohar, Goldberg)
-	//     Stubborn Mining: Generalizing Selfish Mining and Combining with an Eclipse Attack (Nayak, Kumar, Miller, Shi)
-	//     An Overview of BGP Hijacking (https://www.bishopfox.com/blog/2015/08/an-overview-of-bgp-hijacking/)
-
-	// TODO: Currently the gateway does not do much in terms of bucketing. The
-	// gateway should make sure that it has outbound peers from a wide range of IP
-	// addresses, and when kicking inbound peers it shouldn't just favor kicking
-	// peers of the same IP address, it should favor kicking peers of the same ip
-	// address range.
-	//
-	// TODO: There is no public key exchange, so communications cannot be
-	// effectively encrypted or authenticated.
-	//
-	// TODO: Gateway hostname discovery currently has significant centralization,
-	// namely the fallback is a single third-party website that can easily form any
-	// response it wants. Instead, multiple TLS-protected third party websites
-	// should be used, and the plurality answer should be accepted as the true
-	// hostname.
-	//
-	// TODO: The gateway currently does hostname discovery in a non-blocking way,
-	// which means that the first few peers that it connects to may not get the
-	// correct hostname. This means that you may give the remote peer the wrong
-	// hostname, which means they will not be able to dial you back, which means
-	// they will not add you to their node list.
-	//
-	// TODO: The gateway should encrypt and authenticate all communications. Though
-	// the gateway participates in a flood network, practical attacks have been
-	// demonstrated which have been able to confuse nodes by manipulating messages
-	// from their peers. Encryption + authentication would have made the attack
-	// more difficult.
 
 	siasync "gitlab.com/NebulousLabs/Sia/sync"
 	"gitlab.com/NebulousLabs/errors"
