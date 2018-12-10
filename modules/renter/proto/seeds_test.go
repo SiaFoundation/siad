0755fa4b80b7e520214ad2307f8da9eaed3bc584
Chris <chris@sia.tech>
2018-12-10 10:16:17 -0500

    fix test

--- modules/renter/proto/seeds_test.go
+++ modules/renter/proto/seeds_test.go
@@ -18,7 +18,7 @@ func TestEphemeralRenterSeed(t *testing.T) {
 
 	// Test for blockheights 0 to ephemeralSeedInterval-1
 	for bh := types.BlockHeight(0); bh < ephemeralSeedInterval; bh++ {
-		expectedSeed := crypto.HashAll(walletSeed, "renter", 0)
+		expectedSeed := crypto.HashAll(walletSeed, renterSeedSpecifier, 0)
 		seed := EphemeralRenterSeed(walletSeed, bh)
 		if !bytes.Equal(expectedSeed[:], seed[:]) {
 			t.Fatal("Seeds don't match for blockheight", bh)
@@ -26,7 +26,7 @@ func TestEphemeralRenterSeed(t *testing.T) {
 	}
 	// Test for blockheights ephemeralSeedInterval to 2*ephemeralSeedInterval-1
 	for bh := ephemeralSeedInterval; bh < 2*ephemeralSeedInterval; bh++ {
-		expectedSeed := crypto.HashAll(walletSeed, "renter", 1)
+		expectedSeed := crypto.HashAll(walletSeed, renterSeedSpecifier, 1)
 		seed := EphemeralRenterSeed(walletSeed, bh)
 		if !bytes.Equal(expectedSeed[:], seed[:]) {
 			t.Fatal("Seeds don't match for blockheight", bh)
