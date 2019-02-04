a19bb22be0dad389c42ce1a467f3cd2296fd1066
Matthew Sevey <mjsevey@gmail.com>
2019-02-04 14:26:14 -0500

    renter: add unit testing to probe use of recent repair time

--- modules/renter/uploadheap_test.go
+++ modules/renter/uploadheap_test.go
@@ -1,12 +1,14 @@
 package renter
 
 import (
+	"strconv"
 	"testing"
 
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/modules/renter/siafile"
 	"gitlab.com/NebulousLabs/Sia/types"
+	"gitlab.com/NebulousLabs/fastrand"
 )
 
 // TestBuildUnfinishedChunks probes buildUnfinishedChunks to make sure that the
@@ -71,5 +73,81 @@ func TestBuildUnfinishedChunks(t *testing.T) {
 		if !c.stuck {
 			t.Fatal("Found unstuck chunk when expecting only stuck chunks")
 		}
+	}
+}
+
+// TestBuildChunkHeap probes managedBuildChunkHeap to make sure that the correct
+// chunks are being added to the heap
+func TestBuildChunkHeap(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	// Create Renter
+	rt, err := newRenterTester(t.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create 2 files
+	rsc, _ := siafile.NewRSCode(1, 1)
+	up := modules.FileUploadParams{
+		Source:      "",
+		SiaPath:     "testfile-" + strconv.Itoa(int(fastrand.Bytes(8)[0])),
+		ErasureCode: rsc,
+	}
+	f1, err := rt.renter.staticFileSet.NewSiaFile(up, crypto.GenerateSiaKey(crypto.RandomCipherType()), 10e3, 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+	up.SiaPath = "testfile-" + strconv.Itoa(int(fastrand.Bytes(8)[0]))
+	f2, err := rt.renter.staticFileSet.NewSiaFile(up, crypto.GenerateSiaKey(crypto.RandomCipherType()), 10e3, 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Manually add workers to worker pool and create host map
+	hosts := make(map[string]struct{})
+	for i := 0; i < int(f1.NumChunks()+f2.NumChunks()); i++ {
+		rt.renter.workerPool[types.FileContractID{byte(i)}] = &worker{
+			downloadChan: make(chan struct{}, 1),
+			killChan:     make(chan struct{}),
+			uploadChan:   make(chan struct{}, 1),
+		}
+	}
+
+	// Call managedBuildChunkHeap as not stuck loop, the heap should have a
+	// length equal to the number of chunks in both of the files
+	rt.renter.managedBuildChunkHeap("", hosts, targetUnstuckChunks)
+	if len(rt.renter.uploadHeap.heap) != int(f1.NumChunks()+f2.NumChunks()) {
+		t.Fatalf("Expected heap length of %v but got %v", int(f1.NumChunks()+f2.NumChunks()), len(rt.renter.uploadHeap.heap))
+	}
+
+	// Reset upload heap
+	rt.renter.uploadHeap.activeChunks = make(map[uploadChunkID]struct{})
+	rt.renter.uploadHeap.heap = uploadChunkHeap{}
+
+	// Set the first file's RecentRepairTime to now
+	if err := f1.UpdateRecentRepairTime(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Call managedBuildChunkHeap as not stuck loop, the heap should have a
+	// length equal to the number of chunks in only the second file
+	rt.renter.managedBuildChunkHeap("", hosts, targetUnstuckChunks)
+	if len(rt.renter.uploadHeap.heap) != int(f2.NumChunks()) {
+		t.Fatalf("Expected heap length of %v but got %v", int(f2.NumChunks()), len(rt.renter.uploadHeap.heap))
+	}
+
+	// Reset upload heap
+	rt.renter.uploadHeap.activeChunks = make(map[uploadChunkID]struct{})
+	rt.renter.uploadHeap.heap = uploadChunkHeap{}
+
+	// Call managedBuildChunkHeap as stuck loop, the heap should have a length
+	// of zero because neither file is stuck
+	rt.renter.managedBuildChunkHeap("", hosts, targetStuckChunks)
+	if len(rt.renter.uploadHeap.heap) != 0 {
+		t.Fatalf("Expected heap length of %v but got %v", 0, len(rt.renter.uploadHeap.heap))
 	}
 }
