4306eb68b113b35da6a0c3b424cfb11905f43f77
Matthew Sevey <mjsevey@gmail.com>
2019-02-21 15:38:13 -0500

    siatest: move siatest around to eliminate ndfs in test groups

--- siatest/renter/stream_test.go
+++ siatest/renter/stream_test.go
@@ -3,9 +3,12 @@ package renter
 import (
 	"bytes"
 	"testing"
+	"time"
 
 	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/siatest"
+	"gitlab.com/NebulousLabs/errors"
 
 	"gitlab.com/NebulousLabs/fastrand"
 )
@@ -148,6 +151,155 @@ func TestRenterDownloadStreamCache(t *testing.T) {
 		}
 		if bytes.Compare(streamedPartialData, downloadedData[from:to]) != 0 {
 			t.Error("Read range returned the wrong data")
+		}
+	}
+}
+
+// TestRenterStream executes a number of subtests using the same TestGroup to
+// save time on initialization
+func TestRenterStream(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	// Create a group for the subtests
+	groupParams := siatest.GroupParams{
+		Hosts:   5,
+		Renters: 1,
+		Miners:  1,
+	}
+
+	// Specify subtests to run
+	subTests := []test{
+		{"TestStreamingCache", testStreamingCache},
+		{"TestStreamLargeFile", testStreamLargeFile},
+	}
+
+	// Run tests
+	if err := runRenterTests(t, groupParams, subTests); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// testStreamingCache checks if the chunk cache works correctly.
+func testStreamingCache(t *testing.T, tg *siatest.TestGroup) {
+	t.Skip("Caching is broken due to partial downloads")
+	// Grab the first of the group's renters
+	r := tg.Renters()[0]
+
+	// Testing setting StreamCacheSize for streaming
+	// Test setting it to larger than the defaultCacheSize
+	if err := r.RenterSetStreamCacheSizePost(4); err != nil {
+		t.Fatal(err, "Could not set StreamCacheSize to 4")
+	}
+	rg, err := r.RenterGet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rg.Settings.StreamCacheSize != 4 {
+		t.Fatal("StreamCacheSize not set to 4, set to", rg.Settings.StreamCacheSize)
+	}
+
+	// Test resetting to the value of defaultStreamCacheSize (2)
+	if err := r.RenterSetStreamCacheSizePost(2); err != nil {
+		t.Fatal(err, "Could not set StreamCacheSize to 2")
+	}
+	rg, err = r.RenterGet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rg.Settings.StreamCacheSize != 2 {
+		t.Fatal("StreamCacheSize not set to 2, set to", rg.Settings.StreamCacheSize)
+	}
+
+	prev := rg.Settings.StreamCacheSize
+
+	// Test setting to 0
+	if err := r.RenterSetStreamCacheSizePost(0); err == nil {
+		t.Fatal(err, "expected setting stream cache size to zero to fail with an error")
+	}
+	rg, err = r.RenterGet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rg.Settings.StreamCacheSize == 0 {
+		t.Fatal("StreamCacheSize set to 0, should have stayed as previous value or", prev)
+	}
+
+	// Set fileSize and redundancy for upload
+	ct := crypto.TypeDefaultRenter
+	dataPieces := uint64(1)
+	parityPieces := uint64(len(tg.Hosts())) - dataPieces
+
+	// Set the bandwidth limit to 1 chunk per second.
+	chunkSize := int64(siatest.ChunkSize(dataPieces, ct))
+	if err := r.RenterPostRateLimit(chunkSize, chunkSize); err != nil {
+		t.Fatal(err)
+	}
+
+	rg, err = r.RenterGet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rg.Settings.MaxDownloadSpeed != chunkSize {
+		t.Fatal(errors.New("MaxDownloadSpeed doesn't match value set through RenterPostRateLimit"))
+	}
+	if rg.Settings.MaxUploadSpeed != chunkSize {
+		t.Fatal(errors.New("MaxUploadSpeed doesn't match value set through RenterPostRateLimit"))
+	}
+
+	// Upload a file that is a single chunk big.
+	_, remoteFile, err := r.UploadNewFileBlocking(int(chunkSize), dataPieces, parityPieces, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Download the same chunk 250 times. This should take at least 250 seconds
+	// without caching but not more than 30 with caching.
+	start := time.Now()
+	for i := 0; i < 250; i++ {
+		if _, err := r.Stream(remoteFile); err != nil {
+			t.Fatal(err)
+		}
+		if time.Since(start) > time.Second*30 {
+			t.Fatal("download took longer than 30 seconds")
+		}
+	}
+	// Make sure that the stream downloads don't show up in the download
+	// history.
+	rgg, err := r.RenterDownloadsGet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, download := range rgg.Downloads {
+		if download.DestinationType == "httpseekstream" {
+			t.Fatal("Stream downloads shouldn't be added to the history")
+		}
+	}
+}
+
+// testStreamLargeFile tests that using the streaming endpoint to download
+// multiple chunks works.
+func testStreamLargeFile(t *testing.T, tg *siatest.TestGroup) {
+	// Grab the first of the group's renters
+	renter := tg.Renters()[0]
+	// Upload file, creating a piece for each host in the group
+	dataPieces := uint64(1)
+	parityPieces := uint64(len(tg.Hosts())) - dataPieces
+	ct := crypto.TypeDefaultRenter
+	fileSize := int(10 * siatest.ChunkSize(dataPieces, ct))
+	localFile, remoteFile, err := renter.UploadNewFileBlocking(fileSize, dataPieces, parityPieces, false)
+	if err != nil {
+		t.Fatal("Failed to upload a file for testing: ", err)
+	}
+	// Stream the file partially a few times. At least 1 byte is streamed.
+	for i := 0; i < 5; i++ {
+		from := fastrand.Intn(fileSize - 1)             // [0..fileSize-2]
+		to := from + 1 + fastrand.Intn(fileSize-from-1) // [from+1..fileSize-1]
+		_, err = renter.StreamPartial(remoteFile, localFile, uint64(from), uint64(to))
+		if err != nil {
+			t.Fatal(err)
 		}
 	}
 }
