2e76a326483b3edd68a66e18270bba87379aca14
Chris Schinnerl <chris@sia.tech>
2019-04-16 14:16:59 +0200

    fix erasure coder

--- modules/renter/uploadstreamer.go
+++ modules/renter/uploadstreamer.go
@@ -167,6 +167,15 @@ func (r *Renter) UploadStreamFromReader(up modules.FileUploadParams, reader io.R
 // SiaFile for the upload.
 func (r *Renter) managedInitUploadStream(up modules.FileUploadParams) (*siafile.SiaFileSetEntry, error) {
 	siaPath, ec, force := up.SiaPath, up.ErasureCode, up.Force
+	// Check if ec was set. If not use defaults.
+	var err error
+	if ec == nil {
+		up.ErasureCode, err = siafile.NewRSSubCode(defaultDataPieces, defaultParityPieces, 64)
+		if err != nil {
+			return nil, err
+		}
+		ec = up.ErasureCode
+	}
 
 	// Delete existing file if overwrite flag is set. Ignore ErrUnknownPath.
 	if force {
