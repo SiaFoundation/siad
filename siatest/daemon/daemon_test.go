44a5f2befce6ed9810b71264fb8eefdef711528c
Chris Schinnerl <chris@sia.tech>
2019-04-30 11:25:32 +0200

    add vlong test

--- siatest/daemon/daemon_test.go
+++ siatest/daemon/daemon_test.go
@@ -2,7 +2,10 @@ package daemon
 
 import (
 	"testing"
+	"time"
 
+	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/node"
 	"gitlab.com/NebulousLabs/Sia/siatest"
 )
@@ -63,5 +66,80 @@ func TestDaemonRatelimit(t *testing.T) {
 	if dsg.MaxDownloadSpeed != ds || dsg.MaxUploadSpeed != us {
 		t.Fatalf("Limits should be %v/%v but are %v/%v",
 			ds, us, dsg.MaxDownloadSpeed, dsg.MaxUploadSpeed)
+	}
+}
+
+// TestGlobalRatelimitRenter makes sure that if multiple ratelimits are set, the
+// lower one is respected.
+func TestGlobalRatelimitRenter(t *testing.T) {
+	if !build.VLONG || testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	// Create a testgroup,
+	groupParams := siatest.GroupParams{
+		Hosts:   2,
+		Renters: 1,
+		Miners:  1,
+	}
+	testDir := daemonTestDir(t.Name())
+	tg, err := siatest.NewGroupFromTemplate(testDir, groupParams)
+	if err != nil {
+		t.Fatal("Failed to create group: ", err)
+	}
+	defer func() {
+		if err := tg.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// Grab Renter and upload file
+	r := tg.Renters()[0]
+	dataPieces := uint64(1)
+	parityPieces := uint64(1)
+	chunkSize := int64(siatest.ChunkSize(dataPieces, crypto.TypeDefaultRenter))
+	expectedSeconds := 10
+	fileSize := expectedSeconds * int(chunkSize)
+	_, rf, err := r.UploadNewFileBlocking(fileSize, dataPieces, parityPieces, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Set the bandwidth limit to 10 chunks per second.
+	if err := r.RenterPostRateLimit(10*chunkSize, 10*chunkSize); err != nil {
+		t.Fatal(err)
+	}
+	// Set the daemon's limit to 1 chunk per second.
+	if err := r.DaemonGlobalRateLimitPost(chunkSize, chunkSize); err != nil {
+		t.Fatal(err)
+	}
+	// Download the file. It should take at least expectedSeconds seconds.
+	start := time.Now()
+	if _, err := r.DownloadByStream(rf); err != nil {
+		t.Fatal(err)
+	}
+	timePassed := time.Since(start)
+	if timePassed < time.Second*time.Duration(expectedSeconds) {
+		t.Fatalf("download took %v but should've been at least %v",
+			timePassed, time.Second*time.Duration(expectedSeconds))
+	}
+	// Swap the limits and make sure the limit is still in effect.
+	if err := r.RenterPostRateLimit(chunkSize, chunkSize); err != nil {
+		t.Fatal(err)
+	}
+	// Set the daemon's limit to 1 chunk per second.
+	if err := r.DaemonGlobalRateLimitPost(10*chunkSize, 10*chunkSize); err != nil {
+		t.Fatal(err)
+	}
+	// Download the file. It should take at least expectedSeconds seconds.
+	start = time.Now()
+	if _, err := r.DownloadByStream(rf); err != nil {
+		t.Fatal(err)
+	}
+	timePassed = time.Since(start)
+	if timePassed < time.Second*time.Duration(expectedSeconds) {
+		t.Fatalf("download took %v but should've been at least %v",
+			timePassed, time.Second*time.Duration(expectedSeconds))
 	}
 }
