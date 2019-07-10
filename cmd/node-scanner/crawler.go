572373ed6e92954d1253896bf8f841a1caec19cc
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2019-07-10 15:53:37 -0400

    Write results to tmp files in JSON

--- cmd/node-scanner/crawler.go
+++ cmd/node-scanner/crawler.go
@@ -1,37 +1,80 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/encoding"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/modules/gateway"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
 type NodeScanner struct {
+	//The node scanner uses a dummy gateway to connect to nodes and
+	//		requests peers from nodes across the network using the
+	//		ShareNodes RPC.
 	gateway *gateway.Gateway
 
+	// Multiple workers are given addresses to scan using workCh.
+	// The workers always send a result back to the main goroutine
+	// using the resultCh
 	workCh   chan WorkAssignment
 	resultCh chan ShareNodesResult
 
-	queue []modules.NetAddress
-	seen  map[modules.NetAddress]struct{}
+	/*
+		Count the total number of work assignments sent down workCh
+		 and the total number of results received through resultCh.
+		 Since every work assignment sent always sends a result back
+		 (even in case of failure), the main goroutine can tell if the
+		 node scan has finished by checking that:
+			- there are no assignments outstanding in workCh
+			- there are no unprocessed results in resultCh
+			- there are no unassigned addresses in queue
+			- all workers are done with their assignements (totalWorkAssignments = totalResults)
+	*/
+	totalWorkAssignments int
+	totalResults         int
 
-	// Stats
+	// The seen set keeps track of all the addresses seen by the
+	// scanner so far.
+	seen map[modules.NetAddress]struct{}
+	// The queue holds nodes to be added to workCh.
+	queue []modules.NetAddress
+
+	// Counters for successful/unsuccesful connections.
 	successfulConns int
 	failConns       int
-	oldVersionFails int
-	// TODO: file to store other stats
+
+	// Counters for common failure types.
+	unacceptableVersions  int
+	networkIsUnreachables int
+	noRouteToHosts        int
+	connectionRefuseds    int
+
+	// scanFile holds all the results for this scan.
+	scanFile *os.File
+	// encoder is used to store NodeScanResult structs
+	// as JSON objects in the scanFile.
+	encoder *json.Encoder
 }
 
+// WorkAssignment tells a worker which node it should scan,
+// and the number of times it should send the ShareNodes RPC.
+// The ShareNodes RPC is used multiple times because nodes will
+// only return 10 random peers, but we want as many as possible.
 type WorkAssignment struct {
 	node           modules.NetAddress
 	maxRPCAttempts int
 }
 
+// ShareNodesResult gives the set of nodes received from ShareNodes
+// RPCs sent to a specific node. err is nil, an error from connecting,
+// or an error from ShareNodes.
 type ShareNodesResult struct {
 	node  modules.NetAddress
 	err   error
@@ -40,9 +83,9 @@ type ShareNodesResult struct {
 
 // NodeScanResult stores the data we got from the scan of a single address.
 type NodeScanResult struct {
-	addr      modules.NetAddress
-	timestamp time.Time
-	err       error
+	Addr      modules.NetAddress
+	Timestamp int64
+	Err       string
 }
 
 const maxSharedNodes = uint64(1000)
@@ -65,16 +108,16 @@ func main() {
 	ticker := time.NewTicker(1 * time.Second)
 	numRPCAttempts := 3
 
-	// TODO: fix this hacky end condition
-	checkTime := time.Now().Add(15 * time.Minute)
-
 	for {
 		select {
 		case <-ticker.C:
-			log.Printf("MAIN LOOP:\n\t SeenLen: %d\n\tQueueLen: %d, WorkCh len: %d ResultCh len: %d\n", len(ns.seen), len(ns.queue), len(ns.workCh), len(ns.resultCh))
-			fmt.Printf("\nConnection counts: Success: %d All fails: %d, Old version: %d\n", ns.successfulConns, ns.failConns, ns.oldVersionFails)
+			fmt.Printf("Seen: %d,  Queued: %d, In WorkCh: %d, In ResultCh :%d\n", len(ns.seen), len(ns.queue), len(ns.workCh), len(ns.resultCh))
+			fmt.Printf("Number assigned: %d, Number of results :%d\n", ns.totalWorkAssignments, ns.totalResults)
+			fmt.Printf("Successful Connections: %d, Unsuccesful: %d\n\t(unacceptableVersions: %d, networkIsUnreachables: %d, noRouteToHosts: %d, connectionRefuseds: %d)\n\n", ns.successfulConns, ns.failConns, ns.unacceptableVersions, ns.networkIsUnreachables, ns.noRouteToHosts, ns.connectionRefuseds)
 
 		case res := <-ns.resultCh:
+			ns.totalResults++
+
 			// Add any new nodes from this set of results.
 			for node := range res.nodes {
 				if _, alreadySeen := ns.seen[node]; !alreadySeen {
@@ -94,6 +137,7 @@ func main() {
 				break
 			}
 			node, ns.queue = ns.queue[len(ns.queue)-1], ns.queue[:len(ns.queue)-1]
+			ns.totalWorkAssignments++
 			ns.workCh <- WorkAssignment{
 				node:           node,
 				maxRPCAttempts: numRPCAttempts,
@@ -101,10 +145,11 @@ func main() {
 		}
 
 		// Check ending condition.
-		// TODO: this is wrong... but mostly works.
-		if len(ns.queue) == 0 && len(ns.workCh) == 0 && len(ns.resultCh) == 0 && time.Now().After(checkTime) {
-			log.Printf("MAIN LOOP:\n\t SeenLen: %d\n\tQueueLen: %d, WorkCh len: %d ResultCh len: %d\n", len(ns.seen), len(ns.queue), len(ns.workCh), len(ns.resultCh))
-			fmt.Printf("\nConnection counts: Success: %d All fails: %d, Old version: %d\n", ns.successfulConns, ns.failConns, ns.oldVersionFails)
+		if (len(ns.workCh) == 0) && (len(ns.resultCh) == 0) && (len(ns.queue) == 0) && (ns.totalWorkAssignments == ns.totalResults) {
+			fmt.Printf("Seen: %d,  Queued: %d, In WorkCh: %d, In ResultCh :%d\n", len(ns.seen), len(ns.queue), len(ns.workCh), len(ns.resultCh))
+			fmt.Printf("Number assigned: %d, Number of results :%d\n", ns.totalWorkAssignments, ns.totalResults)
+			fmt.Printf("Successful Connections: %d, Unsuccesful: %d\n\t(unacceptableVersions: %d, networkIsUnreachables: %d, noRouteToHosts: %d, connectionRefuseds: %d)\n\n", ns.successfulConns, ns.failConns, ns.unacceptableVersions, ns.networkIsUnreachables, ns.noRouteToHosts, ns.connectionRefuseds)
+			ns.scanFile.Close()
 			return
 		}
 	}
@@ -117,6 +162,33 @@ func main() {
 // ShareNodes RPC.
 func setupNodeScanner(addr modules.NetAddress) (ns *NodeScanner) {
 	ns = new(NodeScanner)
+
+	// Setup the node scanner's directories.
+	scannerDirPath := filepath.Join(os.TempDir(), "SiaNodeScanner")
+	scannerGatewayDirPath := filepath.Join(scannerDirPath, "gateway")
+	if _, err := os.Stat(scannerDirPath); os.IsNotExist(err) {
+		err := os.Mkdir(scannerDirPath, 0777)
+		if err != nil {
+			log.Fatal("Error creating scan directory: ", err)
+		}
+	}
+	if _, err := os.Stat(scannerGatewayDirPath); os.IsNotExist(err) {
+		err := os.Mkdir(scannerGatewayDirPath, 0777)
+		if err != nil {
+			log.Fatal("Error creating scanner gateway directory: ", err)
+		}
+	}
+	log.Println(scannerDirPath)
+
+	// Create the file for this scan.
+	startTime := time.Now().Format("01-02:15:04")
+	scanFileName := scannerDirPath + "/scan-" + startTime + ".json"
+	scanFile, err := os.Create(scanFileName)
+	if err != nil {
+		log.Fatal("Error creating scan file: ", err)
+	}
+	ns.scanFile = scanFile
+	ns.encoder = json.NewEncoder(ns.scanFile)
 
 	// Create dummy gateway at localhost.
 	g, err := gateway.New("localhost:0", true, build.TempDir("node-scanner")) // TODO: make own dir
@@ -154,6 +226,7 @@ func setupNodeScanner(addr modules.NetAddress) (ns *NodeScanner) {
 	ns.resultCh = make(chan ShareNodesResult, workChSize)
 
 	for _, node := range ns.queue {
+		ns.totalWorkAssignments++
 		ns.workCh <- WorkAssignment{
 			node:           node,
 			maxRPCAttempts: maxRPCs,
@@ -166,37 +239,47 @@ func setupNodeScanner(addr modules.NetAddress) (ns *NodeScanner) {
 }
 
 func (ns *NodeScanner) logWorkerResult(res ShareNodesResult) {
+	scanRes := NodeScanResult{
+		Addr:      res.node,
+		Timestamp: time.Now().Unix(),
+		Err:       "",
+	}
+	if res.err != nil {
+		scanRes.Err = res.err.Error()
+	}
+	err := ns.encoder.Encode(scanRes)
+	if err != nil {
+		log.Println("Error writing NodeScanResult to file! - ", err)
+	}
+
 	if res.err == nil {
-		// TODO: log IP, timestamp, etc. in file.
 		log.Printf("success %s\n", res.node)
 		ns.successfulConns++
 		return
 	}
+
 	ns.failConns++
+	log.Printf("Cannot connect to local node at address %s: %s\n", res.node, res.err)
 
 	if strings.Contains(res.err.Error(), "unacceptable version") {
+		ns.unacceptableVersions++
+	} else if strings.Contains(res.err.Error(), "unreachable") {
+		ns.networkIsUnreachables++
+	} else if strings.Contains(res.err.Error(), "no route to host") {
+		ns.noRouteToHosts++
+	} else if strings.Contains(res.err.Error(), "connection refused") {
+		ns.connectionRefuseds++
+	} else {
 		log.Printf("Cannot connect to local node at address %s: %s\n", res.node, res.err)
-		ns.oldVersionFails++
 	}
-
-	/*
-
-	   TODO: add more failure types:
-	   	- "network is unreachable"
-	   	- "no route to host"
-	   	- "connection refused"
-	*/
-
 }
 
+// startWorker starts a worker that continually receives from the workCh,
+// connect to the node it has been assigned, and returns all results
+// using resultCh.
 func startWorker(g *gateway.Gateway, workCh <-chan WorkAssignment, resultCh chan<- ShareNodesResult) {
 	for {
-		work, ok := <-workCh
-		if !ok {
-			fmt.Println("ayy", len(workCh))
-			return
-		}
-		fmt.Printf("\taccepted work: %s\n", work.node)
+		work := <-workCh
 
 		// Try connecting to the node at this address.
 		// If the connection fails, return the error message.
@@ -240,6 +323,5 @@ func sendShareNodesRequests(g *gateway.Gateway, work WorkAssignment) ShareNodesR
 		time.Sleep(timeBetweenRequests)
 	}
 
-	fmt.Printf("\tWorker found %d peers\n", len(result.nodes))
 	return result
 }
