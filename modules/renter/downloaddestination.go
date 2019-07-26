00406b3eb3eff34d6d4dbcde6022d780c9d288bf
Chris Schinnerl <chris@sia.tech>
2019-07-26 18:14:47 +0200

    use sectionwriter instead of mutex

--- modules/renter/downloaddestination.go
+++ modules/renter/downloaddestination.go
@@ -46,6 +46,34 @@ func (sw *skipWriter) Write(p []byte) (int, error) {
 	return n, err
 }
 
+// NewSectionWriter returns a SectionWriter that writes to w
+// starting at offset off and stops with EOF after n bytes.
+func NewSectionWriter(w io.WriterAt, off int64, n int64) *SectionWriter {
+	return &SectionWriter{w, off, off, off + n}
+}
+
+// SectionWriter implements Write on a section
+// of an underlying WriterAt.
+type SectionWriter struct {
+	w     io.WriterAt
+	base  int64
+	off   int64
+	limit int64
+}
+
+// Write implements the io.Writer interface using WriteAt.
+func (s *SectionWriter) Write(p []byte) (n int, err error) {
+	if s.off >= s.limit {
+		return 0, io.EOF
+	}
+	if max := s.limit - s.off; int64(len(p)) > max {
+		p = p[0:max]
+	}
+	n, err = s.w.WriteAt(p, s.off)
+	s.off += int64(n)
+	return
+}
+
 // downloadDestination is the interface that receives the data recovered by the
 // download process. The call to WritePieces is in `threadedRecoverLogicalData`.
 //
@@ -84,8 +112,8 @@ func (dw *downloadDestinationBuffer) WritePieces(_ modules.ErasureCoder, pieces 
 
 // downloadDestinationFile wraps an os.File into a downloadDestination.
 type downloadDestinationFile struct {
-	mu sync.Mutex
-	f  *os.File
+	staticChunkSize int64
+	f               *os.File
 }
 
 // Close implements the io.Closer interface for downloadDestinationFile.
@@ -96,12 +124,8 @@ func (ddf *downloadDestinationFile) Close() error {
 // WritePieces will decode the pieces and write them to a file at the provided
 // offset, using the provided length.
 func (ddf *downloadDestinationFile) WritePieces(ec modules.ErasureCoder, pieces [][]byte, dataOffset uint64, offset int64, length uint64) error {
-	ddf.mu.Lock()
-	defer ddf.mu.Unlock()
-	if _, err := ddf.f.Seek(offset, io.SeekStart); err != nil {
-		return err
-	}
-	return ec.Recover(pieces, dataOffset+length, &skipWriter{w: ddf.f, skip: int(dataOffset)})
+	sw := NewSectionWriter(ddf.f, offset, ddf.staticChunkSize)
+	return ec.Recover(pieces, dataOffset+length, &skipWriter{w: sw, skip: int(dataOffset)})
 }
 
 // downloadDestinationWriter is a downloadDestination that writes to an
