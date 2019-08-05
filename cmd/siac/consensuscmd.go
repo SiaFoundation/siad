a9e200142bd2ed4cd9877eef31e4f9a150c54809
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2019-08-05 19:24:34 +0000

    Apply suggestion to cmd/siac/consensuscmd.go

--- cmd/siac/consensuscmd.go
+++ cmd/siac/consensuscmd.go
@@ -28,7 +28,7 @@ func consensuscmd() {
 	cg, err := httpClient.ConsensusGet()
 	if errors.Contains(err, api.ErrAPICallNotRecognized) {
 		// Assume module is not loaded if status command is not recognized.
-		fmt.Printf("Consensus: \n  Status: %s\n\n", moduleNotReadyStatus)
+		fmt.Printf("Consensus:\n  Status: %s\n\n", moduleNotReadyStatus)
 		return
 	} else if err != nil {
 		die("Could not get current consensus state:", err)
