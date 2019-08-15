02108196a9d0c9a4064f0ff11d6de81dbd8cc607
Matthew Sevey <mjsevey@gmail.com>
2019-08-15 17:47:57 -0400

    add simple siatest

---
+++ siatest/transactionpool/tpool_test.go
@@ -0,0 +1,71 @@
+package transactionpool
+
+import (
+	"path/filepath"
+	"testing"
+
+	"gitlab.com/NebulousLabs/Sia/siatest"
+	"gitlab.com/NebulousLabs/Sia/types"
+)
+
+func TestTpoolTransactionSetGet(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	// Create testing directory.
+	testdir := tpoolTestDir(t.Name())
+
+	// Create a miners
+	miner, err := siatest.NewNode(siatest.Miner(filepath.Join(testdir, "miner")))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := miner.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// Transaction set should be empty to start
+	tpsg, err := miner.TransactionPoolSetsGET()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tpsg.TransactionSets) != 0 {
+		t.Fatal("expected no transaction sets got", len(tpsg.TransactionSets))
+	}
+
+	// miner sends a txn to itself and mines it.
+	uc, err := miner.WalletAddressGet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = miner.WalletSiacoinsPost(types.SiacoinPrecision, uc.Address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Verify the transaction set
+	tpsg, err = miner.TransactionPoolSetsGET()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tpsg.TransactionSets) != 1 {
+		t.Fatal("expected 1 transaction set got", len(tpsg.TransactionSets))
+	}
+
+	// Mine a block to confirm the transaction
+	if err := miner.MineBlock(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Transaction set should now be empty
+	tpsg, err = miner.TransactionPoolSetsGET()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tpsg.TransactionSets) != 0 {
+		t.Fatal("expected no transaction sets got", len(tpsg.TransactionSets))
+	}
+}
