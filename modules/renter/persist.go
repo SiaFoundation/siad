bbd97914acd90df7d0a4dc3a50830714b03e44cf
Chris Schinnerl <chris@sia.tech>
2019-08-16 15:36:36 +0200

    Cleanly shut down wal and log on renter shutdown

--- modules/renter/persist.go
+++ modules/renter/persist.go
@@ -133,6 +133,9 @@ func (r *Renter) managedInitPersist() error {
 	if err != nil {
 		return err
 	}
+	if err := r.tg.AfterStop(r.log.Close); err != nil {
+		return err
+	}
 
 	// Initialize the writeaheadlog.
 	options := writeaheadlog.Options{
@@ -141,6 +144,9 @@ func (r *Renter) managedInitPersist() error {
 	}
 	txns, wal, err := writeaheadlog.NewWithOptions(options)
 	if err != nil {
+		return err
+	}
+	if err := r.tg.AfterStop(wal.Close); err != nil {
 		return err
 	}
 
