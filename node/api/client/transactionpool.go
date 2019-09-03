887b7c0bb6a132a5e1e7f63c40f071c5ba670a77
Matthew Sevey <mjsevey@gmail.com>
2019-09-03 14:52:14 +0000

    Apply suggestion to node/api/client/transactionpool.go

--- node/api/client/transactionpool.go
+++ node/api/client/transactionpool.go
@@ -27,7 +27,7 @@ func (c *Client) TransactionPoolRawPost(txn types.Transaction, parents []types.T
 
 // TransactionPoolTxnsGET uses the /tpool/transactions endpoint to get the
 // transactions of the tpool
-func (c *Client) TransactionPoolTxnsGET() (tptg api.TpoolTxnsGET, err error) {
+func (c *Client) TransactionPoolTransactionnsGet() (tptg api.TpoolTxnsGET, err error) {
 	err = c.get("/tpool/transactions", &tptg)
 	return
 }
