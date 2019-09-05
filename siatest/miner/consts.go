c60357a0d1b6caf210b06e528e7f6c5df1ec8ee1
Chris <chris@sia.tech>
2019-09-05 16:16:30 +0200

    add endpoint to submit solved block

---
+++ siatest/miner/consts.go
@@ -0,0 +1,18 @@
+package miner
+
+import (
+	"os"
+
+	"gitlab.com/NebulousLabs/Sia/siatest"
+)
+
+// minerTestDir creates a temporary testing directory for a miner test. This
+// should only every be called once per test. Otherwise it will delete the
+// directory again.
+func minerTestDir(testName string) string {
+	path := siatest.TestDir("miner", testName)
+	if err := os.MkdirAll(path, 0777); err != nil {
+		panic(err)
+	}
+	return path
+}
