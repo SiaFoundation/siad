e1670075345108a8d506046f62a4056ece73b0aa
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2019-11-05 17:06:38 -0500

    Add managedCanChurn unit tests

---
+++ modules/renter/contractor/churnlimiter_test.go
@@ -0,0 +1,80 @@
+package contractor
+
+import (
+	"testing"
+
+	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/types"
+)
+
+// contractWithSize is a helper function that creates a dummy file contract with a certain size.
+func contractWithSize(size uint64) modules.RenterContract {
+	txn := types.Transaction{
+		FileContractRevisions: []types.FileContractRevision{{NewFileSize: size}},
+	}
+	return modules.RenterContract{Transaction: txn}
+}
+
+// TestCanChurnContract tests the functionality of managedCanChurnContract
+func TestCanChurnContract(t *testing.T) {
+	// This method doesn't use the contractor so this is safe.
+	cl := newChurnLimiter(nil)
+	cl.maxChurnPerPeriod = 1000
+
+	// Test: Not enough remainingChurnBudget
+	cl.remainingChurnBudget = 499
+	cl.aggregateChurnThisPeriod = 0
+	ok := cl.managedCanChurnContract(contractWithSize(500))
+	if ok {
+		t.Fatal("Expected not to be able to churn contract")
+	}
+
+	// Test: just enough remainingChurnBudget.
+	cl.remainingChurnBudget = 500
+	cl.aggregateChurnThisPeriod = 0
+	ok = cl.managedCanChurnContract(contractWithSize(500))
+	if !ok {
+		t.Fatal("Expected to be able to churn contract")
+	}
+
+	// Test: not enough period budget.
+	cl.remainingChurnBudget = 500
+	cl.aggregateChurnThisPeriod = 501
+	ok = cl.managedCanChurnContract(contractWithSize(500))
+	if ok {
+		t.Fatal("Expected not to be able to churn contract")
+	}
+
+	// Test: just enough period budget.
+	cl.remainingChurnBudget = 500
+	cl.aggregateChurnThisPeriod = 500
+	ok = cl.managedCanChurnContract(contractWithSize(500))
+	if !ok {
+		t.Fatal("Expected to be able to churn contract")
+	}
+
+	// Test: can churn contract bigger than remainingChurnBudget as long as
+	// remainingChurnBudget is max and churn fits in period budget.
+	cl.remainingChurnBudget = 500
+	cl.aggregateChurnThisPeriod = 1
+	ok = cl.managedCanChurnContract(contractWithSize(999))
+	if !ok {
+		t.Fatal("Expected to be able to churn contract")
+	}
+
+	// Test: can churn any size contract if no aggregateChurn in period and max remainingChurnBudget.
+	cl.remainingChurnBudget = 500
+	cl.aggregateChurnThisPeriod = 0
+	ok = cl.managedCanChurnContract(contractWithSize(999999999999999999))
+	if !ok {
+		t.Fatal("Expected to be able to churn contract")
+	}
+
+	// Test: avoid churning big contracts if any aggregate churn in the period was found
+	cl.remainingChurnBudget = 1000
+	cl.aggregateChurnThisPeriod = 1
+	ok = cl.managedCanChurnContract(contractWithSize(999999999999999999))
+	if ok {
+		t.Fatal("Expected not to be able to churn contract")
+	}
+}
