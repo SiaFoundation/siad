aa769c6667e19332a95c352fd48d77e2287b5665
PJ <peterjan.brone@gmail.com>
2019-12-11 22:14:17 +0100

    Fix MR against master

--- analysis/cmd/analyze.go
+++ analysis/cmd/analyze.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"gitlab.com/NebulousLabs/Sia/analysis/lockcheck"
 	"gitlab.com/NebulousLabs/Sia/analysis/responsewritercheck"
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
 func main() {
 	multichecker.Main(
-		lockcheck.Analyzer,
 		responsewritercheck.Analyzer,
 	)
 }
