cb530892ef7bdd1dcf4884800b8645f1c14f95c2
David Vorick <david@nebulouslabs.com>
2020-01-10 05:24:10 -0500

    fix misspelling

--- modules/renter/streambuffer.go
+++ modules/renter/streambuffer.go
@@ -127,7 +127,7 @@ type streamBuffer struct {
 	dataSections map[uint64]*dataSection
 
 	// externRefCount is in the same consistency domain as the streamBufferSet,
-	// it needs to be incremented and decremented simulatenously with the
+	// it needs to be incremented and decremented simultaneously with the
 	// creation and deletion of the streamBuffer.
 	externRefCount uint64
 
