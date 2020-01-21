47f5091c662015b0b61a4e595563b648fbe953bf
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2020-01-21 13:27:57 -0500

    Add linux/arm64 binaries to release script

--- release.sh
+++ release.sh
@@ -14,10 +14,13 @@ fi
 # setup build-time vars
 ldflags="-s -w -X 'gitlab.com/NebulousLabs/Sia/build.GitRevision=`git rev-parse --short HEAD`' -X 'gitlab.com/NebulousLabs/Sia/build.BuildTime=`date`' -X 'gitlab.com/NebulousLabs/Sia/build.ReleaseTag=${rc}'"
 
-for os in darwin linux windows; do
+function build {
+  os=$1
+  arch=$2
+
 	echo Building ${os}...
 	# create workspace
-	folder=release/Sia-$version-$os-amd64
+	folder=release/Sia-$version-$os-$arch
 	rm -rf $folder
 	mkdir -p $folder
 	# compile and hash binaries
@@ -26,21 +29,39 @@ for os in darwin linux windows; do
 		if [ "$os" == "windows" ]; then
 			bin=${pkg}.exe
 		fi
-		GOOS=${os} go build -a -tags 'netgo' -trimpath -ldflags="$ldflags" -o $folder/$bin ./cmd/$pkg
+		GOOS=${os} GOARCH=${arch} go build -a -tags 'netgo' -trimpath -ldflags="$ldflags" -o $folder/$bin ./cmd/$pkg
     sha256sum $folder/$bin >> release/Sia-$version-SHA256SUMS.txt
-
   done
+}
+
+# Build amd64 binaries.
+for os in darwin linux windows; do
+  build "$os" "amd64"
 done
 
-for os in darwin linux windows; do
+# Build Raspberry Pi binaries.
+build "linux" "arm64"
+
+function package {
+  os=$1
+  arch=$2
+
 	echo Packaging ${os}...
-	folder=release/Sia-$version-$os-amd64
+	folder=release/Sia-$version-$os-$arch
 
 	# add other artifacts and file of hashes.
 	cp -r release/Sia-$version-SHA256SUMS.txt doc LICENSE README.md $folder
 	# zip
 	(
 		cd release
-		zip -rq Sia-$version-$os-amd64.zip Sia-$version-$os-amd64
+		zip -rq Sia-$version-$os-$arch.zip Sia-$version-$os-$arch
 	)
+}
+
+# Package amd64 binaries.
+for os in darwin linux windows; do
+  package "$os" "amd64"
 done
+
+# Pacakge Raspberry Pi binaries.
+package "linux" "arm64"
