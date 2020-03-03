e1bf4a2dda89d0557cf06cf9fa06569c8edb9734
PJ <peterjan.brone@gmail.com>
2020-03-03 14:49:49 +0100

    Move all MDM cost related consts and functions to the modules package

--- modules/mdm.go
+++ modules/mdm.go
@@ -2,6 +2,7 @@ package modules
 
 import (
 	"encoding/binary"
+	"errors"
 
 	"gitlab.com/NebulousLabs/Sia/types"
 )
@@ -17,16 +18,28 @@ type (
 	InstructionSpecifier types.Specifier
 )
 
-// MDM instruction cost component specifiers
-var (
-	MDMComponentCompute    = types.NewSpecifier("Compute")
-	MDMComponentMemory     = types.NewSpecifier("Memory")
-	MDMOperationDiskAccess = types.NewSpecifier("DiskAccess")
-	MDMOperationDiskRead   = types.NewSpecifier("DiskRead")
-	MDMOperationDiskWrite  = types.NewSpecifier("DiskWrite")
-)
-
 const (
+	// MDMProgramInitTime is the time it takes to execute a program. This is a
+	// hardcoded value which is meant to be replaced in the future. TODO: The
+	// time is hardcoded to 10 for now until we add time management in the
+	// future.
+	MDMProgramInitTime = 10
+
+	// MDMTimeAppend is the time for executing an 'Append' instruction.
+	MDMTimeAppend = 10000
+
+	// MDMTimeCommit is the time used for executing managedFinalize.
+	MDMTimeCommit = 50e3
+
+	// MDMTimeHasSector is the time for executing a 'HasSector' instruction.
+	MDMTimeHasSector = 1
+
+	// MDMTimeReadSector is the time for executing a 'ReadSector' instruction.
+	MDMTimeReadSector = 1000
+
+	// MDMTimeWriteSector is the time for executing a 'WriteSector' instruction.
+	MDMTimeWriteSector = 10000
+
 	// RPCIHasSectorLen is the expected length of the 'Args' of a HasSector
 	// instruction.
 	RPCIHasSectorLen = 8
@@ -41,14 +54,18 @@ const (
 )
 
 var (
-	// SpecifierAppend is the specifier for the Append RPC.
+	// SpecifierAppend is the specifier for the Append instruction.
 	SpecifierAppend = InstructionSpecifier{'A', 'p', 'p', 'e', 'n', 'd'}
 
-	// SpecifierHasSector is the specifier for the ReadSector RPC.
+	// SpecifierHasSector is the specifier for the HasSector instruction.
 	SpecifierHasSector = InstructionSpecifier{'H', 'a', 's', 'S', 'e', 'c', 't', 'o', 'r'}
 
-	// SpecifierReadSector is the specifier for the ReadSector RPC.
+	// SpecifierReadSector is the specifier for the ReadSector instruction.
 	SpecifierReadSector = InstructionSpecifier{'R', 'e', 'a', 'd', 'S', 'e', 'c', 't', 'o', 'r'}
+
+	// ErrMDMInsufficientBudget is the error returned if the remaining budget of
+	// an MDM program is not sufficient to execute the next instruction.
+	ErrMDMInsufficientBudget = errors.New("remaining budget is insufficient")
 )
 
 // RPCIReadSector is a convenience method to create an Instruction of type 'ReadSector'.
@@ -64,4 +81,76 @@ func RPCIReadSector(rootOff, offsetOff, lengthOff uint64, merkleProof bool) Inst
 		Args:      args,
 		Specifier: SpecifierReadSector,
 	}
+}
+
+// MDMAppendCost is the cost of executing an 'Append' instruction.
+func MDMAppendCost(pt RPCPriceTable) (types.Currency, types.Currency) {
+	writeCost := pt.WriteLengthCost.Mul64(SectorSize).Add(pt.WriteBaseCost)
+	storeCost := pt.WriteStoreCost.Mul64(SectorSize) // potential refund
+	return writeCost.Add(storeCost), storeCost
+}
+
+// MDMInitCost is the cost of instantiatine the MDM. It is defined as:
+// 'InitBaseCost' + 'MemoryTimeCost' * 'programLen' * Time
+func MDMInitCost(pt RPCPriceTable, programLen uint64) types.Currency {
+	return pt.MemoryTimeCost.Mul64(programLen).Mul64(MDMProgramInitTime).Add(pt.InitBaseCost)
+}
+
+// MDMHasSectorCost is the cost of executing a 'HasSector' instruction.
+func MDMHasSectorCost(pt RPCPriceTable) (types.Currency, types.Currency) {
+	cost := pt.MemoryTimeCost.Mul64(1 << 20).Mul64(MDMTimeHasSector)
+	refund := types.ZeroCurrency // no refund
+	return cost, refund
+}
+
+// MDMReadCost is the cost of executing a 'Read' instruction. It is defined as:
+// 'readBaseCost' + 'readLengthCost' * `readLength`
+func MDMReadCost(pt RPCPriceTable, readLength uint64) (types.Currency, types.Currency) {
+	cost := pt.ReadLengthCost.Mul64(readLength).Add(pt.ReadBaseCost)
+	refund := types.ZeroCurrency // no refund
+	return cost, refund
+}
+
+// MDMWriteCost is the cost of executing a 'Write' instruction of a certain length.
+func MDMWriteCost(pt RPCPriceTable, writeLength uint64) (types.Currency, types.Currency) {
+	writeCost := pt.WriteLengthCost.Mul64(writeLength).Add(pt.WriteBaseCost)
+	storeCost := types.ZeroCurrency // no refund since we overwrite existing storage
+	return writeCost, storeCost
+}
+
+// MDMCopyCost is the cost of executing a 'Copy' instruction.
+func MDMCopyCost(pt RPCPriceTable, contractSize uint64) types.Currency {
+	return types.SiacoinPrecision // TODO: figure out good cost
+}
+
+// MDMSwapCost is the cost of executing a 'Swap' instruction.
+func MDMSwapCost(pt RPCPriceTable, contractSize uint64) types.Currency {
+	return types.SiacoinPrecision // TODO: figure out good cost
+}
+
+// MDMTruncateCost is the cost of executing a 'Truncate' instruction.
+func MDMTruncateCost(pt RPCPriceTable, contractSize uint64) types.Currency {
+	return types.SiacoinPrecision // TODO: figure out good cost
+}
+
+// MDMAppendMemory returns the additional memory consumption of a 'Append'
+// instruction.
+func MDMAppendMemory() uint64 {
+	return SectorSize // A full sector is added to the program's memory until the program is finalized.
+}
+
+// MDMHasSectorMemory returns the additional memory consumption of a 'HasSector'
+// instruction.
+func MDMHasSectorMemory() uint64 {
+	return 0 // 'HasSector' doesn't hold on to any memory beyond the lifetime of the instruction.
+}
+
+// MDMReadMemory returns the additional memory consumption of a 'Read' instruction.
+func MDMReadMemory() uint64 {
+	return 0 // 'Read' doesn't hold on to any memory beyond the lifetime of the instruction.
+}
+
+// MDMMemoryCost computes the memory cost given a price table, memory and time.
+func MDMMemoryCost(pt RPCPriceTable, usedMemory, time uint64) types.Currency {
+	return pt.MemoryTimeCost.Mul64(usedMemory * time)
 }
