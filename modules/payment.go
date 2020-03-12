af26867559e8f78ce1f894c248d716da6bfd11f6
PJ <peterjan.brone@gmail.com>
2020-03-12 17:27:59 +0100

    Cherry-pick payment from mux-ea-mvp

--- modules/payment.go
+++ modules/payment.go
@@ -4,6 +4,7 @@ import (
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/types"
 	"gitlab.com/NebulousLabs/errors"
+	"gitlab.com/NebulousLabs/siamux"
 )
 
 const (
@@ -12,6 +13,15 @@ const (
 )
 
 var (
+	// ErrUnknownPaymentMethod occurs when the payment method specified in the
+	// PaymentRequest object is unknown. The possible options are outlined below
+	// under "Payment identifiers".
+	ErrUnknownPaymentMethod = errors.New("unknown payment method")
+
+	// ErrInvalidPaymentMethod occurs when the payment method is not accepted
+	// for a specific RPC.
+	ErrInvalidPaymentMethod = errors.New("invalid payment method")
+
 	// ErrInsufficientPaymentForRPC is returned when the provided payment was
 	// lower than the cost of the RPC.
 	ErrInsufficientPaymentForRPC = errors.New("Insufficient payment, the provided payment did not cover the cost of the RPC.")
@@ -39,13 +49,85 @@ var (
 	ErrWithdrawalInvalidSignature = errors.New("ephemeral account withdrawal message signature is invalid")
 )
 
+// PaymentProvider is the interface implemented when payment has to be made
+// for an RPC call to a host.
+type PaymentProvider interface {
+	ProvidePaymentForRPC(rpcID types.Specifier, payment types.Currency, stream siamux.Stream, blockHeight types.BlockHeight) (types.Currency, error)
+}
+
+// PaymentProcessor is the interface implemented when receiving payment for an
+// RPC.
+type PaymentProcessor interface {
+	ProcessPaymentForRPC(stream siamux.Stream) (types.Currency, error)
+}
+
+// PaymentProviderFunc is an adapter for the interface. This allows wrapping
+// an anonymous function as if it were an object implementing the interface.
+type PaymentProviderFunc func(rpcID types.Specifier, payment types.Currency, stream siamux.Stream, blockHeight types.BlockHeight) (types.Currency, error)
+
+// ProvidePaymentForRPC implements the interface
+func (f PaymentProviderFunc) ProvidePaymentForRPC(rpcID types.Specifier, payment types.Currency, stream siamux.Stream, blockHeight types.BlockHeight) (types.Currency, error) {
+	return f(rpcID, payment, stream, blockHeight)
+}
+
+// Payment identifiers
+var (
+	PayByContract         = types.NewSpecifier("PayByContract")
+	PayByEphemeralAccount = types.NewSpecifier("PayByEphemAcc")
+)
+
 type (
+	// PaymentRequest identifies the payment method. This can be either
+	// PayByContract or PayByEphemeralAccount
+	PaymentRequest struct {
+		Type types.Specifier
+	}
+
+	// PayByEphemeralAccountRequest holds all payment details to pay from an
+	// ephemeral account.
+	PayByEphemeralAccountRequest struct {
+		Message   WithdrawalMessage
+		Signature crypto.Signature
+		Priority  int64
+	}
+
+	// PayByEphemeralAccountResponse is the object sent in response to the
+	// PayByEphemeralAccountRequest
+	PayByEphemeralAccountResponse struct {
+		Amount types.Currency
+	}
+
+	// PayByContractRequest holds all payment details to pay from a file
+	// contract.
+	PayByContractRequest struct {
+		ContractID           types.FileContractID
+		NewRevisionNumber    uint64
+		NewValidProofValues  []types.Currency
+		NewMissedProofValues []types.Currency
+		Signature            []byte
+	}
+
+	// PayByContractResponse is the object sent in response to the
+	// PayByContractRequest
+	PayByContractResponse struct {
+		Signature crypto.Signature
+	}
+
 	// WithdrawalMessage contains all details to spend from an ephemeral account
 	WithdrawalMessage struct {
 		Account string
 		Expiry  types.BlockHeight
 		Amount  types.Currency
 		Nonce   [WithdrawalNonceSize]byte
+	}
+
+	// Receipt is returned by the host after a successful deposit into an
+	// ephemeral account and can be used as proof of payment.
+	Receipt struct {
+		Host      types.SiaPublicKey
+		Account   string
+		Amount    types.Currency
+		Timestamp int64
 	}
 )
 
