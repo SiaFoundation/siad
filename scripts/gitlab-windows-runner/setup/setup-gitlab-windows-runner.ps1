5205c478e5b68f0618cd9357508c387d5abc6372
Filip Rysavy <fil@nebulous.tech>
2020-03-27 14:20:45 +0100

    Update runner setup script

--- scripts/gitlab-windows-runner/setup/setup-gitlab-windows-runner.ps1
+++ scripts/gitlab-windows-runner/setup/setup-gitlab-windows-runner.ps1
@@ -14,16 +14,16 @@ Invoke-WebRequest -Uri $url -OutFile $output
 Invoke-Expression ".\install-chocolatey.ps1"
 
 # Install git (Gitlab Runner prerequisity)
-Invoke-Expression "choco install git -y"
+choco install git -y
 
 # Install golang
-Invoke-Expression "choco install golang -y"
+choco install golang -y
 
 # Install make
-Invoke-Expression "choco install make -y"
+choco install make -y
 
 # Install Gitlab Runner
-Invoke-Expression "choco install gitlab-runner -y"
+choco install gitlab-runner -y
 
 Pop-Location
 
@@ -40,11 +40,11 @@ Pop-Location
 Push-Location "C:\gitlab-runner"
 
 # Register runner
-Invoke-Expression "cmd /c gitlab-runner.exe register --non-interactive --url https://gitlab.com/ --registration-token $token --executor shell --description Win10-Server-Hetzner --tag-list nebulous-windows"
+gitlab-runner.exe register --non-interactive --url https://gitlab.com/ --registration-token $token --executor shell --description Win10-Server-Hetzner --tag-list nebulous-windows
 
 # Install and run the service
 
-Invoke-Expression "cmd /c gitlab-runner.exe install"
-Invoke-Expression "cmd /c gitlab-runner.exe start"
+gitlab-runner.exe install
+gitlab-runner.exe start
 
 Pop-Location
