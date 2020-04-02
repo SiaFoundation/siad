b001f617986688b5411ca8744c0a21c81c6edb33
Ivaylo Novakov <inovakov@gmail.com>
2020-04-02 18:26:35 +0200

    Restructuring: once a goroutine errors out it stops until all goroutines stop and then  we start them all back again. WIP: This deadlocks for the moment.

--- modules/renter/proto/refcounter_acid_test.go
+++ modules/renter/proto/refcounter_acid_test.go
@@ -1,6 +1,7 @@
 package proto
 
 import (
+	"fmt"
 	"math"
 	"os"
 	"path/filepath"
@@ -51,136 +52,72 @@ func TestRefCounterFaultyDisk(t *testing.T) {
 	}
 
 	// stat counters
-	var atomicNumRecoveries int64
-	var atomicNumSuccessfulIterations int64
+	var atomicNumRecoveries uint64
+	var atomicNumSuccessfulIterations uint64
 
-	workload := func(rcLocal *RefCounter) {
-		testDone := time.After(testTimeout)
-		// The outer loop is responsible for simulating a restart of siad by
-		// reloading the wal, applying transactions and loading the refcounter
-		// from disk again.
-	OUTER:
+	performUpdates := func(rcLocal *RefCounter, testDone <-chan time.Time) error {
+		// The loop applies a random number of operations on the file.
 		for {
 			select {
 			case <-testDone:
-				break OUTER
+				return nil
 			default:
 			}
-			// The inner loop applies a random number of operations on the file.
-		INNER:
-			for {
-				select {
-				case <-testDone:
-					break OUTER
-				default:
-				}
-				// 5% chance to break out of inner loop.
-				if fastrand.Intn(100) < 5 {
-					break INNER
-				}
 
-				if err := preformUpdateOperations(rcLocal); err != nil {
-					if errors.Contains(err, dependencies.ErrDiskFault) {
-						atomic.AddInt64(&atomicNumRecoveries, 1)
-						break INNER
-					}
-					// If the error wasn't caused by the dependency the
-					// test fails.
-					t.Error(err)
-					return
-				}
-				atomic.AddInt64(&atomicNumSuccessfulIterations, 1)
-			}
-
-			// 20% chance that drive is repaired.
-			if fastrand.Intn(100) < 20 {
-				fdd.Reset()
-			}
-
-			// Try to reload the file. This simulates failures during recovery.
-		LOAD:
-			for tries := 1; ; tries++ {
-				// Every 10 times, we reset the dependency to avoid getting
-				// stuck here.
-				if tries%10 == 0 {
+			fmt.Print(".")
+			err := preformUpdateOperations(rcLocal)
+			if errors.Contains(err, dependencies.ErrDiskFault) {
+				atomic.AddUint64(&atomicNumRecoveries, 1)
+				// 20% chance to auto-recover
+				if fastrand.Intn(100) < 20 {
 					fdd.Reset()
+					err = nil
 				}
-				// Reload the wal from disk and apply unfinished txns.
-				newWal, err := loadWal(rcFilePath, walPath, fdd)
-				if errors.Contains(err, dependencies.ErrDiskFault) {
-					atomic.AddInt64(&atomicNumRecoveries, 1)
-					continue LOAD // try again
-				} else if err != nil {
-					t.Fatal(err)
+			}
+			if err != nil {
+				// we have an error, fake or not we should return
+				return err
+			}
+			atomic.AddUint64(&atomicNumSuccessfulIterations, 1)
+		}
+	}
+
+	testDone := time.After(testTimeout)
+	var wg sync.WaitGroup
+	// The outer loop keeps restarting the tests until the time runs out
+OUTER:
+	for {
+		// Run a high number of tests in parallel
+		for i := 0; i < runtime.NumCPU()*10; i++ {
+			wg.Add(1)
+			go func(n int) {
+				defer wg.Done()
+				//fmt.Println(" >> starting", n)
+				err = performUpdates(rc, testDone)
+				if err != nil && !errors.Contains(err, dependencies.ErrDiskFault) {
+					// We have a real error - fail the test
+					t.Error(err)
 				}
-				// Load the file again.
-				newRc, err := LoadRefCounter(rcFilePath, newWal)
-				if errors.Contains(err, dependencies.ErrDiskFault) {
-					atomic.AddInt64(&atomicNumRecoveries, 1)
-					continue LOAD // try again
-				} else if err != nil {
-					t.Fatal(err)
-				}
-				newRc.staticDeps = fdd
-				rcLocal = &newRc
-				break LOAD
+				//fmt.Println(" >> ending", n)
+			}(i)
+		}
+		wg.Wait()
+
+		select {
+		case <-testDone:
+			// the time has run out, finish the test
+			break OUTER
+		default:
+			// there is still time, load the wal from disk and re-run the test
+			rc, err = reloadRefCounter(rcFilePath, walPath, fdd, &atomicNumRecoveries)
+			if err != nil {
+				t.Fatal("Failed to reload wal from disk:", err)
 			}
 		}
 	}
 
-	// Run the workload on runtime.NumCPU() * 10 threads
-	var wg sync.WaitGroup
-	for i := 0; i < runtime.NumCPU()*10; i++ {
-		wg.Add(1)
-		go func() {
-			workload(rc)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-
 	t.Logf("\nRecovered from %v disk failures\n", atomicNumRecoveries)
 	t.Logf("Inner loop %v iterations without failures\n", atomicNumSuccessfulIterations)
-}
-
-// validateDecrement is a helper method that returns false if the counter is 0.
-// This allows us to avoid a counter underflow.
-func validateDecrement(rc *RefCounter, secNum uint64) error {
-	n, err := rc.readCount(secNum)
-	// Ignore errors coming from the dependency for this one
-	if err != nil && !errors.Contains(err, dependencies.ErrDiskFault) {
-		// If the error wasn't caused by the dependency, the test fails.
-		return err
-	}
-	if n < 1 {
-		return errors.New("Cannot decrement a zero counter")
-	}
-	return nil
-}
-
-// validateDropSectors is a helper method that returns false if the number of
-// sectors in the refcounter is smaller than the number of sectors we want to
-// drop.
-func validateDropSectors(rc *RefCounter, secNum uint64) error {
-	if rc.numSectors < secNum {
-		return errors.New("Cannot drop more sectors than the total")
-	}
-	return nil
-}
-
-// validateDecrement is a helper method that returns false if the counter has
-// reached its maximum value. This allows us to avoid a counter overflow.
-func validateIncrement(rc *RefCounter, secNum uint64) error {
-	n, err := rc.readCount(secNum)
-	// Ignore errors coming from the dependency for this one
-	if err != nil && !errors.Contains(err, dependencies.ErrDiskFault) {
-		// If the error wasn't caused by the dependency, the test fails.
-		return err
-	}
-	if n == math.MaxUint16 {
-		return errors.New("Cannot increment a counter at max value")
-	}
-	return nil
 }
 
 // loadWal reads the wal from disk and applies all outstanding transactions
@@ -188,20 +125,20 @@ func loadWal(filepath string, walPath string, fdd *dependencies.DependencyFaulty
 	// load the wal from disk
 	txns, newWal, err := writeaheadlog.New(walPath)
 	if err != nil {
-		return &writeaheadlog.WAL{}, errors.AddContext(err, "failed to load wal from disk")
+		return nil, errors.AddContext(err, "failed to load wal from disk")
 	}
 	f, err := fdd.OpenFile(filepath, os.O_RDWR, modules.DefaultFilePerm)
 	if err != nil {
-		return &writeaheadlog.WAL{}, errors.AddContext(err, "failed to open refcounter file in order to apply updates")
+		return nil, errors.AddContext(err, "failed to open refcounter file in order to apply updates")
 	}
 	defer f.Close()
 	// apply any outstanding transactions
 	for _, txn := range txns {
 		if err := applyUpdates(f, txn.Updates...); err != nil {
-			return &writeaheadlog.WAL{}, errors.AddContext(err, "failed to apply updates")
+			return nil, errors.AddContext(err, "failed to apply updates")
 		}
 		if err := txn.SignalUpdatesApplied(); err != nil {
-			return &writeaheadlog.WAL{}, errors.AddContext(err, "failed to signal updates applied")
+			return nil, errors.AddContext(err, "failed to signal updates applied")
 		}
 	}
 	return newWal, f.Sync()
@@ -214,14 +151,6 @@ func preformUpdateOperations(rc *RefCounter) (err error) {
 	if err != nil {
 		return
 	}
-	defer func() {
-		if err != nil {
-			// if there is an error wrap up the update session, so we can start
-			// a new one and carry on later
-			rc.UpdateApplied()
-		}
-	}()
-
 	updates := []writeaheadlog.Update{}
 	var u writeaheadlog.Update
 
@@ -295,5 +224,77 @@ func preformUpdateOperations(rc *RefCounter) (err error) {
 		return
 	}
 	rc.UpdateApplied()
+	return nil
+}
+
+// reloadRefCounter tries to reload the file. This simulates failures during recovery.
+func reloadRefCounter(rcFilePath, walPath string, fdd *dependencies.DependencyFaultyDisk, atomicNumRecoveries *uint64) (*RefCounter, error) {
+	// Try to reload the file. This simulates failures during recovery.
+	for tries := 1; ; tries++ {
+		// Every 10 times, we reset the dependency to avoid getting
+		// stuck here.
+		if tries%10 == 0 {
+			fdd.Reset()
+		}
+		// Reload the wal from disk and apply unfinished txns
+		newWal, err := loadWal(rcFilePath, walPath, fdd)
+		if errors.Contains(err, dependencies.ErrDiskFault) {
+			atomic.AddUint64(atomicNumRecoveries, 1)
+			continue // try again
+		} else if err != nil {
+			// an actual error occurred, the test must fail
+			return nil, err
+		}
+		// Reload the refcounter from disk
+		newRc, err := LoadRefCounter(rcFilePath, newWal)
+		if errors.Contains(err, dependencies.ErrDiskFault) {
+			atomic.AddUint64(atomicNumRecoveries, 1)
+			continue // try again
+		} else if err != nil {
+			// an actual error occurred, the test must fail
+			return nil, err
+		}
+		newRc.staticDeps = fdd
+		return &newRc, nil
+	}
+}
+
+// validateDecrement is a helper method that returns false if the counter is 0.
+// This allows us to avoid a counter underflow.
+func validateDecrement(rc *RefCounter, secNum uint64) error {
+	n, err := rc.readCount(secNum)
+	// Ignore errors coming from the dependency for this one
+	if err != nil && !errors.Contains(err, dependencies.ErrDiskFault) {
+		// If the error wasn't caused by the dependency, the test fails.
+		return err
+	}
+	if n < 1 {
+		return errors.New("Cannot decrement a zero counter")
+	}
+	return nil
+}
+
+// validateDropSectors is a helper method that returns false if the number of
+// sectors in the refcounter is smaller than the number of sectors we want to
+// drop.
+func validateDropSectors(rc *RefCounter, secNum uint64) error {
+	if rc.numSectors < secNum {
+		return errors.New("Cannot drop more sectors than the total")
+	}
+	return nil
+}
+
+// validateDecrement is a helper method that returns false if the counter has
+// reached its maximum value. This allows us to avoid a counter overflow.
+func validateIncrement(rc *RefCounter, secNum uint64) error {
+	n, err := rc.readCount(secNum)
+	// Ignore errors coming from the dependency for this one
+	if err != nil && !errors.Contains(err, dependencies.ErrDiskFault) {
+		// If the error wasn't caused by the dependency, the test fails.
+		return err
+	}
+	if n == math.MaxUint16 {
+		return errors.New("Cannot increment a counter at max value")
+	}
 	return nil
 }
