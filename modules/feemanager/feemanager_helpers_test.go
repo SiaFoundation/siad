4b50d51c3ad276804c20b8f1d5143f1783673858
David Vorick <david@nebulouslabs.com>
2020-04-21 08:30:10 -0400

    extend testing of fees

---
+++ modules/feemanager/feemanager_helpers_test.go
@@ -0,0 +1,80 @@
+package feemanager
+
+import (
+	"path/filepath"
+
+	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/crypto"
+	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/modules/consensus"
+	"gitlab.com/NebulousLabs/Sia/modules/gateway"
+	"gitlab.com/NebulousLabs/Sia/modules/transactionpool"
+	"gitlab.com/NebulousLabs/Sia/modules/wallet"
+	"gitlab.com/NebulousLabs/Sia/types"
+	"gitlab.com/NebulousLabs/fastrand"
+)
+
+// newTestingFeeManager creates a FeeManager for testing
+func newTestingFeeManager(testName string) (*FeeManager, error) {
+	// Create testdir
+	testDir := build.TempDir("feemanager", testName)
+
+	// Create Dependencies
+	cs, w, err := testingDependencies(testDir)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return FeeManager
+	return NewCustomFeeManager(cs, w, filepath.Join(testDir, modules.FeeManagerDir), modules.ProdDependencies)
+}
+
+// testingDependencies creates the dependencies needed for the FeeManager
+func testingDependencies(testdir string) (modules.ConsensusSet, modules.Wallet, error) {
+	// Create a gateway
+	g, err := gateway.New("localhost:0", false, filepath.Join(testdir, modules.GatewayDir))
+	if err != nil {
+		return nil, nil, err
+	}
+	// Create a consensus set
+	cs, errChan := consensus.New(g, false, filepath.Join(testdir, modules.ConsensusDir))
+	if err := <-errChan; err != nil {
+		return nil, nil, err
+	}
+	// Create a tpool
+	tp, err := transactionpool.New(cs, g, filepath.Join(testdir, modules.TransactionPoolDir))
+	if err != nil {
+		return nil, nil, err
+	}
+	// Create a wallet and unlock it
+	w, err := wallet.New(cs, tp, filepath.Join(testdir, modules.WalletDir))
+	if err != nil {
+		return nil, nil, err
+	}
+	key := crypto.GenerateSiaKey(crypto.TypeDefaultWallet)
+	_, err = w.Encrypt(key)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = w.Unlock(key)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return cs, w, nil
+}
+
+// addRandomFees iwill add a random number of feees to the FeeManager, always at
+// least 1.
+func addRandomFees(fm *FeeManager) error {
+	for i := 0; i < fastrand.Intn(5)+1; i++ {
+		amount := types.NewCurrency64(fastrand.Uint64n(100))
+		appUID := modules.AppUID(uniqueID())
+		recurring := fastrand.Intn(100)%2 == 0
+		err := fm.AddFee(types.UnlockHash{}, amount, appUID, recurring)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
