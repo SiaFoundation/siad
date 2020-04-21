8e340817a7a8db5aefd97eced2abca5dcfba24d9
Chris Schinnerl <chris@nebulous.tech>
2020-04-21 09:28:13 +0200

    fix build

--- modules/host/rpcfundephemeralaccount_test.go
+++ modules/host/rpcfundephemeralaccount_test.go
@@ -88,7 +88,7 @@ func TestFundEphemeralAccountRPC(t *testing.T) {
 		// expect clean stream close
 		err = modules.RPCRead(stream, struct{}{})
 		if !errors.Contains(err, io.ErrClosedPipe) {
-			return nil, err
+			return nil, nil, err
 		}
 
 		return &payByResponse, &resp, nil
