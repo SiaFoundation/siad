43da3540aee52792e4965521b44bce09fe4161a3
David Vorick <david@nebulouslabs.com>
2020-05-04 01:22:29 -0400

    fix build, reorganize funcs

--- cmd/skynet-test/main.go
+++ cmd/skynet-test/main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"sync"
 	"sync/atomic"
-	// "time"
+	"time"
 
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/modules/renter/filesystem"
@@ -36,6 +36,188 @@ const (
 var (
 	c *client.Client
 )
+
+func main() {
+	fmt.Printf("Skynet performance analysis tool.\n\n")
+
+	args := os.Args
+	var addr string
+	if len(args) == 1 {
+		addr = "localhost:9980"
+	} else if len(args) == 2 {
+		// Parse port.
+		num, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("Error parsing port:", err)
+		}
+		if num > 65535 {
+			fmt.Println("Invalid port number")
+		}
+		addr = "localhost:" + args[1]
+	} else if len(args) > 2 {
+		fmt.Println("Usage: ./skynet-test [optional: port for siad api]")
+		return
+	}
+
+	c = client.New(addr)
+
+	// Establish the directories that we will be using for testing.
+	dirBasePath, err := modules.NewSiaPath(testSiaDir)
+	if err != nil {
+		fmt.Println("Could not create siapath for testing directory:", err)
+		return
+	}
+	dir64kbPath, err := dirBasePath.Join(dir64kb)
+	if err != nil {
+		fmt.Println("Could not create 64kb siapath for testing directory:", err)
+		return
+	}
+	dir1mbPath, err := dirBasePath.Join(dir1mb)
+	if err != nil {
+		fmt.Println("Could not create 1mb siapath for testing directory:", err)
+		return
+	}
+	dir4mbPath, err := dirBasePath.Join(dir4mb)
+	if err != nil {
+		fmt.Println("Could not create 4mb siapath for testing directory:", err)
+		return
+	}
+	dir10mbPath, err := dirBasePath.Join(dir10mb)
+	if err != nil {
+		fmt.Println("Could not create 10mb siapath for testing directory:", err)
+		return
+	}
+
+	// Upload the files for each dir. The filesize used is slightly smaller than
+	// the expected filesize to leave room for metadata overhead. The expected
+	// filesize used is the largest filesize that fits inside of the file limits
+	// for the metrics collector.
+	err = uploadFileSet(dir64kbPath, 61e3, 61440)
+	if err != nil {
+		fmt.Println("Unable to upload 64kb files:", err)
+		return
+	}
+	fmt.Println("64kb files are ready to go.")
+	err = uploadFileSet(dir1mbPath, 982e3, 983040)
+	if err != nil {
+		fmt.Println("Unable to upload 1mb files:", err)
+		return
+	}
+	fmt.Println("1mb files are ready to go.")
+	err = uploadFileSet(dir4mbPath, 3931e3, 3932160)
+	if err != nil {
+		fmt.Println("Unable to upload 4mb files:", err)
+		return
+	}
+	fmt.Println("4mb files are ready to go.")
+	err = uploadFileSet(dir10mbPath, 10e6, 10e6)
+	if err != nil {
+		fmt.Println("Unable to upload 10mb files:", err)
+		return
+	}
+	fmt.Printf("10mb files are ready to go.\n\n")
+
+	fmt.Printf("Beginning download testing. Each test is %v files\n\n", filesPerDir)
+
+	// Download all of the 64kb files.
+	threadss := []uint64{1, 4, 16, 64} // threadss is the plural of threads, deal with it
+	downloadStart := time.Now()
+	for _, threads := range threadss {
+		err = downloadFileSet(dir64kbPath, 61440, threads)
+		if err != nil {
+			fmt.Println("Unable to download all 64kb files:", err)
+		}
+		fmt.Printf("64kb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
+		downloadStart = time.Now()
+		err = downloadFileSet(dir1mbPath, 983040, threads)
+		if err != nil {
+			fmt.Println("Unable to download all 1mb files:", err)
+		}
+		fmt.Printf("1mb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
+		downloadStart = time.Now()
+		err = downloadFileSet(dir4mbPath, 3932160, threads)
+		if err != nil {
+			fmt.Println("Unable to download all 4mb files:", err)
+		}
+		fmt.Printf("4mb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
+		downloadStart = time.Now()
+		err = downloadFileSet(dir10mbPath, 10e6, threads)
+		if err != nil {
+			fmt.Println("Unable to download all 10mb files:", err)
+		}
+		fmt.Printf("10mb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
+		downloadStart = time.Now()
+	}
+}
+
+// downloadFileSet will download all of the files of the expected fetch size in
+// a dir.
+func downloadFileSet(dir modules.SiaPath, expectedFetchSize uint64, threads uint64) error {
+	// Create a thread pool and fill it. Need to grab a struct from the pool
+	// before launching a thread, need to drop the object back into the pool
+	// when done.
+	threadPool := make(chan struct{}, threads)
+	for i := uint64(0); i < threads; i++ {
+		threadPool <- struct{}{}
+	}
+
+	// Loop over every file. Block until there is an object ready in the thread
+	// pool, then launch a thread.
+	var atomicDownloadErrors uint64
+	var wg sync.WaitGroup
+	for i := 0; i < filesPerDir; i++ {
+		// Get permission to launch a thread.
+		<-threadPool
+
+		// Launch the downloading thread.
+		wg.Add(1)
+		go func(i int) {
+			// Make room for the next thread.
+			defer func() {
+				threadPool <- struct{}{}
+			}()
+			// Clear the wait group.
+			defer wg.Done()
+
+			// Figure out the siapath of the dir.
+			siaPath, err := dir.Join(strconv.Itoa(i))
+			if err != nil {
+				fmt.Println("Dir error:", err)
+				atomic.AddUint64(&atomicDownloadErrors, 1)
+				return
+			}
+			// Figure out the skylink for the file.
+			rf, err := c.RenterFileRootGet(siaPath)
+			if err != nil {
+				fmt.Println("Error geting file info:", err)
+				atomic.AddUint64(&atomicDownloadErrors, 1)
+				return
+			}
+			// Get a reader / stream for the download.
+			reader, err := c.SkynetSkylinkReaderGet(rf.File.Skylinks[0])
+			if err != nil {
+				fmt.Println("Error geting skylink reader:", err)
+				atomic.AddUint64(&atomicDownloadErrors, 1)
+				return
+			}
+			// Download and discard the result, we only care about the speeds,
+			// not the data.
+			_, err = io.Copy(ioutil.Discard, reader)
+			if err != nil {
+				fmt.Println("Error performing download:", err)
+				atomic.AddUint64(&atomicDownloadErrors, 1)
+				return
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	// Don't need to use atomics, all threads have returned.
+	if atomicDownloadErrors != 0 {
+		return fmt.Errorf("there were %v errors while downloading", atomicDownloadErrors)
+	}
+	return nil
+}
 
 // getMissingFiles will fetch a map of all the files that are missing or don't
 // have skylinks
@@ -135,186 +317,4 @@ func uploadFileSet(dir modules.SiaPath, fileSize uint64, expectedFetchSize uint6
 		return errors.New("Upload appears unsuccessful")
 	}
 	return nil
-}
-
-// downloadFileSet will download all of the files of the expected fetch size in
-// a dir.
-func downloadFileSet(dir modules.SiaPath, expectedFetchSize uint64, threads uint64) error {
-	// Create a thread pool and fill it. Need to grab a struct from the pool
-	// before launching a thread, need to drop the object back into the pool
-	// when done.
-	threadPool := make(chan struct{}, threads)
-	for i := uint64(0); i < threads; i++ {
-		threadPool <- struct{}{}
-	}
-
-	// Loop over every file. Block until there is an object ready in the thread
-	// pool, then launch a thread.
-	var atomicDownloadErrors uint64
-	var wg sync.WaitGroup
-	for i := 0; i < filesPerDir; i++ {
-		// Get permission to launch a thread.
-		<-threadPool
-
-		// Launch the downloading thread.
-		wg.Add(1)
-		go func(i int) {
-			// Make room for the next thread.
-			defer func() {
-				threadPool <- struct{}{}
-			}()
-			// Clear the wait group.
-			defer wg.Done()
-
-			// Figure out the siapath of the dir.
-			siaPath, err := dir.Join(strconv.Itoa(i))
-			if err != nil {
-				fmt.Println("Dir error:", err)
-				atomic.AddUint64(&atomicDownloadErrors, 1)
-				return
-			}
-			// Figure out the skylink for the file.
-			rf, err := c.RenterFileRootGet(siaPath)
-			if err != nil {
-				fmt.Println("Error geting file info:", err)
-				atomic.AddUint64(&atomicDownloadErrors, 1)
-				return
-			}
-			// Get a reader / stream for the download.
-			reader, err := c.SkynetSkylinkReaderGet(rf.File.Skylinks[0])
-			if err != nil {
-				fmt.Println("Error geting skylink reader:", err)
-				atomic.AddUint64(&atomicDownloadErrors, 1)
-				return
-			}
-			// Download and discard the result, we only care about the speeds,
-			// not the data.
-			_, err = io.Copy(ioutil.Discard, reader)
-			if err != nil {
-				fmt.Println("Error performing download:", err)
-				atomic.AddUint64(&atomicDownloadErrors, 1)
-				return
-			}
-		}(i)
-	}
-	wg.Wait()
-
-	// Don't need to use atomics, all threads have returned.
-	if atomicDownloadErrors != 0 {
-		return fmt.Errorf("there were %v errors while downloading", atomicDownloadErrors)
-	}
-	return nil
-}
-
-func main() {
-	fmt.Printf("Skynet performance analysis tool.\n\n")
-
-	args := os.Args
-	var addr string
-	if len(args) == 1 {
-		addr = "localhost:9980"
-	} else if len(args) == 2 {
-		// Parse port.
-		num, err := strconv.Atoi(args[1])
-		if err != nil {
-			fmt.Println("Error parsing port:", err)
-		}
-		if num > 65535 {
-			fmt.Println("Invalid port number")
-		}
-		addr = "localhost:" + args[1]
-	} else if len(args) > 2 {
-		fmt.Println("Usage: ./skynet-test [optional: port for siad api]")
-		return
-	}
-
-	c = client.New("localhost:9970")
-
-	// Establish the directories that we will be using for testing.
-	dirBasePath, err := modules.NewSiaPath(testSiaDir)
-	if err != nil {
-		fmt.Println("Could not create siapath for testing directory:", err)
-		return
-	}
-	dir64kbPath, err := dirBasePath.Join(dir64kb)
-	if err != nil {
-		fmt.Println("Could not create 64kb siapath for testing directory:", err)
-		return
-	}
-	dir1mbPath, err := dirBasePath.Join(dir1mb)
-	if err != nil {
-		fmt.Println("Could not create 1mb siapath for testing directory:", err)
-		return
-	}
-	dir4mbPath, err := dirBasePath.Join(dir4mb)
-	if err != nil {
-		fmt.Println("Could not create 4mb siapath for testing directory:", err)
-		return
-	}
-	dir10mbPath, err := dirBasePath.Join(dir10mb)
-	if err != nil {
-		fmt.Println("Could not create 10mb siapath for testing directory:", err)
-		return
-	}
-
-	// Upload the files for each dir. The filesize used is slightly smaller than
-	// the expected filesize to leave room for metadata overhead. The expected
-	// filesize used is the largest filesize that fits inside of the file limits
-	// for the metrics collector.
-	err = uploadFileSet(dir64kbPath, 61e3, 61440)
-	if err != nil {
-		fmt.Println("Unable to upload 64kb files:", err)
-		return
-	}
-	fmt.Println("64kb files are ready to go.")
-	err = uploadFileSet(dir1mbPath, 982e3, 983040)
-	if err != nil {
-		fmt.Println("Unable to upload 1mb files:", err)
-		return
-	}
-	fmt.Println("1mb files are ready to go.")
-	err = uploadFileSet(dir4mbPath, 3931e3, 3932160)
-	if err != nil {
-		fmt.Println("Unable to upload 4mb files:", err)
-		return
-	}
-	fmt.Println("4mb files are ready to go.")
-	err = uploadFileSet(dir10mbPath, 10e6, 10e6)
-	if err != nil {
-		fmt.Println("Unable to upload 10mb files:", err)
-		return
-	}
-	fmt.Printf("10mb files are ready to go.\n\n")
-
-	fmt.Printf("Beginning download testing. Each test is %v files\n\n", filesPerDir)
-
-	// Download all of the 64kb files.
-	threadss := []uint64{1, 4, 16, 64} // threadss is the plural of threads, deal with it
-	downloadStart := time.Now()
-	for _, threads := range threadss {
-		err = downloadFileSet(dir64kbPath, 61440, threads)
-		if err != nil {
-			fmt.Println("Unable to download all 64kb files:", err)
-		}
-		fmt.Printf("64kb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
-		downloadStart = time.Now()
-		err = downloadFileSet(dir1mbPath, 983040, threads)
-		if err != nil {
-			fmt.Println("Unable to download all 1mb files:", err)
-		}
-		fmt.Printf("1mb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
-		downloadStart = time.Now()
-		err = downloadFileSet(dir4mbPath, 3932160, threads)
-		if err != nil {
-			fmt.Println("Unable to download all 4mb files:", err)
-		}
-		fmt.Printf("4mb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
-		downloadStart = time.Now()
-		err = downloadFileSet(dir10mbPath, 10e6, threads)
-		if err != nil {
-			fmt.Println("Unable to download all 10mb files:", err)
-		}
-		fmt.Printf("10mb downloads on %v threads finished in %v\n", threads, time.Since(downloadStart))
-		downloadStart = time.Now()
-	}
 }
