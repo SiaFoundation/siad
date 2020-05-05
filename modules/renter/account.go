5426245349b0618c698968012556f730ac82e56d
PJ <peterjan.brone@gmail.com>
2020-05-05 10:15:28 +0200

    Move persistence into its own file

--- modules/renter/account.go
+++ modules/renter/account.go
@@ -1,13 +1,8 @@
 package renter
 
 import (
-	"bytes"
-	"io"
-	"os"
-	"path/filepath"
 	"sync"
 
-	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/encoding"
 	"gitlab.com/NebulousLabs/Sia/modules"
@@ -18,9 +13,6 @@ import (
 )
 
 const (
-	// accountSize is the fixed account size in bytes
-	accountSize = 1 << 8 // 256 bytes
-
 	// withdrawalValidityPeriod defines the period (in blocks) a withdrawal
 	// message remains spendable after it has been created. Together with the
 	// current block height at time of creation, this period makes up the
@@ -28,27 +20,7 @@ const (
 	withdrawalValidityPeriod = 6
 )
 
-var (
-	// accountsFilename is the filename of the accounts persistence file
-	accountsFilename = "accounts.dat"
-
-	// Metadata
-	metadataHeader  = types.NewSpecifier("Accounts\n")
-	metadataVersion = types.NewSpecifier("v1.5.0\n")
-	metadataSize    = 2*types.SpecifierLen + 1 // 1 byte for 'clean' flag
-
-	// Metadata validation errors
-	errWrongHeader  = errors.New("wrong header")
-	errWrongVersion = errors.New("wrong version")
-)
-
 type (
-	// accountsMetadata is the metadata of the accounts persist file
-	accountsMetadata struct {
-		Header  types.Specifier
-		Version types.Specifier
-		Clean   bool
-	}
 
 	// account represents a renter's ephemeral account on a host.
 	account struct {
@@ -62,16 +34,6 @@ type (
 		pendingSpends types.Currency
 
 		staticMu sync.RWMutex
-	}
-
-	// accountPersistence is the account's persistence object which holds all
-	// data that gets persisted for a single account.
-	accountPersistence struct {
-		AccountID modules.AccountID
-		Balance   types.Currency
-		HostKey   types.SiaPublicKey
-		SecretKey crypto.SecretKey
-		Checksum  crypto.Hash
 	}
 )
 
@@ -127,128 +89,6 @@ func (a *account) managedAvailableBalance() types.Currency {
 	return types.ZeroCurrency
 }
 
-// toAccountPersistence returns the account's persistence object
-func (a *account) toAccountPersistence() accountPersistence {
-	ap := accountPersistence{
-		AccountID: a.staticID,
-		Balance:   a.balance,
-		HostKey:   a.staticHostKey,
-		SecretKey: a.staticSecretKey,
-	}
-	ap.Checksum = ap.checksum()
-	return ap
-}
-
-// managedLoadAccounts loads the accounts from the persistence file onto the
-// Renter object.
-func (r *Renter) managedLoadAccounts() error {
-	// fetch the fileInfo before opening the file
-	path := filepath.Join(r.persistDir, accountsFilename)
-	_, statErr := os.Stat(path)
-
-	// open the file, create it if it does not exist yet
-	file, err := r.deps.OpenFile(path, os.O_RDWR|os.O_CREATE, defaultFilePerm)
-	if err != nil {
-		return errors.AddContext(err, "Failed to open accounts persist file")
-	}
-
-	// read the metadata
-	var metadata accountsMetadata
-	if os.IsNotExist(statErr) {
-		metadata = accountsMetadata{
-			Header:  metadataHeader,
-			Version: metadataVersion,
-			Clean:   true,
-		}
-	} else {
-		buffer := make([]byte, metadataSize)
-		_, err := io.ReadFull(file, buffer)
-		if err != nil {
-			return errors.AddContext(err, "Failed to read metadata from accounts file")
-		}
-		err = encoding.Unmarshal(buffer, &metadata)
-		if err != nil {
-			return errors.AddContext(err, "Failed to decode metadata from accounts file")
-		}
-	}
-
-	// validate the metadata
-	if metadata.Header != metadataHeader {
-		return errors.AddContext(errWrongHeader, "Failed to verify accounts metadata")
-	}
-	if metadata.Version != metadataVersion {
-		return errors.AddContext(errWrongVersion, "Failed to verify accounts metadata")
-	}
-
-	// sanity check that the account size is larger than the metadata size,
-	// before setting the initial offset
-	if metadataSize > accountSize {
-		build.Critical("Metadata size is larger than account size, this means the initial offset is too small")
-	}
-	initialOffset := int64(accountSize)
-
-	// read the raw account data and decode them into accounts
-	accounts := make(map[string]*account)
-
-	accountOffset := initialOffset
-	for offset := initialOffset; ; offset += accountSize {
-		accountBytes := make([]byte, accountSize, accountSize)
-		_, err = file.ReadAt(accountBytes, offset)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			r.log.Println("ERROR:", errors.AddContext(err, "Failed to read account data"))
-			continue
-		}
-
-		var accountData accountPersistence
-		err = encoding.Unmarshal(accountBytes, &accountData)
-		if err != nil {
-			r.log.Println("ERROR:", errors.AddContext(err, "Failed to unmarshal account"))
-			continue
-		}
-		if !accountData.verifyChecksum(accountData.Checksum) {
-			r.log.Println("ERROR:", errors.New("Account ignored because checksum did not match"))
-			continue
-		}
-
-		acc := &account{
-			staticID:        accountData.AccountID,
-			staticHostKey:   accountData.HostKey,
-			staticOffset:    accountOffset,
-			staticSecretKey: accountData.SecretKey,
-			balance:         accountData.Balance,
-		}
-
-		// reset the account balances after an unclean shutdown
-		if !metadata.Clean {
-			acc.balance = types.ZeroCurrency
-		}
-		accounts[acc.staticHostKey.String()] = acc
-		accountOffset += accountSize
-	}
-
-	// mark the metadata as 'dirty' and update the metadata on disk - this
-	// ensures only after a successful shutdown, accounts are reloaded from disk
-	metadata.Clean = false
-	_, err = file.WriteAt(encoding.Marshal(metadata), 0)
-	if err != nil {
-		return errors.AddContext(err, "Failed to write metadata to accounts file")
-	}
-	err = file.Sync()
-	if err != nil {
-		return errors.AddContext(err, "Failed to sync accounts file")
-	}
-
-	// load the accounts on to the renter
-	id := r.mu.Lock()
-	r.accounts = accounts
-	r.staticAccountsFile = file
-	r.mu.Unlock(id)
-	return nil
-}
-
 // managedOpenAccount returns an account for the given host. If it does not
 // exist already one is created.
 func (r *Renter) managedOpenAccount(hostKey types.SiaPublicKey) *account {
@@ -262,46 +102,6 @@ func (r *Renter) managedOpenAccount(hostKey types.SiaPublicKey) *account {
 	acc = r.newAccount(hostKey)
 	r.accounts[hostKey.String()] = acc
 	return acc
-}
-
-// managedSaveAccounts is called on shutdown and ensures the account data is
-// properly persisted to disk
-func (r *Renter) managedSaveAccounts() error {
-	// grab the accounts
-	id := r.mu.RLock()
-	accounts := r.accounts
-	r.mu.RUnlock(id)
-
-	// save the account data to disk
-	for _, account := range accounts {
-		account.staticMu.Lock()
-		accountData := account.toAccountPersistence()
-		account.staticMu.Unlock()
-
-		bytes := accountData.toBytes()
-		_, err := r.staticAccountsFile.WriteAt(bytes, account.staticOffset)
-		if err != nil {
-			r.log.Println("ERROR:", err)
-			continue
-		}
-	}
-
-	// update the metadata and mark the file as clean
-	metadata := accountsMetadata{
-		Header:  metadataHeader,
-		Version: metadataVersion,
-		Clean:   true,
-	}
-	_, err := r.staticAccountsFile.WriteAt(encoding.Marshal(metadata), 0)
-	if err != nil {
-		return errors.AddContext(err, "Failed to update accounts file metadata")
-	}
-
-	// sync and close the persist file
-	return errors.Compose(
-		r.staticAccountsFile.Sync(),
-		r.staticAccountsFile.Close(),
-	)
 }
 
 // newAccount returns an new account object for the given host.
@@ -328,36 +128,6 @@ func (r *Renter) newAccount(hostKey types.SiaPublicKey) *account {
 	}
 
 	return acc
-}
-
-// checksum returns the checksum of the accountPeristence object
-func (ap accountPersistence) checksum() crypto.Hash {
-	return crypto.HashAll(
-		ap.AccountID,
-		ap.HostKey,
-		ap.SecretKey,
-		ap.Balance,
-	)
-}
-
-// toBytes is a helper method on the persistence object that marshals the object
-// into a byte slice and performs a sanity check on the length
-func (ap accountPersistence) toBytes() []byte {
-	apMar := encoding.Marshal(ap)
-	if len(apMar) > accountSize {
-		build.Critical("marshaled object is larger than expected size")
-	}
-
-	accountBytes := make([]byte, accountSize, accountSize)
-	copy(accountBytes, apMar)
-	return accountBytes
-}
-
-// verifyChecksum creates a checksum of the accountPersistence object and
-// checks if it's equal to the given checksum
-func (ap accountPersistence) verifyChecksum(checksum crypto.Hash) bool {
-	expected := ap.checksum()
-	return bytes.Equal(expected[:], checksum[:])
 }
 
 // newWithdrawalMessage is a helper function that takes a set of parameters and
--- modules/renter/account_test.go
+++ modules/renter/account_test.go
@@ -14,6 +14,85 @@ import (
 	"gitlab.com/NebulousLabs/fastrand"
 )
 
+// TestNewAccount verifies newAccount returns a valid account object
+func TestNewAccount(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	rt, err := newRenterTester(t.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rt.Close()
+	r := rt.renter
+
+	// create a random hostKey
+	_, pk := crypto.GenerateKeyPair()
+	hostKey := types.SiaPublicKey{
+		Algorithm: types.SignatureEd25519,
+		Key:       pk[:],
+	}
+
+	// create an account with a different hostkey to ensure the account we are
+	// going to validate has an offset different from 0
+	tmpKey := hostKey
+	fastrand.Read(tmpKey.Key[:4])
+	_ = r.newAccount(tmpKey)
+
+	// create a new account object
+	account := r.newAccount(hostKey)
+
+	// validate the account object
+	if account.staticID.IsZeroAccount() {
+		t.Fatal("Invalid account ID")
+	}
+	if account.staticOffset == 0 {
+		t.Fatal("Invalid offset")
+	}
+	if !account.staticHostKey.Equals(hostKey) {
+		t.Fatal("Invalid host key")
+	}
+
+	// validate the account id is built using a valid SiaPublicKey and the
+	// account's secret key belongs to the public key used to construct the id
+	hash := crypto.HashBytes(fastrand.Bytes(10))
+	sig := crypto.SignHash(hash, account.staticSecretKey)
+	err = crypto.VerifyHash(hash, account.staticID.SPK().ToPublicKey(), sig)
+	if err != nil {
+		t.Fatal("Invalid secret key")
+	}
+}
+
+// TestNewWithdrawalMessage verifies the newWithdrawalMessage helper
+// properly instantiates all required fields on the WithdrawalMessage
+func TestNewWithdrawalMessage(t *testing.T) {
+	// create a withdrawal message using random parameters
+	aid, _ := modules.NewAccountID()
+	amount := types.NewCurrency64(fastrand.Uint64n(100))
+	blockHeight := types.BlockHeight(fastrand.Intn(100))
+	msg := newWithdrawalMessage(aid, amount, blockHeight)
+
+	// validate the withdrawal message
+	if msg.Account != aid {
+		t.Fatal("Unexpected account ID")
+	}
+	if !msg.Amount.Equals(amount) {
+		t.Fatal("Unexpected amount")
+	}
+	if msg.Expiry != blockHeight+withdrawalValidityPeriod {
+		t.Fatal("Unexpected expiry")
+	}
+	if len(msg.Nonce) != modules.WithdrawalNonceSize {
+		t.Fatal("Unexpected nonce length")
+	}
+	var nonce [modules.WithdrawalNonceSize]byte
+	if bytes.Equal(msg.Nonce[:], nonce[:]) {
+		t.Fatal("Uninitialized nonce")
+	}
+}
+
 // TestAccountSave verifies accounts are properly saved and loaded onto the
 // renter when it goes through a graceful shutdown and reboot.
 func TestAccountSave(t *testing.T) {
@@ -274,85 +353,6 @@ func TestAccountMetadataMarhsaling(t *testing.T) {
 	}
 	if uMar.Clean != am.Clean {
 		t.Fatal("Unexpected clean flag")
-	}
-}
-
-// TestNewAccount verifies newAccount returns a valid account object
-func TestNewAccount(t *testing.T) {
-	if testing.Short() {
-		t.SkipNow()
-	}
-	t.Parallel()
-
-	rt, err := newRenterTester(t.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer rt.Close()
-	r := rt.renter
-
-	// create a random hostKey
-	_, pk := crypto.GenerateKeyPair()
-	hostKey := types.SiaPublicKey{
-		Algorithm: types.SignatureEd25519,
-		Key:       pk[:],
-	}
-
-	// create an account with a different hostkey to ensure the account we are
-	// going to validate has an offset different from 0
-	tmpKey := hostKey
-	fastrand.Read(tmpKey.Key[:4])
-	_ = r.newAccount(tmpKey)
-
-	// create a new account object
-	account := r.newAccount(hostKey)
-
-	// validate the account object
-	if account.staticID.IsZeroAccount() {
-		t.Fatal("Invalid account ID")
-	}
-	if account.staticOffset == 0 {
-		t.Fatal("Invalid offset")
-	}
-	if !account.staticHostKey.Equals(hostKey) {
-		t.Fatal("Invalid host key")
-	}
-
-	// validate the account id is built using a valid SiaPublicKey and the
-	// account's secret key belongs to the public key used to construct the id
-	hash := crypto.HashBytes(fastrand.Bytes(10))
-	sig := crypto.SignHash(hash, account.staticSecretKey)
-	err = crypto.VerifyHash(hash, account.staticID.SPK().ToPublicKey(), sig)
-	if err != nil {
-		t.Fatal("Invalid secret key")
-	}
-}
-
-// TestNewWithdrawalMessage verifies the newWithdrawalMessage helper
-// properly instantiates all required fields on the WithdrawalMessage
-func TestNewWithdrawalMessage(t *testing.T) {
-	// create a withdrawal message using random parameters
-	aid, _ := modules.NewAccountID()
-	amount := types.NewCurrency64(fastrand.Uint64n(100))
-	blockHeight := types.BlockHeight(fastrand.Intn(100))
-	msg := newWithdrawalMessage(aid, amount, blockHeight)
-
-	// validate the withdrawal message
-	if msg.Account != aid {
-		t.Fatal("Unexpected account ID")
-	}
-	if !msg.Amount.Equals(amount) {
-		t.Fatal("Unexpected amount")
-	}
-	if msg.Expiry != blockHeight+withdrawalValidityPeriod {
-		t.Fatal("Unexpected expiry")
-	}
-	if len(msg.Nonce) != modules.WithdrawalNonceSize {
-		t.Fatal("Unexpected nonce length")
-	}
-	var nonce [modules.WithdrawalNonceSize]byte
-	if bytes.Equal(msg.Nonce[:], nonce[:]) {
-		t.Fatal("Uninitialized nonce")
 	}
 }
 
