d4720d34bdc57ac2c580a4a6bd1e2b009a107139
Filip Rysavy <fil@nebulous.tech>
2020-05-13 10:31:41 +0200

    Split TestSkykeyCommands to subtests

--- cmd/siac/skykeycmd_test.go
+++ cmd/siac/skykeycmd_test.go
@@ -9,9 +9,12 @@ import (
 	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/node"
+	"gitlab.com/NebulousLabs/Sia/node/api/client"
 	"gitlab.com/NebulousLabs/Sia/siatest"
 	"gitlab.com/NebulousLabs/Sia/skykey"
 )
+
+var testSkykeyString = "BAAAAAAAAABrZXkxAAAAAAAAAAQgAAAAAAAAADiObVg49-0juJ8udAx4qMW-TEHgDxfjA0fjJSNBuJ4a"
 
 // TestSkykeyCommands tests the basic functionality of the siac skykey commands
 // interface. More detailed testing of the skykey manager is done in the skykey
@@ -99,6 +102,126 @@ func TestSkykeyCommands(t *testing.T) {
 	}
 	// Using neither name or id param should return an error
 	_, err = skykeyGet(n.Client, "", "")
+	if err == nil {
+		t.Fatal("Expected error when using neither name or id params")
+	}
+}
+
+//xxxqqq
+func TestSkykeyCommandsXXX(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	//xxxccc move to local variable after !4387 is merged
+	httpClient = newTestingClient(t)
+
+	// Set the (global) cipher type to the only allowed type.
+	// This is normally done by the flag parser.
+	skykeyCipherType = "XChaCha20"
+
+	t.Run("TestDuplicateSkykeyAdd", testDuplicateSkykeyAdd)
+	t.Run("TestChangeKeyEntropyKeepName", testChangeKeyEntropyKeepName)
+	t.Run("TestAddKeyTwice", testAddKeyTwice)
+	t.Run("TestInvalidCipherType", testInvalidCipherType)
+	t.Run("TestSkykeyGet", testSkykeyGet)
+	t.Run("TestUsingNameAndID", testUsingNameAndID)
+}
+
+//xxxqqq
+func newTestingClient(t *testing.T) client.Client {
+	// Create a node for the test
+	n, err := siatest.NewNode(node.AllModules(build.TempDir(t.Name())))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return n.Client
+}
+
+func testDuplicateSkykeyAdd(t *testing.T) {
+	err := skykeyAdd(testSkykeyString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = skykeyAdd(testSkykeyString)
+	if !strings.Contains(err.Error(), skykey.ErrSkykeyWithIDAlreadyExists.Error()) {
+		t.Fatal("Expected duplicate name error but got", err)
+	}
+}
+
+func testChangeKeyEntropyKeepName(t *testing.T) {
+	// Change the key entropy, but keep the same name.
+	var sk skykey.Skykey
+	err := sk.FromString(testSkykeyString)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sk.Entropy[0] ^= 1 // flip the first byte.
+
+	skString, err := sk.ToString()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// This should return a duplicate name error.
+	err = skykeyAdd(skString)
+	if !strings.Contains(err.Error(), skykey.ErrSkykeyWithNameAlreadyExists.Error()) {
+		t.Fatal("Expected duplicate name error", err)
+	}
+}
+
+func testAddKeyTwice(t *testing.T) {
+	// Check that adding same key twice returns an error.
+	keyName := "createkey1"
+	_, err := skykeyCreate(keyName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = skykeyCreate(keyName)
+	if !strings.Contains(err.Error(), skykey.ErrSkykeyWithNameAlreadyExists.Error()) {
+		t.Fatal("Expected error when creating key with same name")
+	}
+}
+
+func testInvalidCipherType(t *testing.T) {
+	// Check that invalid cipher types are caught.
+	//xxxqqq skykeyCipherType is global, should not be global
+	skykeyCipherType = "InvalidCipherType"
+	_, err := skykeyCreate("createkey2")
+	if !errors.Contains(err, crypto.ErrInvalidCipherType) {
+		t.Fatal("Expected error when creating key with invalid ciphertype")
+	}
+	skykeyCipherType = "XChaCha20" //reset the ciphertype
+}
+
+func testSkykeyGet(t *testing.T) {
+	keyName := "createkey testSkykeyGet"
+	newSkykey, err := skykeyCreate(keyName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Test skykeyGet
+	// known key should have no errors.
+	getKeyStr, err := skykeyGet(keyName, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if getKeyStr != newSkykey {
+		t.Fatal("Expected keys to match")
+	}
+}
+
+func testUsingNameAndID(t *testing.T) {
+	// Using both name and id params should return an error
+	_, err := skykeyGet("name", "id")
+	if err == nil {
+		t.Fatal("Expected error when using both name and id")
+	}
+	// Using neither name or id param should return an error
+	_, err = skykeyGet("", "")
 	if err == nil {
 		t.Fatal("Expected error when using neither name or id params")
 	}
