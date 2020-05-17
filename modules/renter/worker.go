9a7973c980f7922e6681212d89b0c0df2c2aa10b
David Vorick <david@nebulouslabs.com>
2020-05-17 13:50:31 -0400

    switch worker to lockless cache

--- modules/renter/worker.go
+++ modules/renter/worker.go
@@ -27,22 +27,12 @@ package renter
 import (
 	"sync"
 	"time"
+	"unsafe"
 
-	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/types"
 
 	"gitlab.com/NebulousLabs/errors"
-)
-
-var (
-	// workerCacheUpdateFrequency specifies how much time must pass before the
-	// worker updates its cache.
-	workerCacheUpdateFrequency = build.Select(build.Var{
-		Dev:      time.Second * 5,
-		Standard: time.Minute,
-		Testing:  time.Second,
-	}).(time.Duration)
 )
 
 // A worker listens for work on a certain host.
@@ -59,14 +49,14 @@ var (
 // uploading and downloading with flaky hosts in the worker sets has
 // substantially reduced overall performance and throughput.
 type worker struct {
+	// atomicCache contains a pointer to the latest cache in the worker.
+	// Atomics are used to minimze lock contention on the worker object.
+	atomicCache                  unsafe.Pointer // points to a workerCache object
+
 	// The host pub key also serves as an id for the worker, as there is only
 	// one worker per host.
 	staticHostPubKey    types.SiaPublicKey
 	staticHostPubKeyStr string
-
-	// Cached value for the contract utility, updated infrequently.
-	cachedContractID      types.FileContractID
-	cachedContractUtility modules.ContractUtility
 
 	// Download variables related to queuing work. They have a separate mutex to
 	// minimize lock contention.
@@ -120,10 +110,11 @@ func (w *worker) status() modules.WorkerStatus {
 		w.staticAccount.managedAvailableBalance()
 	}
 
+	cache := w.staticCache()
 	return modules.WorkerStatus{
 		// Contract Information
-		ContractID:      w.cachedContractID,
-		ContractUtility: w.cachedContractUtility,
+		ContractID:      cache.staticContractID,
+		ContractUtility: cache.staticContractUtility,
 		HostPubKey:      w.staticHostPubKey,
 
 		// Download information
@@ -174,24 +165,6 @@ func (w *worker) managedBlockUntilReady() bool {
 		case <-time.After(offlineCheckFrequency):
 		}
 	}
-	return true
-}
-
-// managedUpdateCache will check how recently each of the cached values of the
-// worker has been updated and update anything that is not recent enough.
-//
-// 'false' will be returned if the cache cannot be updated, signaling that the
-// worker should exit.
-func (w *worker) managedUpdateCache() bool {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-
-	renterContract, exists := w.renter.hostContractor.ContractByPublicKey(w.staticHostPubKey)
-	if !exists {
-		return false
-	}
-	w.cachedContractID = renterContract.ID
-	w.cachedContractUtility = renterContract.Utility
 	return true
 }
 
@@ -355,7 +328,7 @@ func (r *Renter) newWorker(hostPubKey types.SiaPublicKey) (*worker, error) {
 	}
 	// Get the worker cache set up before returning the worker. This prevents a
 	// race condition in some tests.
-	if !w.managedUpdateCache() {
+	if !w.staticTryUpdateCache() {
 		return nil, errors.New("unable to build cache for worker")
 	}
 	return w, nil
---
+++ modules/renter/workercache.go
@@ -0,0 +1,87 @@
+package renter
+
+import (
+	"sync/atomic"
+	"time"
+	"unsafe"
+
+	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/types"
+)
+
+var (
+	// workerCacheUpdateFrequency specifies how much time must pass before the
+	// worker updates its cache.
+	workerCacheUpdateFrequency = build.Select(build.Var{
+		Dev:      time.Second * 5,
+		Standard: time.Minute,
+		Testing:  time.Second,
+	}).(time.Duration)
+)
+
+type (
+	// workerCache contains all of the cached values for the worker. Every field
+	// must be static because this object is saved and loaded using
+	// atomic.Pointer.
+	workerCache struct {
+		staticBlockHeight     types.BlockHeight
+		staticContractID      types.FileContractID
+		staticContractUtility modules.ContractUtility
+		staticHostVersion     string
+		staticSynced          bool
+
+		staticLastUpdate time.Time
+	}
+)
+
+// staticUpdateCache will perform a cache update on the worker.
+//
+// 'false' will be returned if the cache cannot be updated, signaling that the
+// worker should exit.
+//
+// TODO: When updating the block height, take into account whether or not we are
+// synced. Might make sense to add a staticSynced variable to the workerCache.
+func (w *worker) staticTryUpdateCache() bool {
+	// Check if an update is necessary. If not, return success.
+	cache := w.staticCache()
+	if cache != nil && time.Since(cache.staticLastUpdate) < workerCacheUpdateFrequency {
+		return true
+	}
+
+	// Grab the host to check the version.
+	host, ok, err := w.renter.hostDB.Host(w.staticHostPubKey)
+	if !ok || err != nil {
+		w.renter.log.Printf("Worker %v could not update the cache, hostdb found host with %v and %v values", w.staticHostPubKeyStr, ok, err)
+		return false
+	}
+
+	// Grab the renter contract from the host contractor.
+	renterContract, exists := w.renter.hostContractor.ContractByPublicKey(w.staticHostPubKey)
+	if !exists {
+		w.renter.log.Printf("Worker %v could not update the cache, host not found in contractor", w.staticHostPubKeyStr)
+		return false
+	}
+
+	// Create the cache object.
+	cache = &workerCache{
+		staticBlockHeight:     w.renter.cs.Height(),
+		staticContractID:      renterContract.ID,
+		staticContractUtility: renterContract.Utility,
+		staticHostVersion:     host.Version,
+		staticSynced:          w.renter.cs.Synced(),
+
+		staticLastUpdate: time.Now(),
+	}
+
+	// Atomically store the cache object in the worker.
+	ptr := unsafe.Pointer(cache)
+	atomic.StorePointer(&w.atomicCache, ptr)
+	return true
+}
+
+// staticCache returns the current worker cache object.
+func (w *worker) staticCache() *workerCache {
+	ptr := atomic.LoadPointer(&w.atomicCache)
+	return (*workerCache)(ptr)
+}
