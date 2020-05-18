f4c947d913303f6f3fd0af72317c625db09ef146
David Vorick <david@nebulouslabs.com>
2020-05-18 00:17:22 -0400

    implementation of managedExecuteProgram in renter

--- modules/renter/workerrpc.go
+++ modules/renter/workerrpc.go
@@ -1,11 +1,126 @@
 package renter
 
 import (
+	"io"
+
 	"gitlab.com/NebulousLabs/Sia/build"
 	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/types"
+
 	"gitlab.com/NebulousLabs/errors"
 	"gitlab.com/NebulousLabs/siamux"
 )
+
+// programResponse is a helper struct that wraps the RPCExecuteProgramResponse
+// alongside the data output
+type programResponse struct {
+	modules.RPCExecuteProgramResponse
+	Output []byte
+}
+
+// managedExecuteProgram performs the ExecuteProgramRPC on the host
+func (w *worker) managedExecuteProgram(p modules.Program, data []byte, fcid types.FileContractID, cost types.Currency) (responses []programResponse, err error) {
+	// check host version
+	cache := w.staticCache()
+	if build.VersionCmp(cache.staticHostVersion, minAsyncVersion) < 0 {
+		build.Critical("Executing new RHP RPC on host with version", cache.staticHostVersion)
+	}
+
+	// track the withdrawal
+	// TODO: this is very naive and does not consider refunds at all
+	w.staticAccount.managedTrackWithdrawal(cost)
+	defer func() {
+		w.staticAccount.managedCommitWithdrawal(cost, err == nil)
+	}()
+
+	// create a new stream
+	var stream siamux.Stream
+	stream, err = w.staticNewStream()
+	if err != nil {
+		err = errors.AddContext(err, "Unable to create a new stream")
+		return
+	}
+	defer func() {
+		if err := stream.Close(); err != nil {
+			w.renter.log.Println("ERROR: failed to close stream", err)
+		}
+	}()
+
+	// grab some variables from the worker
+	bh := cache.staticBlockHeight
+
+	// write the specifier
+	err = modules.RPCWrite(stream, modules.RPCExecuteProgram)
+	if err != nil {
+		return
+	}
+
+	// send price table uid
+	pt := w.staticPriceTable().staticPriceTable
+	err = modules.RPCWrite(stream, pt.UID)
+	if err != nil {
+		return
+	}
+
+	// prepare the request.
+	epr := modules.RPCExecuteProgramRequest{
+		FileContractID:    fcid,
+		Program:           p,
+		ProgramDataLength: uint64(len(data)),
+	}
+
+	// provide payment
+	err = w.staticAccount.ProvidePayment(stream, w.staticHostPubKey, modules.RPCUpdatePriceTable, cost, w.staticAccount.staticID, bh)
+	if err != nil {
+		return
+	}
+
+	// send the execute program request.
+	err = modules.RPCWrite(stream, epr)
+	if err != nil {
+		return
+	}
+
+	// send the programData.
+	_, err = stream.Write(data)
+	if err != nil {
+		return
+	}
+
+	// TODO we call this manually here in order to save a RT and write the
+	// program instructions and data before reading. Remove when !4446 is
+	// merged.
+
+	// receive PayByEphemeralAccountResponse
+	var payByResponse modules.PayByEphemeralAccountResponse
+	err = modules.RPCRead(stream, &payByResponse)
+	if err != nil {
+		return
+	}
+
+	// read the responses.
+	responses = make([]programResponse, len(epr.Program))
+	for i := range responses {
+		err = modules.RPCRead(stream, &responses[i])
+		if err != nil {
+			return
+		}
+
+		// Read the output data.
+		outputLen := responses[i].OutputLength
+		responses[i].Output = make([]byte, outputLen, outputLen)
+		_, err = io.ReadFull(stream, responses[i].Output)
+		if err != nil {
+			return
+		}
+
+		// If the response contains an error we are done.
+		if responses[i].Error != nil {
+			break
+		}
+	}
+	return
+}
 
 // staticNewStream returns a new stream to the worker's host
 func (w *worker) staticNewStream() (siamux.Stream, error) {
