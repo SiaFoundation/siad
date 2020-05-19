621a3fca9a4fc6a2f9e2361c8ad77701e0bf6941
Marcin S <scatman@bu.edu>
2020-05-19 14:27:05 +0200

    Address review comments

--- persist/log.go
+++ persist/log.go
@@ -15,12 +15,11 @@ type Logger struct {
 var (
 	// options contains log options with Sia- and build-specific information.
 	options = log.Options{
-		BinaryName:      "Sia",
-		BugReportURL:    build.IssuesURL,
-		Debug:           build.Release == "testing",
-		PanicOnCritical: build.DEBUG,
-		Release:         build.Release,
-		Version:         build.Version,
+		BinaryName:   "Sia",
+		BugReportURL: build.IssuesURL,
+		Debug:        build.DEBUG,
+		Release:      build.Release,
+		Version:      build.Version,
 	}
 )
 
