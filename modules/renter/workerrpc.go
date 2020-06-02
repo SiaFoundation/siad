e4408c7c5b895328ad1211c2948d9ad7cfd58fd2
PJ <peterjan.brone@gmail.com>
2020-06-02 17:36:24 +0200

    Read the cancellation token

--- modules/renter/workerrpc.go
+++ modules/renter/workerrpc.go
@@ -84,6 +84,13 @@ func (w *worker) managedExecuteProgram(p modules.Program, data []byte, fcid type
 		return
 	}
 
+	// read the cancellation token.
+	ct := make([]byte, modules.MDMCancellationTokenLen)
+	_, err = io.ReadFull(stream, ct)
+	if err != nil {
+		return
+	}
+
 	// read the responses.
 	responses = make([]programResponse, len(epr.Program))
 	for i := range responses {
