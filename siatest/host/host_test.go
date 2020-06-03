44b42c78f1631e613c8ad75b0cf8739557f3f91f
Chris Schinnerl <chris@nebulous.tech>
2020-06-03 18:04:57 +0200

    fix TestHostContracts

--- siatest/host/host_test.go
+++ siatest/host/host_test.go
@@ -312,9 +312,7 @@ func TestHostContracts(t *testing.T) {
 		t.Fatal("expected host to have no contracts")
 	}
 
-	r := node.RenterTemplate
-	r.RenterDeps = &dependencies.DependencyPreventEARefill{}
-	if _, err := tg.AddNodes(r); err != nil {
+	if _, err := tg.AddNodes(node.RenterTemplate); err != nil {
 		t.Fatal(err)
 	}
 
@@ -368,8 +366,8 @@ func TestHostContracts(t *testing.T) {
 		t.Fatal("valid payout should be greater than old valid payout")
 	}
 
-	if hc.Contracts[0].MissedProofOutputs[1].Value.Cmp(prevMissPayout) != -1 {
-		t.Fatal("missed payout should be less than old missed payout")
+	if cmp := hc.Contracts[0].MissedProofOutputs[1].Value.Cmp(prevMissPayout); cmp != 1 {
+		t.Fatal("missed payout should be more than old missed payout", cmp)
 	}
 }
 
