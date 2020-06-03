bd920e935b6d4ab35f62ec60e377f445c02d2b68
PJ <peterjan.brone@gmail.com>
2020-06-03 10:13:29 +0200

    Fix faulty ct write

--- modules/host/host_test.go
+++ modules/host/host_test.go
@@ -435,7 +435,7 @@ func (p *renterHostPair) managedExecuteProgram(epr modules.RPCExecuteProgramRequ
 
 	// Read the cancellation token.
 	ct := make([]byte, modules.MDMCancellationTokenLen)
-	_, err = io.ReadFull(stream, ct)
+	err = modules.RPCRead(stream, &ct)
 	if err != nil {
 		return nil, limit, err
 	}
