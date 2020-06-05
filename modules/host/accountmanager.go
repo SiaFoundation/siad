a9ea429e94866cac374bdb9fe94e8e096aa271ba
PJ <peterjan.brone@gmail.com>
2020-06-05 16:54:09 +0200

    Improve copy and naming

--- modules/host/accountmanager.go
+++ modules/host/accountmanager.go
@@ -125,11 +125,11 @@ type (
 		// not fully synced, or when it goes out of sync.
 		withdrawalsInactive bool
 
-		// rotatingFingerprints indicates whether the account manager is in the
-		// process of rotating the fingerprint buckets on disk, we use this to
-		// ensure concurrent calls to `callConsensusChanged` don't cause the
-		// rotation to fail
-		rotatingFingerprints bool
+		// rotatingFingerprintsOnDisk indicates whether the account manager is
+		// in the process of rotating the fingerprint buckets on disk, we use
+		// this to ensure concurrent calls to `callConsensusChanged` that
+		// trigger a rotation don't cause the ongoing rotation to fail
+		rotatingFingerprintsOnDisk bool
 
 		mu sync.Mutex
 		h  *Host
@@ -436,24 +436,25 @@ func (am *accountManager) callConsensusChanged(cc modules.ConsensusChange, oldHe
 	}
 
 	// If the host is rotating fingerprints on disk already, we do not want to
-	// rotate them. This should never happen, which is why this is a critical
-	// event.
-	if am.rotatingFingerprints {
-		// we manually unset this to ensure the host never finds itself in a
-		// deadlock situation where this flag prevents the rotation from
-		// occurring
-		am.rotatingFingerprints = false
-
+	// trigger the rotation again in rapid succession. This should never happen,
+	// which is why this is a critical event.
+	//
+	// Note that we will also manually unset this flag to ensure the host never
+	// finds itself in a deadlock type situation where this flag prevents the
+	// rotation from occurring in perpetuity. Such an event is devastating to
+	// the host as withdrawals would be deactivated.
+	if am.rotatingFingerprintsOnDisk {
+		am.rotatingFingerprintsOnDisk = false
 		am.h.log.Critical("ERROR: `callConsensusChanged` called twice in extremely rapid succession, this should never happen and bears investigation")
 		return
 	}
 
 	// Rotate fingerprint buckets on disk
-	am.rotatingFingerprints = true
+	am.rotatingFingerprintsOnDisk = true
 	am.mu.Unlock()
 	errRotate := am.staticAccountsPersister.callRotateFingerprintBuckets()
 	am.mu.Lock()
-	am.rotatingFingerprints = false
+	am.rotatingFingerprintsOnDisk = false
 
 	// Rotate in memory only if the on-disk rotation succeeded
 	if errRotate == nil {
