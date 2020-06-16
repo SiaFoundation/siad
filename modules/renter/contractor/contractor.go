e13eb93960060ad5bb0a4ed48a09abf1fac6e7c0
PJ <peterjan.brone@gmail.com>
2020-06-16 14:51:22 +0200

    Add TestRevisionNumberSync

--- modules/renter/contractor/contractor.go
+++ modules/renter/contractor/contractor.go
@@ -269,9 +269,11 @@ func (c *Contractor) ProvidePayment(stream siamux.Stream, host types.SiaPublicKe
 	}
 
 	// commit payment intent
-	err = sc.CommitPaymentIntent(walTxn, signedTxn, amount, rpc)
-	if err != nil {
-		return errors.AddContext(err, "Failed to commit unknown spending intent")
+	if !c.staticDeps.Disrupt("DisableCommitPaymentIntent") {
+		err = sc.CommitPaymentIntent(walTxn, signedTxn, amount, rpc)
+		if err != nil {
+			return errors.AddContext(err, "Failed to commit unknown spending intent")
+		}
 	}
 
 	return nil
--- modules/renter/workertools_test.go
+++ modules/renter/workertools_test.go
@@ -1,6 +1,60 @@
 package renter
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/siatest/dependencies"
+	"gitlab.com/NebulousLabs/errors"
+)
+
+// TestRevisionNumberSync is a unit test that verifies if the revision number
+// fix is attempted and whether it properly resync the revision.
+func TestRevisionNumberSync(t *testing.T) {
+	t.Parallel()
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	deps := dependencies.NewDependencyDisableCommitPaymentIntent()
+	wt, err := newWorkerTesterCustomDependency(t.Name(), deps)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := wt.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+	w := wt.worker
+
+	// wait until our dependency got triggered
+	err = build.Retry(100, 100*time.Millisecond, func() error {
+		if deps.Occurrences() == 0 {
+			return errors.New("commit payment intent not interrupted")
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// wait until we have a valid pricetable
+	err = build.Retry(100, 100*time.Millisecond, func() error {
+		if !w.staticPriceTable().staticValid() {
+			return errors.New("price table not updated yet")
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// if we reach this point we have verified the attempted revision fix took
+	// place and was successful
+}
 
 // TestSuspectRevisionNumberMismatchFlag is a small unit test that verifes the
 // methods involved in setting and unsetting the SuspectRevisionNumberMismatch
