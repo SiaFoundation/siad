f6a8bd79f682239d67b6787cdb2b0f0597f6cfa6
PJ <peterjan.brone@gmail.com>
2020-06-18 11:52:39 +0200

    Extend TestUseHostBlockHeight to check if we throw build.Critical

--- modules/renter/workerrpc_test.go
+++ modules/renter/workerrpc_test.go
@@ -1,6 +1,7 @@
 package renter
 
 import (
+	"fmt"
 	"strings"
 	"sync/atomic"
 	"testing"
@@ -43,6 +44,7 @@ func TestUseHostBlockHeight(t *testing.T) {
 
 	// manually corrupt the price table's host blockheight
 	wpt := w.staticPriceTable()
+	hbh := wpt.staticPriceTable.HostBlockHeight // save host blockheight
 	var pt modules.RPCPriceTable
 	err = encoding.Unmarshal(encoding.Marshal(wpt.staticPriceTable), &pt)
 	if err != nil {
@@ -58,9 +60,15 @@ func TestUseHostBlockHeight(t *testing.T) {
 	w.staticSetPriceTable(wptc)
 
 	// manually corrupt the synced property on the worker's cache
-	cache := w.staticCache()
-	cache.staticSynced = false
-	ptr := unsafe.Pointer(cache)
+	wc := w.staticCache()
+	ptr := unsafe.Pointer(&workerCache{
+		staticBlockHeight:     wc.staticBlockHeight,
+		staticContractID:      wc.staticContractID,
+		staticContractUtility: wc.staticContractUtility,
+		staticHostVersion:     wc.staticHostVersion,
+		staticSynced:          false,
+		staticLastUpdate:      wc.staticLastUpdate,
+	})
 	atomic.StorePointer(&w.atomicCache, ptr)
 
 	// create a dummy has sector program
@@ -77,4 +85,40 @@ func TestUseHostBlockHeight(t *testing.T) {
 	if err == nil || !strings.Contains(err.Error(), "ephemeral account withdrawal message expires too far into the future") {
 		t.Fatal("Unexpected error", err)
 	}
+
+	// manually reset the host blockheight on the price table
+	wpt = w.staticPriceTable()
+	err = encoding.Unmarshal(encoding.Marshal(wpt.staticPriceTable), &pt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pt.HostBlockHeight = hbh
+	wptc = new(workerPriceTable)
+	wptc.staticConsecutiveFailures = wpt.staticConsecutiveFailures
+	wptc.staticExpiryTime = wpt.staticExpiryTime
+	wptc.staticUpdateTime = wpt.staticUpdateTime
+	wptc.staticPriceTable = pt
+	w.staticSetPriceTable(wptc)
+
+	// manually corrupt the cache and increase our blockheight, this should
+	// trigger a build.Critical when we try and use the host's blockheight
+	wc = w.staticCache()
+	ptr = unsafe.Pointer(&workerCache{
+		staticBlockHeight:     hbh + priceTableHostBlockHeightLeeWay + 1,
+		staticContractID:      wc.staticContractID,
+		staticContractUtility: wc.staticContractUtility,
+		staticHostVersion:     wc.staticHostVersion,
+		staticSynced:          false,
+		staticLastUpdate:      wc.staticLastUpdate,
+	})
+	atomic.StorePointer(&w.atomicCache, ptr)
+
+	defer func() {
+		r := recover()
+		if r == nil || !strings.Contains(fmt.Sprintf("%v", r), "blockheight is significantly lower") {
+			t.Error("Expected build.Critical")
+			t.Log(r)
+		}
+	}()
+	w.managedExecuteProgram(p, data, types.FileContractID{}, cost)
 }
