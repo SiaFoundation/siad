ce1ca3503329405c0990a8668308fe75c8e9ed69
Christopher Schinnerl <ch.schinnerl@gmail.com>
2020-07-06 16:36:27 +0200

    don't update threadgroup

--- modules/consensus/persist.go
+++ modules/consensus/persist.go
@@ -75,13 +75,18 @@ func (cs *ConsensusSet) initPersist() error {
 		return err
 	}
 	// Set up closing the logger.
-	cs.tg.AfterStop(func() {
+	err = cs.tg.AfterStop(func() error {
 		err := cs.log.Close()
 		if err != nil {
 			// State of the logger is unknown, a println will suffice.
 			fmt.Println("Error shutting down consensus set logger:", err)
+			return err
 		}
+		return nil
 	})
+	if err != nil {
+		return err
+	}
 
 	// Try to load an existing database from disk - a new one will be created
 	// if one does not exist.
@@ -90,11 +95,16 @@ func (cs *ConsensusSet) initPersist() error {
 		return err
 	}
 	// Set up the closing of the database.
-	cs.tg.AfterStop(func() {
+	err = cs.tg.AfterStop(func() error {
 		err := cs.db.Close()
 		if err != nil {
 			cs.log.Println("ERROR: Unable to close consensus set database at shutdown:", err)
+			return err
 		}
+		return nil
 	})
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- modules/renter/contractor/contractor.go
+++ modules/renter/contractor/contractor.go
@@ -2,7 +2,6 @@ package contractor
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -49,7 +48,7 @@ type Contractor struct {
 	persistDir    string
 	staticAlerter *modules.GenericAlerter
 	staticDeps    modules.Dependencies
-	tg            siasync.ThreadGroup
+	tg            threadgroup.ThreadGroup
 	tpool         modules.TransactionPool
 	wallet        modules.Wallet
 
@@ -457,17 +456,21 @@ func contractorBlockingStartup(cs modules.ConsensusSet, w modules.Wallet, tp mod
 	c.staticWatchdog = newWatchdog(c)
 
 	// Close the contract set and logger upon shutdown.
-	c.tg.AfterStop(func() {
+	err := c.tg.AfterStop(func() error {
 		if err := c.staticContracts.Close(); err != nil {
-			c.log.Println("Failed to close contract set:", err)
+			return errors.AddContext(err, "failed to close contract set")
 		}
 		if err := c.log.Close(); err != nil {
-			fmt.Println("Failed to close the contractor logger:", err)
+			return errors.AddContext(err, "failed to close the contractor logger")
 		}
+		return nil
 	})
+	if err != nil {
+		return nil, err
+	}
 
 	// Load the prior persistence structures.
-	err := c.load()
+	err = c.load()
 	if err != nil && !os.IsNotExist(err) {
 		return nil, err
 	}
@@ -476,9 +479,13 @@ func contractorBlockingStartup(cs modules.ConsensusSet, w modules.Wallet, tp mod
 	c.managedUpdatePubKeyToContractIDMap()
 
 	// Unsubscribe from the consensus set upon shutdown.
-	c.tg.OnStop(func() {
+	err = c.tg.OnStop(func() error {
 		cs.Unsubscribe(c)
+		return nil
 	})
+	if err != nil {
+		return nil, err
+	}
 
 	// We may have upgraded persist or resubscribed. Save now so that we don't
 	// lose our work.
