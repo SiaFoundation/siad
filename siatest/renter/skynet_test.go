b9f5061831fa54c01a7ea38ebef1c6ad03b13e8a
PJ <peterjan.brone@gmail.com>
2020-07-13 20:06:04 +0200

    Move skynet helpers

--- siatest/renter/skynet_test.go
+++ siatest/renter/skynet_test.go
@@ -2,7 +2,6 @@ package renter
 
 import (
 	"archive/tar"
-	"archive/zip"
 	"bytes"
 	"compress/gzip"
 	"encoding/hex"
@@ -2863,72 +2862,4 @@ func testSkynetSingleFileNoSubfiles(t *testing.T, tg *siatest.TestGroup) {
 	if metadata.Subfiles != nil {
 		t.Fatal("Expected empty subfiles on download, got", sup.FileMetadata.Subfiles)
 	}
-}
-
-// fileMap is a helper type that maps filenames onto the raw file data
-type fileMap map[string][]byte
-
-// readTarArchive is a helper function that takes a reader containing a tar
-// archive and returns an fileMap, which is a small helper struct that maps the
-// filename to the data.
-func readTarArchive(r io.Reader) (fileMap, error) {
-	a := make(fileMap)
-	tr := tar.NewReader(r)
-	for {
-		header, err := tr.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-		data, err := ioutil.ReadAll(tr)
-		if err != nil {
-			return nil, err
-		}
-		a[header.Name] = data
-	}
-	return a, nil
-}
-
-// readZipArchive is a helper function that takes a reader containing a zip
-// archive and returns an fileMap, which is a small helper struct that maps the
-// filename to the data.
-func readZipArchive(r io.Reader) (fileMap, error) {
-	a := make(fileMap)
-
-	// copy all data to a buffer (this is necessary because the zipreader
-	// expects a `ReaderAt`)
-	buff := bytes.NewBuffer([]byte{})
-	size, err := io.Copy(buff, r)
-	if err != nil {
-		return nil, err
-	}
-	reader := bytes.NewReader(buff.Bytes())
-	zr, err := zip.NewReader(reader, size)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, f := range zr.File {
-		rc, err := f.Open()
-		if err != nil {
-			return nil, err
-		}
-
-		data, err := ioutil.ReadAll(rc)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		err = rc.Close()
-		if err != nil {
-			return nil, err
-		}
-		a[f.FileHeader.Name] = data
-	}
-	return a, nil
 }
