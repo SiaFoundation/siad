abaf4d4117b2c9886c5b7013105978f34cde7cdc
PJ <peterjan.brone@gmail.com>
2020-08-11 10:47:00 +0200

    Upgrade host persistence to fix EphemeralAccountExpiry field

---
+++ modules/host/persist_compat_1.5.1.go
@@ -0,0 +1,51 @@
+package host
+
+import (
+	"path/filepath"
+
+	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/modules"
+)
+
+// upgradeFromV143ToV151 is an upgrade layer that fixes a bug in the host's
+// settings which got introduced when EphemeralAccountExpiry got moved from a
+// uint64 to a time.Duration. At that time, no compat was added, resulting in a
+// persist value in seconds, that is being interpreted as nanoseconds.
+func (h *Host) upgradeFromV143ToV151() error {
+	h.log.Println("Attempting an upgrade for the host from v1.4.3 to v1.5.1")
+
+	// Load the persistence object
+	p := new(persistence)
+	err := h.dependencies.LoadFile(modules.Hostv143PersistMetadata, p, filepath.Join(h.persistDir, settingsFile))
+	if err != nil {
+		return build.ExtendErr("could not load persistence object", err)
+	}
+
+	// The persistence object for hosts that upgraded to v1.5.0 (so non-new
+	// hosts) will have the EphemeralAccountExpiry persisted in seconds, and
+	// wrongfully interpreted as a time.Duration, expressed in nanoseconds.
+	//
+	// We fix this by updating the field to the default value, but only if the
+	// value in the persistence object wasn't manually altered to zero
+	if shouldResetEphemeralAccountExpiry(p.Settings) {
+		p.Settings.EphemeralAccountExpiry = modules.DefaultEphemeralAccountExpiry // reset to default
+	}
+
+	// Load it on the host
+	h.loadPersistObject(p)
+
+	// Save the updated persist so that the upgrade is not triggered again.
+	err = h.saveSync()
+	if err != nil {
+		return build.ExtendErr("could not save persistence object", err)
+	}
+
+	return nil
+}
+
+// shouldResetEphemeralAccountExpiry is a helper function that returns true if
+// the given settings contain a value for the `EphemeralAccountExpiry` field
+// that needs to be reset. Extracted for unit testing purposes.
+func shouldResetEphemeralAccountExpiry(his modules.HostInternalSettings) bool {
+	return his.EphemeralAccountExpiry != modules.DefaultEphemeralAccountExpiry && his.EphemeralAccountExpiry.Nanoseconds() != 0
+}
---
+++ modules/host/persist_compat_1.5.1_test.go
@@ -0,0 +1,145 @@
+package host
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"gitlab.com/NebulousLabs/Sia/build"
+	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/persist"
+	"gitlab.com/NebulousLabs/log"
+	"gitlab.com/NebulousLabs/siamux"
+	"gitlab.com/NebulousLabs/siamux/mux"
+)
+
+const (
+	// v143Host is the name of the file that contains the legacy host
+	// persistence directory testdata.
+	v143Host = "v143Host.tar.gz"
+
+	// v143HostDir is the name of the directory containing the v143Host
+	// test data.
+	v143HostDir = "v143Host"
+)
+
+// TestV143HostUpgrade creates a host with a legacy persistence file,
+// and then attempts to upgrade.
+func TestV143HostUpgrade(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	// create a tmp dir and ensure it's empty
+	persistDir := build.TempDir(t.Name())
+	err := os.RemoveAll(persistDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// extract the legacy host data
+	hostDir := build.TempDir(persistDir, modules.HostDir)
+	source := filepath.Join("testdata", v143Host)
+	err = build.ExtractTarGz(source, hostDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// copy its persistence file into the appropriate location
+	src := filepath.Join(hostDir, v143HostDir, settingsFile)
+	dst := filepath.Join(hostDir, settingsFile)
+	err = loadHostPersistenceFile(src, dst, hostDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// load the persistence, to extract the public and secret keys for the
+	// siamux but also to verify we're using the correct testdata
+	hp := persistence{}
+	err = persist.LoadJSON(modules.Hostv143PersistMetadata, &hp, dst)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create a siamux using the compat flow to ensure we save it using the
+	// host's key pair, this simulates how it would be on production (without
+	// having to add siamux testdata in the host module)
+	siaMuxDir := filepath.Join(persistDir, modules.SiaMuxDir)
+	err = os.MkdirAll(siaMuxDir, persist.DefaultDiskPermissionsTest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// convert the keypair into a mux.ED25519 key pair
+	smPK := modules.SiaPKToMuxPK(hp.PublicKey)
+	var smSK mux.ED25519SecretKey
+	copy(smSK[:], hp.SecretKey[:])
+
+	mux, err := siamux.CompatV1421NewWithKeyPair("localhost:0", "localhost:0", log.DiscardLogger, siaMuxDir, smSK, smPK)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = mux.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// load a new host
+	closefn, host, err := loadExistingHostWithNewDeps(persistDir, siaMuxDir, hostDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := closefn()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// verify the upgrade properly fixed the EphemeralAccountExpiry
+	his := host.InternalSettings()
+	if his.EphemeralAccountExpiry != modules.DefaultEphemeralAccountExpiry {
+		t.Fatal("EphemeralAccountExpiry not properly fixed after upgrade")
+	}
+
+	// sanity check the metadata version
+	err = persist.LoadJSON(modules.Hostv151PersistMetadata, struct{}{}, filepath.Join(hostDir, modules.HostSettingsFile))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// TestShouldResetEphemeralAccountExpiry is a unit test that covers the
+// functionality of `shouldResetEphemeralAccountExpiry`
+func TestShouldResetEphemeralAccountExpiry(t *testing.T) {
+	t.Parallel()
+
+	oneWeekInSeconds := 60 * 60 * 24 * 7
+
+	// legacy default (seconds interpreted in nanoseconds)
+	his := modules.HostInternalSettings{}
+	his.EphemeralAccountExpiry = time.Duration(oneWeekInSeconds)
+	if !shouldResetEphemeralAccountExpiry(his) {
+		t.Fatal("Unexpected outcome of `shouldResetEphemeralAccountExpiry`")
+	}
+
+	// altered to zero, (seconds interpreted in nanoseconds)
+	his.EphemeralAccountExpiry = time.Duration(0)
+	if shouldResetEphemeralAccountExpiry(his) {
+		t.Fatal("Unexpected outcome of `shouldResetEphemeralAccountExpiry`")
+	}
+
+	// altered to something other than zero (seconds interpreted in nanoseconds)
+	his.EphemeralAccountExpiry = time.Duration(oneWeekInSeconds / 2)
+	if !shouldResetEphemeralAccountExpiry(his) {
+		t.Fatal("Unexpected outcome of `shouldResetEphemeralAccountExpiry`")
+	}
+
+	// default in seconds
+	his.EphemeralAccountExpiry = time.Duration(time.Duration(oneWeekInSeconds) * time.Second)
+	if shouldResetEphemeralAccountExpiry(his) {
+		t.Fatal("Unexpected outcome of `shouldResetEphemeralAccountExpiry`")
+	}
+}
