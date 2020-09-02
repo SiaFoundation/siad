939cd6d74592437d4e8447c0fef77d79313d4beb
Ivaylo Novakov <inovakov@gmail.com>
2020-09-02 13:05:37 +0200

    Ensure we get the right redirect link.

--- node/api/skynet.go
+++ node/api/skynet.go
@@ -362,7 +362,8 @@ func (api *API) skynetSkylinkHandlerGET(w http.ResponseWriter, req *http.Request
 		// We also don't need to redirect if this is a HEAD request or if it's a
 		// download as attachment.
 		if isSkapp && !attachment && req.Method == http.MethodGet && !strings.HasSuffix(skylinkStringNoQuery, "/") {
-			w.Header().Set("Location", skylinkStringNoQuery+"/"+req.URL.RawQuery)
+			query := strings.TrimSuffix(req.URL.RawQuery, "?")
+			w.Header().Set("Location", skylinkStringNoQuery+"/?"+query)
 			w.WriteHeader(http.StatusTemporaryRedirect)
 			return
 		}
