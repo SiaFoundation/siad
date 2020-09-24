c55e7255256342137972885c3427c5bee5927306
Filip Rysavy <fil@nebulous.tech>
2020-09-24 14:03:41 +0200

    Allow local net address and host announcement with local net address

--- modules/netaddress.go
+++ modules/netaddress.go
@@ -101,7 +101,7 @@ func (na NetAddress) IsLocal() bool {
 // (which is the original reason that the loopback address was banned).
 func (na NetAddress) IsValid() error {
 	// Check the loopback address.
-	if na.IsLoopback() && build.Release != "testing" {
+	if na.IsLoopback() && build.Release == "standard" {
 		return errors.New("host is a loopback address")
 	}
 	return na.IsStdValid()
