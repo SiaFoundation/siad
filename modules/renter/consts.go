45423b1e77476cf1f0ad231ba6b51173dc3048aa
Ivaylo Novakov <inovakov@gmail.com>
2020-09-30 16:50:27 +0200

    Add the redundancy value of the file to the build.Critical that warns us about bad health files.

--- modules/renter/consts.go
+++ modules/renter/consts.go
@@ -25,8 +25,8 @@ const (
 
 // AlertCauseSiafileLowRedundancy creates a customized "cause" for a siafile
 // with a certain path and health.
-func AlertCauseSiafileLowRedundancy(siaPath modules.SiaPath, health float64) string {
-	return fmt.Sprintf("Siafile '%v' has a health of %v", siaPath.String(), health)
+func AlertCauseSiafileLowRedundancy(siaPath modules.SiaPath, health, redundancy float64) string {
+	return fmt.Sprintf("Siafile '%v' has a health of %v and redundadncy of %v", siaPath.String(), health, redundancy)
 }
 
 // Default redundancy parameters.
