8478fc323ac38fb502a3a53e6f6eca36d4d79b52
PJ <peterjan.brone@gmail.com>
2020-12-02 10:19:24 +0100

    Split MR - split off projectdownload init and overdrive

--- modules/renter/projectdownloadchunk_test.go
+++ modules/renter/projectdownloadchunk_test.go
@@ -2,65 +2,13 @@ package renter
 
 import (
 	"bytes"
-	"container/heap"
 	"context"
-	"math"
 	"testing"
-	"time"
 
 	"gitlab.com/NebulousLabs/Sia/crypto"
 	"gitlab.com/NebulousLabs/Sia/modules"
-	"gitlab.com/NebulousLabs/Sia/types"
 	"gitlab.com/NebulousLabs/fastrand"
 )
-
-// TestAddCostPenalty is a unit test that covers the `addCostPenalty` helper
-// function.
-func TestAddCostPenalty(t *testing.T) {
-	// verify overflow
-	jt := time.Duration(1)
-	jc := types.NewCurrency64(math.MaxUint64).Mul64(10)
-	pricePerMS := types.NewCurrency64(2)
-	jt = addCostPenalty(jt, jc, pricePerMS)
-	if jt != time.Duration(math.MaxInt64) {
-		t.Error("Expected job time to be adjusted to MaxInt64 on overflow")
-	}
-
-	// verify penalty higher than MaxInt64
-	jt = time.Duration(1)
-	jc = types.NewCurrency64(math.MaxInt64).Add64(1)
-	pricePerMS = types.NewCurrency64(1)
-	jt = addCostPenalty(jt, jc, pricePerMS)
-	if jt != time.Duration(math.MaxInt64) {
-		t.Error("Expected job time to be adjusted to MaxInt64 when penalty exceeds MaxInt64")
-	}
-
-	// verify high job time overflowing after adding penalty
-	jc = types.NewCurrency64(10)
-	pricePerMS = types.NewCurrency64(1)   // penalty is 10
-	jt = time.Duration(math.MaxInt64 - 5) // job time + penalty exceeds MaxInt64
-	jt = addCostPenalty(jt, jc, pricePerMS)
-	if jt != time.Duration(math.MaxInt64) {
-		t.Error("Expected job time to be adjusted to MaxInt64 when job time + penalty exceeds MaxInt64")
-	}
-
-	// verify happy case
-	jt = time.Duration(fastrand.Intn(10) + 1)
-	jc = types.NewCurrency64(fastrand.Uint64n(100) + 1)
-	pricePerMS = types.NewCurrency64(fastrand.Uint64n(10) + 1)
-	adjusted := addCostPenalty(jt, jc, pricePerMS)
-	if adjusted <= jt {
-		t.Error("unexpected")
-	}
-
-	// verify we assert pricePerMS to be higher than zero
-	defer func() {
-		if r := recover(); r == nil {
-			t.Error("Expected a panic when pricePerMS is zero")
-		}
-	}()
-	addCostPenalty(jt, jc, types.ZeroCurrency)
-}
 
 // TestProjectDownloadChunkFinalize is a unit test for the 'finalize' function
 // on the pdc. It verifies whether the returned data is properly offset to
@@ -225,46 +173,6 @@ func TestProjectDownloadChunkFinished(t *testing.T) {
 		t.Fatal("unexpected error", err)
 	}
 	if !finished {
-		t.Fatal("unexpected")
-	}
-}
-
-// TestProjectDownloadChunkHeap is a unit test that covers the functionality of
-// the pdcWorkerHeap
-func TestProjectDownloadChunkHeap(t *testing.T) {
-	var wh pdcWorkerHeap
-	if wh.Len() != 0 {
-		t.Fatal("unexpected")
-	}
-
-	now := time.Now()
-	tMin1 := now.Add(time.Minute)
-	tMin5 := now.Add(5 * time.Minute)
-	tMin10 := now.Add(10 * time.Minute)
-
-	// add one element
-	heap.Push(&wh, &pdcInitialWorker{completeTime: tMin5})
-	if wh.Len() != 1 {
-		t.Fatal("unexpected")
-	}
-
-	// add more elements in a way they should be popped off in a different order
-	heap.Push(&wh, &pdcInitialWorker{completeTime: tMin1})
-	heap.Push(&wh, &pdcInitialWorker{completeTime: tMin10})
-	if wh.Len() != 3 {
-		t.Fatal("unexpected")
-	}
-
-	worker := heap.Pop(&wh).(*pdcInitialWorker)
-	if worker == nil || worker.completeTime != tMin1 {
-		t.Fatal("unexpected")
-	}
-	worker = heap.Pop(&wh).(*pdcInitialWorker)
-	if worker == nil || worker.completeTime != tMin5 {
-		t.Fatal("unexpected")
-	}
-	worker = heap.Pop(&wh).(*pdcInitialWorker)
-	if worker == nil || worker.completeTime != tMin10 {
 		t.Fatal("unexpected")
 	}
 }
