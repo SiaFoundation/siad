7465d657c2ddffc3d150ba7ec71374a68a632fdd
netzbasis <programmer@netzbasis.de>
2021-01-18 17:29:55 +0000

    Build golangci-lint from scratch. Makes it work even when there are no pre-build binaries.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,7 +3,7 @@ set -e
 
 if ! [ -x "$(command -v golangci-lint)" ]; then
   echo "Installing golangci-lint..."
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.24.0
+  go get github.com/golangci/golangci-lint@v1.24.0
 fi
 
 if ! [ -x "$(command -v codespell)" ]; then
