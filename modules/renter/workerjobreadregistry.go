8332c2c8a3077e82142e652ecdd517d56d6864b3
Chris Schinnerl <chris@nebulous.tech>
2021-03-09 11:16:59 +0100

    extend TestRegistryUpdateRead

--- modules/renter/workerjobreadregistry.go
+++ modules/renter/workerjobreadregistry.go
@@ -187,6 +187,8 @@ func (j *jobReadRegistry) callExecute() {
 	// Check if we have a cached version of the looked up entry. If the new entry
 	// has a higher revision number we update it. If it has a lower one we know that
 	// the host should be punished for losing it or trying to cheat us.
+	// TODO: update the cache to store the hash in addition to the revision
+	// number for verifying the pow.
 	if srv != nil {
 		cachedRevision, cached := w.staticRegistryCache.Get(j.staticSiaPublicKey, j.staticTweak)
 		if cached && cachedRevision > srv.Revision {
