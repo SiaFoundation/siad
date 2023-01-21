1c7d3138afaf7e685dbbfdf446b51be580a062b7
Nate Maninger <me@n8m.us>
2023-01-21 08:29:53 -0700

    testnet: change environment variables for testnet

--- build/env_testnet.go
+++ build/env_testnet.go
@@ -1,26 +1,26 @@
-//go:build !testnet
-// +build !testnet
+//go:build testnet
+// +build testnet
 
 package build
 
 var (
 	// siaAPIPassword is the environment variable that sets a custom API
 	// password if the default is not used
-	siaAPIPassword = "SIA_API_PASSWORD"
+	siaAPIPassword = "SIA_ZEN_API_PASSWORD"
 
 	// siaDataDir is the environment variable that tells siad where to put the
 	// general sia data, e.g. api password, configuration, logs, etc.
-	siaDataDir = "SIA_DATA_DIR"
+	siaDataDir = "SIA_ZEN_DATA_DIR"
 
 	// siadDataDir is the environment variable which tells siad where to put the
 	// siad-specific data
-	siadDataDir = "SIAD_DATA_DIR"
+	siadDataDir = "SIAD_ZEN_DATA_DIR"
 
 	// siaWalletPassword is the environment variable that can be set to enable
 	// auto unlocking the wallet
-	siaWalletPassword = "SIA_WALLET_PASSWORD"
+	siaWalletPassword = "SIA_ZEN_WALLET_PASSWORD"
 
 	// siaExchangeRate is the environment variable that can be set to
 	// show amounts (additionally) in a different currency
-	siaExchangeRate = "SIA_EXCHANGE_RATE"
+	siaExchangeRate = "SIA_ZEN_EXCHANGE_RATE"
 )
