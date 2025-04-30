1e325be4dc6832041e8c8f4d2ea9c8895b860ea8
Nate <me@n8m.us>
2025-04-30 13:19:22 -0700

    fix lint again

--- api/types.go
+++ api/types.go
@@ -78,7 +78,7 @@ type (
 		DustThreshold types.Currency `json:"dustthreshold"`
 	}
 
-	//WalletWatchPOST contains the set of addresses to add or remove from the watch set.
+	// WalletWatchPOST contains the set of addresses to add or remove from the watch set.
 	WalletWatchPOST struct {
 		Addresses []types.Address `json:"addresses"`
 		Remove    bool            `json:"remove"`
